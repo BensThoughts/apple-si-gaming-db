23c40f3f6bf78ec737215b7edab65c97577a0dca
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-06 00:00:48 -0700

    import app details request

--- apps/remix-app/app/lib/data-utils/steamApi.server.ts
+++ apps/remix-app/app/lib/data-utils/steamApi.server.ts
@@ -1,2 +1,2 @@
-import { getSteamPlayerOwnedGamesRequest } from '@apple-si-gaming-db/steam-api';
-export { getSteamPlayerOwnedGamesRequest };
+import { getSteamPlayerOwnedGamesRequest, getSteamAppDetailsRequest } from '@apple-si-gaming-db/steam-api';
+export { getSteamPlayerOwnedGamesRequest, getSteamAppDetailsRequest };
