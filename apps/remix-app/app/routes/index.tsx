6b6ba83bd5f3156ddcbcad0e83c46731bba0059b
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-07 12:34:18 -0700

    removed unused css class

--- apps/remix-app/app/routes/index.tsx
+++ apps/remix-app/app/routes/index.tsx
@@ -1,11 +1,10 @@
 export default function Index() {
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center bg-primary">
+    <main className="relative min-h-screen sm:flex sm:items-center sm:justify-center bg-primary">
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
             <div className="absolute inset-0">
-
               <div className="absolute inset-0 bg-[color:rgba(27,167,254,0.5)] mix-blend-multiply" />
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
