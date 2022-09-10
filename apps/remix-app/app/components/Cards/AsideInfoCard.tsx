05780b1a38e8ebdc42debf7abcb0969225a63f72
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-09 22:24:26 -0700

    extend aside card with div attributes

--- apps/remix-app/app/components/Cards/AsideInfoCard.tsx
+++ apps/remix-app/app/components/Cards/AsideInfoCard.tsx
@@ -1,15 +1,18 @@
+import React from 'react';
 import { InformationCircleIcon } from '~/components/Icons';
 
 export default function AsideInfoCard({
   title,
+  className,
   children,
+  ...rest
 }: {
   title: string;
   children: React.ReactNode;
-}) {
+} & React.HTMLAttributes<HTMLDivElement>) {
   return (
     <aside className={`relative px-6 py-8 border-l-[4px] border-solid border-secondary
-                       rounded-lg mt-2 bg-primary`}>
+                       rounded-lg mt-2 bg-primary ${className}`} {...rest}>
       <div className="rounded-[50%] absolute inset-0 bg-app-bg w-[40px] h-[40px] -translate-x-[50%] -translate-y-[50%]">
         <InformationCircleIcon className="absolute inset-0 translate-x-[3px] translate-y-[3px] text-icon-secondary w-8 h-8" />
       </div>
