15a82661cc2ee0fdfe8386e475101644fc528937
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-09 22:24:05 -0700

    added standardized heading component

---
+++ apps/remix-app/app/components/Heading.tsx
@@ -0,0 +1,18 @@
+export default function Heading({
+  children,
+}: {
+  children: string;
+}) {
+  return (
+    <div className={`mt-4
+    after:w-full after:h-[2px] after:block after:relative
+    after:top-0 after:left-0 after:bg-secondary
+    after:-translate-y-4 after:-translate-x-[110%]
+
+    before:w-full before:h-[2px] before:block before:relative
+    before:top-0 before:right-0 before:bg-secondary
+    before:translate-y-4 before:translate-x-[110%]`}>
+      <h2 className='text-secondary text-xl'>{children}</h2>
+    </div>
+  );
+}
