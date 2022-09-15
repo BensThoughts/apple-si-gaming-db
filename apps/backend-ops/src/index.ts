ef8064cc1fba20d7f2e602f9f8d7e372a81ab804
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-14 22:08:12 -0700

    use commander to launch different backend commands

--- apps/backend-ops/src/index.ts
+++ apps/backend-ops/src/index.ts
@@ -1,6 +1,8 @@
 import chalk from 'chalk';
 import { Command } from 'commander';
 import { stage } from './stage';
+import { updateAppIds } from './update-appids';
+import { logger } from '@apple-si-gaming-db/logger';
 const program = new Command();
 
 program
@@ -11,17 +13,49 @@ ${chalk.red('DATABASE_URL env needs to be set correctly to connect to the db')}`
 
 program.command('stage')
     .description('Stage the entire db. Upsert data from the steam API to the db')
-    .option('-p, --page <number>', chalk.yellow('The initial page in the db to start on'), '0')
+    .option(
+        '-p, --page <number>',
+        chalk.yellow('The initial page in the db to start on, 0 is the first page. Can also be set via env var $ASGD_INITIAL_PAGE'),
+        '0',
+    )
+    .option(
+        '-b, --batch-size <number>',
+        chalk.yellow('The number of appids to pull from steam every 5 minutes. Can also be set via env var $ASGD_BATCH_SIZE'),
+        '200',
+    )
     .action(async (opts) => {
       // const STARTING_PAGE = 3; // fly.io current page
       // const STARTING_PAGE = 145; // local current page
-      const initialPage = Number(opts.page);
-      if (isNaN(initialPage)) {
-        console.log(chalk.yellow('option -p --page needs to be a number'));
+      const {
+        ASGD_INITIAL_PAGE,
+        ASGD_BATCH_SIZE,
+      } = process.env;
+      const INITIAL_PAGE = Number(ASGD_INITIAL_PAGE ? ASGD_INITIAL_PAGE : opts.age);
+      if (isNaN(INITIAL_PAGE)) {
+        logger.warn('env var $ASGD_INITIAL_PAGE or option -p --page needs to be a valid number', {
+          '$ASGD_INITIAL_PAGE': ASGD_INITIAL_PAGE,
+          'opts.page': opts.page,
+        });
         process.exit(1);
       }
-      stage(initialPage);
-      // stage(initialPage);
+
+      const BATCH_SIZE = Number(ASGD_BATCH_SIZE ? ASGD_BATCH_SIZE : opts.batchSize);
+      if (isNaN(BATCH_SIZE)) {
+        logger.warn('env var $ASGD_BATCH_SIZE or option -b --batch-size needs to be a valid number', {
+          '$ASGD_BATCH_SIZE': ASGD_BATCH_SIZE,
+          'opts.batchSize': opts.batchSize,
+        });
+        process.exit(1);
+      }
+      logger.info(`Initial page set to ${INITIAL_PAGE}`);
+      logger.info(`Batch size set to ${BATCH_SIZE}`);
+      stage(INITIAL_PAGE, BATCH_SIZE);
+    });
+
+program.command('update-appids')
+    .description('Sync the appids in db with the steam-api')
+    .action(async () => {
+      await updateAppIds();
     });
 
 program.parse();
--- apps/backend-ops/src/stage.ts
+++ apps/backend-ops/src/stage.ts
@@ -9,7 +9,7 @@ import {
   prisma,
 } from '@apple-si-gaming-db/database';
 
-import logger from '@apple-si-gaming-db/logger';
+import { logger } from '@apple-si-gaming-db/logger';
 
 interface AppIdData {
   steamAppId: number;
@@ -57,10 +57,10 @@ async function getSteamAppDataAndUpdateDB(appIdData: AppIdData) {
   }
 }
 
-async function getPageOfData(page: number, totalPages: number) {
+async function getPageOfData(page: number, batchSize: number, totalPages: number) {
   const appids = await prisma.steamApp.findMany({
-    skip: page * BATCH_SIZE,
-    take: BATCH_SIZE,
+    skip: page * batchSize,
+    take: batchSize,
     select: {
       steamAppId: true,
       name: true,
@@ -73,7 +73,7 @@ async function getPageOfData(page: number, totalPages: number) {
   logger.info(`Starting page ${page} of ${totalPages}`);
   logger.info(`Page ${page} appids found in prisma: ${appids.length}`);
 
-  let totalIdx = page * BATCH_SIZE;
+  let totalIdx = page * batchSize;
   let currIdx = 0;
 
   const oneSecondInterval = setInterval(async () => {
@@ -92,32 +92,36 @@ async function getPageOfData(page: number, totalPages: number) {
   }, ONE_SECOND_INTERVAL);
 }
 
-const BATCH_SIZE = 200; // The number of apps per a "page" of data
 const ONE_SECOND_INTERVAL = (1000); // 1seconds
 const FIVE_MINUTE_INTERVAL = (5 * 60 * 1000) + 1000; // 5 minutes + 1 second
 
 // DB starts at page 0
-// const STARTING_PAGE = 3; // fly.io current page
-const STARTING_PAGE = 144; // local current page
 
-async function getTotalPages() {
-  const aggregate = await prisma.steamApp.aggregate({ _count: true });
-  const NUM_APPIDS = aggregate._count;
-  logger.info(`Number of appids ${NUM_APPIDS}`);
-  return Math.ceil(NUM_APPIDS / BATCH_SIZE);
+async function getTotalPages(batchSize: number) {
+  const numAppIds = await prisma.steamApp.count();
+  logger.info(`Number of appids ${numAppIds}`);
+  return Math.ceil(numAppIds / batchSize);
 }
 
-export default async function stage() {
-  let page = STARTING_PAGE;
-  const TOTAL_PAGES = await getTotalPages();
 
-  getPageOfData(page, TOTAL_PAGES);
+/**
+ * Sync detailed information about each app in the db with the steam api.
+ * Uses steams getAppDetails request
+ * @param  {number} initialPage - The page of appids in db to start on
+ * @param  {number} batchSize - The number of appids per a page
+ */
+export async function stage(initialPage: number, batchSize: number) {
+  const totalPages = await getTotalPages(batchSize);
+
+  getPageOfData(initialPage, batchSize, totalPages);
+
+  let currPage = initialPage;
 
   const fiveMinuteInterval = setInterval(async () => {
-    page += 1;
-    if (page > TOTAL_PAGES) {
+    currPage += 1;
+    if (currPage > totalPages) {
       clearInterval(fiveMinuteInterval);
     }
-    getPageOfData(page, TOTAL_PAGES);
+    getPageOfData(currPage, batchSize, totalPages);
   }, FIVE_MINUTE_INTERVAL);
 }
