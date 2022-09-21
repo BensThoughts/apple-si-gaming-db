2986cf3dc6b28e96ac27eb4c7873bd2c34d376fb
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-21 12:54:17 -0700

    start adding server side error checking on search

--- apps/remix-app/app/routes/search.tsx
+++ apps/remix-app/app/routes/search.tsx
@@ -1,14 +1,25 @@
 import { Outlet } from '@remix-run/react';
-import SearchInput from '~/components/FormComponents/SearchInput';
-
 
 export default function SearchRoute() {
   return (
     <div className="flex flex-col gap-6 items-center min-h-screen w-full">
-      <div className="mt-2">
-        <SearchInput size="large" />
+      <div className="w-full min-h-[2rem] bg-primary py-2 px-6">
+        <h1 className="text-2xl md:text-3xl text-center text-secondary">
+          Search
+        </h1>
       </div>
-      <Outlet />
+      <div className="flex flex-col w-full items-center gap-6">
+        <div className="w-[180px] block">
+          <img
+            src="/svg-images/searching-robot.svg"
+            alt="searching robot"
+            onError={(e) => {
+              e.currentTarget.src = '/svg-images/no-image-placeholder.svg';
+            }}
+          />
+        </div>
+        <Outlet />
+      </div>
     </div>
   );
 }
@@ -17,7 +28,9 @@ export function ErrorBoundary({ error }: { error: Error }) {
   return (
     <div>
       <h1>Error in /search route</h1>
-      <pre>{error.message}</pre>
+      <div>{error.message}</div>
     </div>
   );
 }
+
+
--- apps/remix-app/app/routes/search/index.tsx
+++ apps/remix-app/app/routes/search/index.tsx
@@ -2,63 +2,124 @@ import type { LoaderArgs } from '@remix-run/server-runtime';
 import { json } from '@remix-run/node';
 import { useLoaderData, useTransition } from '@remix-run/react';
 import { searchReleasedSteamAppsByName } from '~/models/steamApp.server';
+import { useMediaIsWide } from '~/lib/hooks/useMedia';
 import SearchTitleCard from '~/components/Cards/SearchTitleCard';
 import LoadingComponent from '~/components/LoadingComponent';
+import SearchInput from '~/components/FormComponents/SearchInput';
+
+
+function validateSearchQuery(searchQuery: string) {
+  if (searchQuery.length > 100) {
+    return `The search query is too long (100 character maximum)`;
+  }
+  if ((searchQuery === '') || (searchQuery.length < 1)) {
+    return `Search query must contain at least 1 character`;
+  }
+}
+
+export type LoaderData = {
+  steamApps?: {
+    name: string;
+    steamAppId: number;
+    headerImage: string | null;
+    releaseDate: string | null;
+  }[] | null;
+  formError?: string;
+  fieldErrors?: {
+    searchQuery?: string | undefined;
+  };
+  fields?: {
+    searchQuery: string;
+  };
+};
+
+const badRequest = (data: LoaderData) => json(data, { status: 400 });
 
 export async function loader({
   request,
 }: LoaderArgs) {
   const url = new URL(request.url);
   const searchQuery = url.searchParams.get('searchQuery')?.trim();
-  if (searchQuery && searchQuery != '') {
-    const steamApps = await searchReleasedSteamAppsByName(searchQuery);
-    return json({ steamApps });
+
+  // This is the case where someone navigates to /search initially
+  if (searchQuery === undefined) {
+    return json({ steamApps: null, fields: { searchQuery: '' } });
   }
-  return json({
-    steamApps: null,
-  });
+
+  const t = '';
+  console.log(t.length);
+
+  if (typeof searchQuery !== 'string') {
+    return badRequest({
+      formError: `Search form not submitted correctly.`,
+    });
+  }
+  const fieldErrors = {
+    searchQuery: validateSearchQuery(searchQuery),
+  };
+  const fields = { searchQuery };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
+  }
+
+  const steamApps = await searchReleasedSteamAppsByName(searchQuery);
+  return json({ steamApps, fields });
 }
 
-
 export default function SearchIndexRoute() {
-  const { steamApps } = useLoaderData<typeof loader>();
+  const isWide = useMediaIsWide();
+  const { steamApps, fields } = useLoaderData<typeof loader>();
+  const searchQuery = fields?.searchQuery ? fields.searchQuery : '';
   const transition = useTransition();
 
-  if (transition.state === 'loading' || transition.state === 'submitting') {
-    return (
-      <LoadingComponent />
-    );
-  }
+  const isLoadingSearchResults = (
+    (transition.state === 'loading' && transition.location.pathname === '/search') ||
+    (transition.state === 'submitting' && transition.location.pathname === '/search')
+  ) ? true : false;
 
-  if (!steamApps) {
-    return (
-      <div>
-        Search for an app!
-      </div>
-    );
-  }
   return (
-    <>
-      {steamApps.length > 0
-        ? (
-          <div className="flex flex-col gap-3 items-center w-full">
-            {steamApps.map(({ steamAppId, name, headerImage, releaseDate }) => (
-              <div key={steamAppId} className="">
-                <SearchTitleCard
-                  name={name}
-                  steamAppId={steamAppId}
-                  headerImageSrc={headerImage}
-                  releaseDate={releaseDate}
-                />
+    <div>
+      <div>
+        <SearchInput
+          defaultValue={searchQuery}
+          componentSize={isWide ? 'large' : 'medium'}
+        />
+      </div>
+      <div>
+        {(isLoadingSearchResults ? <LoadingComponent /> : (
+          <>
+            {(steamApps && steamApps.length > 0) ? (
+              <div className="flex flex-col gap-3 items-center w-full">
+                {steamApps.map(({ steamAppId, name, headerImage, releaseDate }) => (
+                  <div key={steamAppId}>
+                    <SearchTitleCard
+                      name={name}
+                      steamAppId={steamAppId}
+                      headerImageSrc={headerImage}
+                      releaseDate={releaseDate}
+                    />
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            No Apps Found
-          </div>
-        )
-      }
-    </>
+            ) : (
+              <>
+                {(searchQuery.length > 0) ? <div>No Apps Found</div> : null}
+              </>
+            )}
+          </>
+        ))}
+
+      </div>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <div>
+      <h1>Error in /search/index route</h1>
+      <div>{error.message}</div>
+    </div>
   );
 }
