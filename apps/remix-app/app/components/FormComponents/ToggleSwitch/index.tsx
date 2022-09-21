766d32d50e73ed1a68f19e72984c9c353b8b94c0
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-21 12:50:59 -0700

    changed ring color

--- apps/remix-app/app/components/FormComponents/ToggleSwitch/index.tsx
+++ apps/remix-app/app/components/FormComponents/ToggleSwitch/index.tsx
@@ -18,7 +18,7 @@ export default function ToggleSwitch({
           relative inline-flex items-center h-[38px] w-[74px] shrink-0 cursor-pointer
           rounded-full border-2 border-transparent transition-colors duration-200
           ease-in-out focus:outline-none focus-visible:ring-2
-          focus-visible:ring-white focus-visible:ring-opacity-75`}
+          focus-visible:ring-secondary focus-visible:ring-opacity-75`}
       >
         <span className="sr-only">Filter Apple Only Games</span>
         <span
