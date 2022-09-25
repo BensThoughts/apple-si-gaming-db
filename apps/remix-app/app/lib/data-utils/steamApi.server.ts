1a55ba2847512e4d0c3d87fe472560b180c40c00
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-24 17:41:53 -0700

    simplified export statement

--- apps/remix-app/app/lib/data-utils/steamApi.server.ts
+++ apps/remix-app/app/lib/data-utils/steamApi.server.ts
@@ -1,2 +1,4 @@
-import { getSteamPlayerOwnedGamesRequest, getSteamAppDetailsRequest } from '@apple-si-gaming-db/steam-api';
-export { getSteamPlayerOwnedGamesRequest, getSteamAppDetailsRequest };
+export {
+  getSteamPlayerOwnedGamesRequest,
+  getSteamAppDetailsRequest,
+} from '@apple-si-gaming-db/steam-api';
