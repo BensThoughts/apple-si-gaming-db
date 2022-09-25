3698a6366c41abf7b700bda36e46076aeb0d3be7
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-24 17:43:08 -0700

    added export of SystemSpec types

--- packages/database/src/interfaces/index.ts
+++ packages/database/src/interfaces/index.ts
@@ -1,4 +1,5 @@
 export * from './PrismaSteamApp';
 export * from './PrismaSteamUser';
 export * from './PrismaPerformancePost';
+export * from './PrismaSteamUserSystemSpecs';
 export { Prisma } from '@prisma/client';
