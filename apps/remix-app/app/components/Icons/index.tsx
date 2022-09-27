e5a271d44c12015ee68caaba85b4697f36b0471f
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-27 16:26:17 -0700

    added Edit and Trash icons

--- apps/remix-app/app/components/Icons/index.tsx
+++ apps/remix-app/app/components/Icons/index.tsx
@@ -3,11 +3,13 @@ export { default as BarsIcon } from './Bars';
 export { default as CheckIcon } from './Check';
 export { default as ChevronUpIcon } from './ChevronUp';
 export { default as CloudMoonIcon } from './CloudMoon';
+export { default as EditIcon } from './Edit';
 export { default as ExternalLinkIcon } from './ExternalLink';
 export { default as InformationCircleIcon } from './InformationCircle';
 export { default as LinuxIcon } from './Linux';
 export { default as RatingMedalIcon } from './RatingMedal';
 export { default as SteamIcon } from './Steam';
 export { default as SunIcon } from './Sun';
+export { default as TrashIcon } from './Trash';
 export { default as WindowsIcon } from './Windows';
 
