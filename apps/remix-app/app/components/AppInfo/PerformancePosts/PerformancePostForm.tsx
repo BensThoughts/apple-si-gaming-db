bd221d4dc26019f54f52ff65a5918e83470e6723
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-27 16:39:08 -0700

    simplify logic by returning early

--- apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostForm.tsx
+++ apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostForm.tsx
@@ -1,18 +1,31 @@
-import { Form } from '@remix-run/react';
+import { Form, Link } from '@remix-run/react';
 import RoundedButton from '~/components/RoundedButton';
 import SelectMenu from '~/components/FormComponents/SelectMenu';
 import type { CreatePostActionData } from '~/routes/apps/$steamAppId/performance-posts';
 import type { RatingMedal } from '~/interfaces/database';
+import AnimatedUnderline from '~/components/AnimatedUnderline';
+import React from 'react';
 
 
 interface PerformancePostFormProps {
   steamAppId: number;
   steamUserIsLoggedIn: boolean;
   steamUserOwnsApp: boolean;
-  steamUserSystemNames: string[] | null;
+  steamUserSystemNames: string[];
   fields: CreatePostActionData['fields'];
   fieldErrors: CreatePostActionData['fieldErrors'];
   formError: CreatePostActionData['formError'];
+}
+
+function Wrapper({
+  children,
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={`flex flex-col gap-3 items-center justify-center bg-tertiary
+                     border-solid border-1 border-secondary p-3 rounded-lg w-full`}>
+      {children}
+    </div>
+  );
 }
 
 export default function PerformancePostForm({
@@ -24,68 +37,90 @@ export default function PerformancePostForm({
   formError,
   fieldErrors,
 }: PerformancePostFormProps) {
+  if (!steamUserIsLoggedIn) {
+    return (
+      <Wrapper>
+        <div>
+          You are not logged in. You must&nbsp;
+          <Link to="/profile">
+            <AnimatedUnderline>
+              <span className="text-secondary">login</span>
+            </AnimatedUnderline>
+          </Link>
+          &nbsp;to post a performance review for this app.
+        </div>
+      </Wrapper>
+    );
+  }
+  if (!steamUserOwnsApp) {
+    return (
+      <Wrapper>
+        <div>
+          It looks like you do not own this app yet. Add it to your steam library to leave a
+          performance review. (You also need to add at least 1 system to your profile to leave
+          a review)
+        </div>
+      </Wrapper>
+    );
+  }
+  if (steamUserSystemNames.length < 1) {
+    return (
+      <Wrapper>
+        <div>
+          You need to add a system to your&nbsp;
+          <Link to="/profile">
+            <AnimatedUnderline>
+              <span className="text-secondary">profile</span>
+            </AnimatedUnderline>
+          </Link>
+          &nbsp;to post a performance review for this app.
+        </div>
+      </Wrapper>
+    );
+  }
   const ratingOptions: (RatingMedal | 'None')[] = ['None', 'Platinum', 'Gold', 'Silver', 'Bronze', 'Borked'];
   return (
-    <div className={`flex flex-col gap-3 items-center justify-center bg-tertiary
-                     border-solid border-1 border-secondary p-3 rounded-lg w-full`}>
-      {steamUserIsLoggedIn ? (
-        <>
-          {(steamUserOwnsApp && steamUserSystemNames) ? (
-            <>
-              <h2 className="text-secondary text-lg">Submit Your Own Performance Post</h2>
-              {formError && <div className="text-color-error">{formError}</div>}
-              <Form
-                method="post"
-                name="performancePost"
-                className="flex flex-col items-center gap-3 w-full max-w-lg"
-                action={`/apps/${steamAppId}/performance-posts`}
-              >
-                <label className="w-full">
+    <Wrapper>
+      <h2 className="text-secondary text-lg">Submit Your Own Performance Post</h2>
+      {formError && <div className="text-color-error">{formError}</div>}
+      <Form
+        method="post"
+        name="performancePost"
+        className="flex flex-col items-center gap-3 w-full max-w-lg"
+        action={`/apps/${steamAppId}/performance-posts`}
+      >
+        <label className="w-full">
                   Post Text{`: `}
-                  {fieldErrors?.postText ? (
+          {fieldErrors?.postText ? (
                     <span className="text-color-error">
                       {fieldErrors.postText}
                     </span>
                   ) : null}
-                  <textarea
-                    id="performancePostText"
-                    name="performancePostText"
-                    className="bg-primary rounded-lg p-2 w-full h-28"
-                    defaultValue={fields?.postText ? fields.postText : ''}
-                    required
-                    minLength={3}
-                    maxLength={500}
-                  />
-                </label>
-                <SelectMenu
-                  name="performancePostRatingMedal"
-                  defaultValue={fields?.ratingMedal ? fields.ratingMedal : 'None'}
-                  options={ratingOptions}
-                  label="Rating"
-                  errorMessage={fieldErrors?.ratingMedal}
-                />
-                <SelectMenu
-                  name="performancePostSystemName"
-                  defaultValue={steamUserSystemNames[0]}
-                  options={steamUserSystemNames}
-                  label="Select System"
-                />
-                <RoundedButton type="submit" className="max-w-xs">Submit</RoundedButton>
-              </Form>
-            </>
-          ): (
-            <div>
-              It looks like you do not own this app yet. Add it to your steam library to leave a
-              performance review. (You also need to add at least 1 system to your profile to leave
-              a review)
-            </div>
-          )}
-        </>
-      ): (
-        <div>
-          You are not logged in. You must login to post a performance review for this app.
-        </div>
-      )}
-    </div>
+          <textarea
+            id="performancePostText"
+            name="performancePostText"
+            className="bg-primary rounded-lg p-2 w-full h-28"
+            defaultValue={fields?.postText ? fields.postText : ''}
+            required
+            minLength={3}
+            maxLength={500}
+          />
+        </label>
+        <SelectMenu
+          name="performancePostRatingMedal"
+          defaultValue={fields?.ratingMedal ? fields.ratingMedal : 'None'}
+          options={ratingOptions}
+          label="Rating"
+          errorMessage={fieldErrors?.ratingMedal}
+        />
+        <SelectMenu
+          name="performancePostSystemName"
+          defaultValue={steamUserSystemNames[0]}
+          options={steamUserSystemNames}
+          label="Select System"
+        />
+        <RoundedButton type="submit" className="max-w-xs">Submit</RoundedButton>
+      </Form>
+    </Wrapper>
   );
 }
