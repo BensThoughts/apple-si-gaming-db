250a32f4d7a3405f7c2fe107df96814227deb9b9
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-27 16:22:03 -0700

    added className prop for external styling

--- apps/remix-app/app/components/AnimatedUnderline.tsx
+++ apps/remix-app/app/components/AnimatedUnderline.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 export default function AnimatedUnderline({
+  className,
   children,
+  ...rest
 }: React.HTMLAttributes<HTMLSpanElement>) {
   return (
-    <span className={`relative px-0 py-[0.1em] overflow-hidden no-underline
-                      after:block after:absolute after:bottom-[-3px] after:left-0
-                      after:w-0 after:h-[0.2em] after:bg-secondary after:transition-transform
-                      after:hover:w-full text-primary-highlight`}>
+    <span
+      className={`relative px-0 py-[0.1em] overflow-hidden no-underline
+                  after:block after:absolute after:bottom-[-3px] after:left-0
+                  after:w-0 after:h-[0.2em] after:bg-secondary after:transition-transform
+                  after:hover:w-full text-primary-highlight ${className}`}
+      {...rest}
+    >
       {children}
     </span>
   );
