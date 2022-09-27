805e64a638a9217c2449f2810fe442a3a65e5975
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-09-27 16:23:08 -0700

    removed beefed up RoundedButton to be more generic

--- apps/remix-app/app/components/RoundedButton.tsx
+++ apps/remix-app/app/components/RoundedButton.tsx
@@ -1,6 +1,14 @@
 export default function RoundedButton({ className, children, ...rest }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button className={`px-4 py-2 rounded bg-secondary hover:bg-secondary-highlight ${className}`} {...rest}>
+    <button
+      className={`text-sm text-primary-highlight inline-flex justify-center items-center
+                  border border-transparent font-medium px-4 py-2 rounded
+                  bg-secondary hover:bg-secondary-highlight focus:outline-none
+                  focus-visible:bg-secondary-highlight focus-visible:ring-2
+                  focus-visible:ring-secondary-highlight focus-visible:ring-offset-1
+                  focus-visible:ring-offset-white ${className}`}
+      {...rest}
+    >
       {children}
     </button>
   );
