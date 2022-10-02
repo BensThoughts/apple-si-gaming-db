7aecf9807a17e7b7572c3262ec0872acfdc5c8fd
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-01 19:26:57 -0700

    linted

--- apps/remix-app/tailwind.config.js
+++ apps/remix-app/tailwind.config.js
@@ -33,11 +33,6 @@ module.exports = {
       'transparent': colors.transparent,
     },
     extend: {
-      fontFamily: {
-        'press-start': ['"Press Start 2P"', 'cursive'],
-        'roboto-slab': ['"Roboto Slab"', 'serif'],
-        'merriweather': ['"Merriweather"', 'serif'],
-      },
       textColor: {
         'primary': generateColorClass('color-text-primary'),
         'primary-highlight': generateColorClass('color-text-primary-highlight'),
@@ -54,25 +49,28 @@ module.exports = {
       borderWidth: {
         1: '1px',
       },
-      animation: {
-        'enter': 'enter 200ms ease-out',
-        'slide-in': 'slide-in 1.2s cubic-bezier(.41,.73,.51,1.02)',
-        'leave': 'leave 150ms ease-in forwards',
-      },
-      keyframes: {
-        'enter': {
-          '0%': { transform: 'scale(0.9)', opacity: 0 },
-          '100%': { transform: 'scale(1)', opacity: 1 },
-        },
-        'leave': {
-          '0%': { transform: 'scale(1)', opacity: 1 },
-          '100%': { transform: 'scale(0.9)', opacity: 0 },
-        },
-        'slide-in': {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' },
-        },
-      },
+      // fontFamily: {
+      //   'merriweather': ['"Merriweather"', 'serif'],
+      // },
+      // animation: {
+      //   'enter': 'enter 200ms ease-out',
+      //   'slide-in': 'slide-in 1.2s cubic-bezier(.41,.73,.51,1.02)',
+      //   'leave': 'leave 150ms ease-in forwards',
+      // },
+      // keyframes: {
+      //   'enter': {
+      //     '0%': { transform: 'scale(0.9)', opacity: 0 },
+      //     '100%': { transform: 'scale(1)', opacity: 1 },
+      //   },
+      //   'leave': {
+      //     '0%': { transform: 'scale(1)', opacity: 1 },
+      //     '100%': { transform: 'scale(0.9)', opacity: 0 },
+      //   },
+      //   'slide-in': {
+      //     '0%': { transform: 'translateY(-100%)' },
+      //     '100%': { transform: 'translateY(0)' },
+      //   },
+      // },
     },
   },
   variants: {
