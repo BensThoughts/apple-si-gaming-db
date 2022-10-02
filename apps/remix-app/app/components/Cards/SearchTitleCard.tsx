18060fe6c9e2f0dbdfa6a5901ae60808bb081ffc
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-02 11:29:57 -0700

    changed layout

--- apps/remix-app/app/components/Cards/SearchTitleCard.tsx
+++ apps/remix-app/app/components/Cards/SearchTitleCard.tsx
@@ -16,34 +16,43 @@ export default function SearchTitleCard({
   return (
     <Link
       to={`/apps/${steamAppId}/performance-posts`}
-      className="focus-visible:show-ring p-1 md:p-2 bg-primary hover:bg-primary-highlight rounded-lg
-      flex flex-col gap-2 items-center max-w-[460px]"
+      className="focus-visible:show-ring bg-tertiary hover:bg-tertiary-highlight
+                 rounded-lg w-full h-full max-w-lg focus-visible:bg-tertiary-highlight
+                 block"
     >
-      {headerImageSrc && (
-        <div className="rounded-lg flex items-center justify-center max-w-[464px]">
-          <img
-            src={headerImageSrc}
-            alt={`Header for ${name}`}
-            width={460}
-            height={215}
-            className="rounded-md"
-            loading="lazy"
-            onError={(e) => {
-              e.currentTarget.src = '/svg-images/no-image-placeholder.svg';
-            }}
-          />
+      <div
+        className="p-1 md:p-2 flex flex-col md:flex-row gap-2 items-center md:items-start
+        w-full max-w-lg"
+      >
+
+        <div>
+          {headerImageSrc && (
+            <div className="rounded-lg flex items-center justify-center max-w-lg">
+              <img
+                src={headerImageSrc}
+                alt={`Header for ${name}`}
+                width={460}
+                height={215}
+                className="rounded-md"
+                loading="lazy"
+                onError={(e) => {
+                  e.currentTarget.src = '/svg-images/no-image-placeholder.svg';
+                }}
+              />
+            </div>
+          )}
         </div>
-      )}
-      <div className="flex flex-col flex-wrap justify-between w-full px-2">
-        {releaseDate && (
-          <span className="hidden md:inline text-sm">
+        <div className="flex flex-col flex-wrap gap-1 justify-between w-full px-2 md:text-right text-primary-highlight">
+          {releaseDate && (
+            <div className="text-sm order-1 md:order-2 italic text-primary-faded">
               Released:&nbsp;
-            <i className="italic">{releaseDate}</i>
-          </span>
-        )}
-        <span>
-          {name}
-        </span>
+              <i className="italic">{releaseDate}</i>
+            </div>
+          )}
+          <div className="order-2 md:order-1 text-base text-secondary">
+            {name}
+          </div>
+        </div>
       </div>
     </Link>
   );
