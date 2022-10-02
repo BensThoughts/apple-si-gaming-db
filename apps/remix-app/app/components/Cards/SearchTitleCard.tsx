b379c6dbecf0bb2d52d7b3f315fd26d7b79ab1e6
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-02 10:03:22 -0700

    updated focus styles

--- apps/remix-app/app/components/Cards/SearchTitleCard.tsx
+++ apps/remix-app/app/components/Cards/SearchTitleCard.tsx
@@ -16,36 +16,34 @@ export default function SearchTitleCard({
   return (
     <Link
       to={`/apps/${steamAppId}/performance-posts`}
+      className="focus-visible:show-ring p-1 md:p-2 bg-primary hover:bg-primary-highlight rounded-lg
+      flex flex-col gap-2 items-center max-w-[460px]"
     >
-      <div className={`p-1 md:p-2 bg-primary hover:bg-primary-highlight rounded-lg
-                       flex flex-col gap-2 items-center max-w-[460px]
-                     `}>
-        {headerImageSrc && (
-          <div className="rounded-lg flex items-center justify-center max-w-[464px]">
-            <img
-              src={headerImageSrc}
-              alt={`Header for ${name}`}
-              width={460}
-              height={215}
-              className="rounded-md"
-              loading="lazy"
-              onError={(e) => {
-                e.currentTarget.src = '/svg-images/no-image-placeholder.svg';
-              }}
-            />
-          </div>
-        )}
-        <div className="flex flex-col flex-wrap justify-between w-full px-2">
-          {releaseDate && (
-            <span className="hidden md:inline text-sm">
-              Released:&nbsp;
-              <i className="italic">{releaseDate}</i>
-            </span>
-          )}
-          <span>
-            {name}
-          </span>
+      {headerImageSrc && (
+        <div className="rounded-lg flex items-center justify-center max-w-[464px]">
+          <img
+            src={headerImageSrc}
+            alt={`Header for ${name}`}
+            width={460}
+            height={215}
+            className="rounded-md"
+            loading="lazy"
+            onError={(e) => {
+              e.currentTarget.src = '/svg-images/no-image-placeholder.svg';
+            }}
+          />
         </div>
+      )}
+      <div className="flex flex-col flex-wrap justify-between w-full px-2">
+        {releaseDate && (
+          <span className="hidden md:inline text-sm">
+              Released:&nbsp;
+            <i className="italic">{releaseDate}</i>
+          </span>
+        )}
+        <span>
+          {name}
+        </span>
       </div>
     </Link>
   );
