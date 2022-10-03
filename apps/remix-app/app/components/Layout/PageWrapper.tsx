9ebeda10936a8517f66ef5d8d21191b30875bd5f
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-03 14:46:44 -0700

    added background waves component

--- apps/remix-app/app/components/Layout/PageWrapper.tsx
+++ apps/remix-app/app/components/Layout/PageWrapper.tsx
@@ -1,8 +1,34 @@
+import { useContext } from 'react';
+import { ThemeContext } from '~/lib/context/colorMode';
 import PageFooter from './PageFooter';
 import PageHeader from './PageHeader';
 import PageMain from './PageMain';
 
 type PageProps = { title?: string; } & React.HTMLAttributes<HTMLDivElement>
+
+function Background() {
+  const { colorMode } = useContext(ThemeContext);
+  if (colorMode === 'dark') {
+    return (
+      <>
+        <div
+          className="absolute top-0 bg-no-repeat bg-cover rotate-180 max-h-screen
+                     bg-[url('/svg-images/layered-waves-dark.svg')] aspect-[3/2]
+                     w-full -z-50"
+        />
+      </>
+    );
+  }
+  return (
+    <>
+      <div
+        className="absolute top-0 bg-no-repeat bg-cover rotate-180 max-h-screen
+                   bg-[url('/svg-images/layered-waves-light.svg')] aspect-[3/2]
+                   w-full -z-50"
+      />
+    </>
+  );
+}
 
 export default function PageWrapper({
   title,
@@ -12,9 +38,10 @@ export default function PageWrapper({
     <div className="flex flex-col justify-between h-[calc(100vh_-_3.5rem)]">
       <div>
         {title && <PageHeader title={title} titlePosition="left" className="mb-4 md:mb-8" />}
-        <PageMain className="mt-4 md:mt-8 mb-10">
+        <PageMain className="mt-4 md:mt-12 mb-10">
           {children}
         </PageMain>
+        <Background />
       </div>
       <PageFooter className="mt-10" />
     </div>
