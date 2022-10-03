b7fdbee6f3db00e984dc98d6cef4839d263e17b2
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-03 14:45:45 -0700

    fixed margin

--- apps/remix-app/app/components/Cards/AsideCard.tsx
+++ apps/remix-app/app/components/Cards/AsideCard.tsx
@@ -15,7 +15,7 @@ export default function AsideCard({
   const iconBg = iconBackground === 'primary' ? 'bg-app-bg' : 'bg-app-bg-secondary';
   return (
     <aside className={`relative px-6 py-8 border-l-[4px] border-solid border-secondary
-                       rounded-lg mt-2 bg-primary text-primary ${className}`} {...rest}>
+                       rounded-lg mt-5 bg-primary text-primary ${className}`} {...rest}>
       <div className={`rounded-[50%] absolute inset-0 w-[40px] h-[40px]
                        -translate-x-[50%] -translate-y-[50%] ${iconBg}`}>
         <InformationCircleIcon className="absolute inset-0 translate-x-[3px] translate-y-[3px] text-icon-secondary w-8 h-8" />
