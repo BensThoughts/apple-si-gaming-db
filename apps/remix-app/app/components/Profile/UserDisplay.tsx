aa845ea81c98fbcc08881ad0a61b1ddb4b948b39
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-03 14:47:06 -0700

    added bg color and border

--- apps/remix-app/app/components/Profile/UserDisplay.tsx
+++ apps/remix-app/app/components/Profile/UserDisplay.tsx
@@ -47,7 +47,7 @@ export default function UserDisplay({
 }: UserDisplayProps) {
   return (
     <div className="flex flex-col items-center w-full gap-10">
-      <div className="flex flex-col gap-6 items-center w-full">
+      <div className="flex flex-col gap-6 items-center w-full max-w-3xl p-4 rounded-lg bg-app-bg border-1 border-secondary-highlight">
         <Heading>Systems</Heading>
         <AsideCard title="System Information" iconBackground="primary" className="max-w-md">
           This is where you can define your system information. This is the
@@ -71,7 +71,7 @@ export default function UserDisplay({
           />
         </div>
       </div>
-      <div className="flex flex-col gap-3 items-center w-full">
+      <div className="flex flex-col gap-3 items-center w-full p-4 rounded-lg border-1 border-secondary-highlight">
         <Heading>Library</Heading>
         {(ownedApps && ownedApps.length > 0) ? (
           <div>
