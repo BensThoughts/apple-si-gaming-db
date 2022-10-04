0168e25bbc1c23f3f22c6072ff1952e4e27a780a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-03 17:24:18 -0700

    fixed icon background

--- apps/remix-app/app/components/Cards/AsideCard.tsx
+++ apps/remix-app/app/components/Cards/AsideCard.tsx
@@ -9,15 +9,14 @@ export default function AsideCard({
   ...rest
 }: {
   title: string;
-  iconBackground: 'primary' | 'secondary',
+  iconBackground: 'bg-primary' | 'bg-secondary' | 'bg-tertiary',
   children: React.ReactNode;
 } & React.HTMLAttributes<HTMLDivElement>) {
-  const iconBg = iconBackground === 'primary' ? 'bg-app-bg' : 'bg-app-bg-secondary';
   return (
     <aside className={`relative px-6 py-8 border-l-[4px] border-solid border-secondary
                        rounded-lg mt-5 bg-primary text-primary ${className}`} {...rest}>
       <div className={`rounded-[50%] absolute inset-0 w-[40px] h-[40px]
-                       -translate-x-[50%] -translate-y-[50%] ${iconBg}`}>
+                       -translate-x-[50%] -translate-y-[50%] ${iconBackground}`}>
         <InformationCircleIcon className="absolute inset-0 translate-x-[3px] translate-y-[3px] text-icon-secondary w-8 h-8" />
       </div>
       <strong className="font-bold">{title}</strong>
