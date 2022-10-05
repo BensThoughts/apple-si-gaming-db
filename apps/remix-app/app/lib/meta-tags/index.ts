834fc59db3eb113d1880bdd6e41fbed23f1b24c1
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-05 14:04:10 -0700

    created metaTags module

---
+++ apps/remix-app/app/lib/meta-tags/index.ts
@@ -0,0 +1,18 @@
+const TITLE = 'Apple Silicon on Steam DB';
+
+export const metaTags = {
+  'title': TITLE,
+  'og:image': 'https://res.cloudinary.com/bensthoughts/image/upload/v1664943160/apple-silicon-gaming-db/og-images/og-image-home_yxvlgi.png',
+  'og:url': 'https://apple-si-gaming-db-remix-app.fly.dev/',
+  'og:type': 'website',
+  'og:title': TITLE,
+  'og:description': 'Read and write performance reviews about Steam games running on Apple',
+  'twitter:card': 'summary_large_image',
+  'twitter:domain': 'apple-si-gaming-db-remix-app.fly.dev',
+  'twitter:url': 'https://apple-si-gaming-db-remix-app.fly.dev/',
+  'twitter:creator': '@bensthoughts',
+  'twitter:site': '@bensthoughts',
+  'twitter:title': TITLE,
+  'twitter:description': 'Read and write performance reviews about Steam games running on Apple',
+  'twitter:image': 'https://res.cloudinary.com/bensthoughts/image/upload/v1664943160/apple-silicon-gaming-db/og-images/og-image-home_yxvlgi.png',
+};
