004f9c7be6cf62aba5880789128336a8aab9e527
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-05 14:06:17 -0700

    linted/grammar

--- apps/remix-app/app/routes/index.tsx
+++ apps/remix-app/app/routes/index.tsx
@@ -26,11 +26,11 @@ export default function IndexRoute() {
           <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-20 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-20">
             <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
               <span className="block uppercase text-primary-highlight drop-shadow-md">
-                  Apple Silicon on Steam
+                Apple Silicon on Steam
               </span>
             </h1>
             <p className="mx-auto mt-6 max-w-lg text-center text-xl text-primary sm:max-w-3xl">
-                Write and read user experience reports about steam games running on apple silicon from users verified to own the game.
+                Read and write user experience reports about steam games running on apple silicon from users verified to own the game.
             </p>
           </div>
         </div>
@@ -59,9 +59,6 @@ export default function IndexRoute() {
           </div>
         </div>
       )}
-      <div>
-
-      </div>
     </PageWrapper>
   );
 }
