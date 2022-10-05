18a3af538bd597d48d8bb6bfdc750a656cc0dfce
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-04 17:29:06 -0700

    created TrendingSteamAppCard

---
+++ apps/remix-app/app/components/Cards/TrendingSteamAppCard.tsx
@@ -0,0 +1,39 @@
+import { Link } from '@remix-run/react';
+
+interface TrendingSteamAppProps {
+  steamAppId: number;
+  name: string;
+  headerImage: string | null;
+  releaseDate: string | null;
+  numNewPerformancePosts: number;
+}
+
+export default function TrendingSteamAppCard({
+  steamAppId,
+  name,
+  headerImage,
+  releaseDate,
+  numNewPerformancePosts,
+}: TrendingSteamAppProps) {
+  return (
+    <Link
+      to={`/apps/${steamAppId}/performance-posts`}
+      className="flex flex-col gap-2 items-center justify-center p-2 bg-tertiary
+                 border-1 border-secondary-highlight rounded-md hover:bg-tertiary-highlight
+                 focus-visible:show-ring focus-visible:bg-tertiary-highlight w-full
+                 max-w-md"
+    >
+      <div>
+        {name}
+        <span className="text-primary-faded">
+          {` - `}New Posts:{` `}
+        </span>
+        <span className="text-secondary">
+          {numNewPerformancePosts}
+        </span>
+      </div>
+      <div>
+      </div>
+    </Link>
+  );
+}
