68b4eb07c03b379f449fd3d0abb068632d78f298
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-05 14:03:09 -0700

    created AppLoadContextSteamUser interface

--- apps/remix-app/app/interfaces/AppLoadContext.ts
+++ apps/remix-app/app/interfaces/AppLoadContext.ts
@@ -1,5 +1,26 @@
-import type { SteamUserWithoutMetadata } from '@apple-si-gaming-db/database';
+export type AppLoadContextSteamUser = {
+  steamUserId: string;
+  displayName?: string | null;
+  communityVisibilityState?: number | null;
+  profileState?: number | null;
+  personaName?: string | null;
+  commentPermission?: number | null;
+  profileUrl?: string | null;
+  avatar?: string | null;
+  avatarMedium?: string | null;
+  avatarFull?: string | null;
+  avatarHash?: string | null;
+  lastLogoff?: number | null;
+  personaState?: number | null;
+  realName?: string | null;
+  primaryClanId?: string | null;
+  timeCreated?: number | null;
+  personaStateFlags?: number | null;
+  locCountryCode?: string | null;
+  locStateCode?: string | null;
+  locCityId?: number | null;
+}
 
 export type ExtendedAppLoadContext = {
-  'steamUser': SteamUserWithoutMetadata | null;
+  'steamUser': AppLoadContextSteamUser | null;
 }
