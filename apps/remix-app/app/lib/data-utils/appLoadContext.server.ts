4bcda86d94706503a86a19a3117cc386c0cf63a4
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-05 14:03:57 -0700

    using new AppLoadContext interfaces

--- apps/remix-app/app/lib/data-utils/appLoadContext.server.ts
+++ apps/remix-app/app/lib/data-utils/appLoadContext.server.ts
@@ -1,9 +1,8 @@
 import type { AppLoadContext } from '@remix-run/server-runtime';
-import type { SteamUser } from '~/interfaces/database';
-import type { ExtendedAppLoadContext } from '~/interfaces/AppLoadContext';
+import type { AppLoadContextSteamUser, ExtendedAppLoadContext } from '~/interfaces/AppLoadContext';
 
 export function extractAppLoadContext(context: AppLoadContext): ExtendedAppLoadContext {
-  const steamUser = context.steamUser ? context.steamUser as SteamUser : null;
+  const steamUser = context.steamUser ? context.steamUser as AppLoadContextSteamUser : null;
   return {
     steamUser,
   };
