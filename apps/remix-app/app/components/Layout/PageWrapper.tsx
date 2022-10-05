696229ff4100f3b9b843b73a178f1f4b8b6ecbe8
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-04 19:42:47 -0700

    clamped min/max height on background waves

--- apps/remix-app/app/components/Layout/PageWrapper.tsx
+++ apps/remix-app/app/components/Layout/PageWrapper.tsx
@@ -8,23 +8,16 @@ type PageProps = { title?: string; } & React.HTMLAttributes<HTMLDivElement>
 
 function Background() {
   const { colorMode } = useContext(ThemeContext);
+  let bgImageCss = `bg-[url('/svg-images/layered-waves-light.svg')]`;
   if (colorMode === 'dark') {
-    return (
-      <>
-        <div
-          className="absolute top-0 bg-no-repeat bg-cover rotate-180 max-h-screen
-                     bg-[url('/svg-images/layered-waves-dark.svg')] aspect-[3/2]
-                     w-full -z-50"
-        />
-      </>
-    );
+    bgImageCss = `bg-[url('/svg-images/layered-waves-dark.svg')]`;
   }
   return (
     <>
       <div
-        className="absolute top-0 bg-no-repeat bg-cover rotate-180 max-h-screen
-                   bg-[url('/svg-images/layered-waves-light.svg')] aspect-[3/2]
-                   w-full -z-50"
+        className={`absolute top-0 bg-no-repeat bg-cover
+                    aspect-[16/9] max-h-[calc(100vh_-_3.5rem)] min-h-[540px] w-full
+                    max-w-screen -z-50 ${bgImageCss}`}
       />
     </>
   );
@@ -36,7 +29,7 @@ export default function PageWrapper({
 }: PageProps) {
   return (
     <div className="flex flex-col justify-between h-[calc(100vh_-_3.5rem)]">
-      <div>
+      <div className="relative">
         {title && <PageHeader title={title} titlePosition="left" className="mb-4 md:mb-8" />}
         <PageMain className="mt-4 md:mt-12 mb-10">
           {children}
