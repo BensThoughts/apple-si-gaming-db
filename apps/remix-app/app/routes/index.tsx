a1aa727e38fb555066550b50b73915b879cb6fca
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-04 19:42:24 -0700

    changed padding

--- apps/remix-app/app/routes/index.tsx
+++ apps/remix-app/app/routes/index.tsx
@@ -23,7 +23,7 @@ export default function IndexRoute() {
     <PageWrapper>
       <div className="relative sm:flex sm:items-center sm:justify-center">
         <div className="mx-auto max-w-3xl sm:px-6 lg:px-8">
-          <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
+          <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-20 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-20">
             <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
               <span className="block uppercase text-primary-highlight drop-shadow-md">
                   Apple Silicon on Steam
