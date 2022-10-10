86958fe920fe051e4306e57077ec71fb58f72adb
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-10 01:16:53 -0700

    added label option to switch

--- apps/remix-app/app/components/FormComponents/ToggleSwitch/index.tsx
+++ apps/remix-app/app/components/FormComponents/ToggleSwitch/index.tsx
@@ -1,33 +1,54 @@
 import { Switch } from '@headlessui/react';
 
 export default function ToggleSwitch({
-  enabled,
+  checked,
+  name,
+  label,
+  labelPassive = true,
+  disabled,
   onChange,
 }: {
-  enabled: boolean;
+  checked: boolean;
+  name?: string;
+  label?: string;
+  labelPassive?: boolean;
+  disabled?: boolean;
   onChange: (e: boolean) => void,
 }) {
-  // const [enabled, setEnabled] = useState(false);
-
   return (
     <div>
-      <Switch
-        checked={enabled}
-        onChange={onChange}
-        className={`${enabled ? 'bg-secondary' : 'bg-primary'}
+      <Switch.Group>
+        {label &&
+        <div className="">
+          <Switch.Label passive={labelPassive}>
+            <span className="text-primary-highlight">
+              {label}{`: `}
+            </span>
+          </Switch.Label>
+        </div>
+        }
+        <div className="mt-1.5 mb-[2px]">
+          <Switch
+            checked={checked}
+            disabled={disabled}
+            name={name}
+            onChange={onChange}
+            className={`${checked ? 'bg-secondary' : 'bg-primary'}
           relative inline-flex items-center h-[38px] w-[74px] shrink-0 cursor-pointer
           rounded-full border-2 border-transparent transition-colors duration-200
           ease-in-out focus:outline-none focus-visible:ring-2
           focus-visible:ring-secondary focus-visible:ring-opacity-75`}
-      >
-        <span className="sr-only">Filter Apple Only Games</span>
-        <span
-          aria-hidden="true"
-          className={`${enabled ? 'translate-x-[38px]' : 'translate-x-[4px]'}
+          >
+            {/* <span className="sr-only">Filter Apple Only Games</span> */}
+            <span
+              aria-hidden="true"
+              className={`${checked ? 'translate-x-[38px] bg-primary-text-highlight' : 'translate-x-[4px] bg-primary-text'}
             pointer-events-none inline-block h-[30px] w-[30px] transform rounded-full
-            bg-primary-text-faded shadow-lg ring-0 transition duration-200 ease-in-out`}
-        />
-      </Switch>
+            shadow-lg ring-0 transition duration-200 ease-in-out`}
+            />
+          </Switch>
+        </div>
+      </Switch.Group>
     </div>
   );
 }
