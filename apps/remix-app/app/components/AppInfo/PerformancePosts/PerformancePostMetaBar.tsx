2c1c65ca74a752bc2ccb8a416cede4092ced4907
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-10 01:14:29 -0700

    added frame rate to meta bar

--- apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostMetaBar.tsx
+++ apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostMetaBar.tsx
@@ -1,37 +1,63 @@
-import type { RatingMedal } from '~/interfaces/database';
+import type { FrameRate, RatingMedal } from '~/interfaces/database';
 
 interface PerformancePostMetaBarProps {
   createdAt: Date;
   ratingMedal: RatingMedal;
+  frameRateAverage?: FrameRate | null;
+  frameRateStutters?: boolean | null;
 }
 
 function getRatingMedalText(ratingMedal: RatingMedal) {
   switch (ratingMedal) {
     case 'Platinum':
-      return 'Runs [ perfect ] - Platinum';
+      return 'Runs [ perfect ]';
     case 'Gold':
-      return 'Runs [ perfect after tweaks ] - Gold';
+      return 'Runs [ perfect after tweaks ]';
     case 'Silver':
-      return 'Runs [ with minor issues ] - Silver';
+      return 'Runs [ with minor issues ]';
     case 'Bronze':
-      return 'Runs [ often crashes ] - Bronze';
+      return 'Runs [ often crashes ]';
     case 'Borked':
-      return `Doesn't Run - Borked`;
+      return `Doesn't Run`;
+  }
+}
+
+function getFrameRateText(frameRateAverage: FrameRate) {
+  switch (frameRateAverage) {
+    case 'VeryLow':
+      return '<25 FPS';
+    case 'Low':
+      return '25 - 40 FPS';
+    case 'Medium':
+      return '40 - 60 FPS';
+    case 'High':
+      return '60 - 120 FPS';
+    case 'VeryHigh':
+      return '120+ FPS';
   }
 }
 
 export default function PerformancePostMetaBar({
   createdAt,
   ratingMedal,
+  frameRateAverage,
+  frameRateStutters,
 }: PerformancePostMetaBarProps) {
   return (
-    <div className="flex items-center justify-between bg-primary w-full text-primary-faded text-sm px-3 py-1 rounded-sm">
+    <div className="flex flex-col gap-1 md:gap-0 md:flex-row items-start md:items-center justify-between bg-primary w-full text-primary-faded text-sm px-3 py-1 rounded-sm">
+      <div className="flex flex-col gap-1 md:flex-row md:gap-2">
+        <div>
+          <span className="text-primary-highlight">{ratingMedal}</span> - {getRatingMedalText(ratingMedal)}
+        </div>
+        <div>
+          {/* {(frameRateAverage || frameRateStutters) && <span>Frame Rate: </span>} */}
+          {frameRateAverage && <span className="text-primary-highlight">{getFrameRateText(frameRateAverage)}</span>}
+          {frameRateStutters && <span className="text-primary-highlight">{frameRateStutters ? ' [ Stutters ]' : null }</span>}
+        </div>
+      </div>
       <i className="italic">
         {createdAt.toDateString()}
       </i>
-      <div>
-        {getRatingMedalText(ratingMedal)}
-      </div>
     </div>
   );
 }
