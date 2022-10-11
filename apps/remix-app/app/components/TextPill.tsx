7a27baf0553b58fb1e0a530332d77c6fd5a6d4d2
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-11 16:42:18 -0700

    added select-none and className prop

--- apps/remix-app/app/components/TextPill.tsx
+++ apps/remix-app/app/components/TextPill.tsx
@@ -1,11 +1,14 @@
 export default function TextPill({
   children,
+  className,
 }: {
   children: string,
+  className?: string,
 }) {
   return (
     <div className={`flex justify-center items-center py-1 px-2 text-xs
-                     rounded-md border-solid border-1 border-secondary`}>
+                     rounded-md border-solid border-1 border-secondary
+                     select-none ${className}`}>
       {children}
     </div>
   );
