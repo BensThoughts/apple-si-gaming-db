90925b9b40e6179e129c53760da7f57eda00778c
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-11 16:48:14 -0700

    added required * and changed label layout

--- apps/remix-app/app/components/FormComponents/TextArea/index.tsx
+++ apps/remix-app/app/components/FormComponents/TextArea/index.tsx
@@ -9,17 +9,34 @@ export default function TextArea({
   id,
   defaultValue,
   className,
+  required,
   ...rest
 }: TextAreaProps) {
   return (
     <div className="w-full max-w-lg">
       <label htmlFor={id}>
-        {labelText}:{fieldError && <span className="text-color-error">&nbsp;{fieldError}</span>}
+        <span className="text-primary">
+          {labelText}
+        </span>
+        {required &&
+          <span className="text-color-error">
+            *&nbsp;
+          </span>
+        }
+        {fieldError &&
+          <span className="text-primary">
+            {`: `}
+            <span className="text-color-error">
+              {fieldError}
+            </span>
+          </span>
+        }
       </label>
       <textarea
         id={id}
         defaultValue={defaultValue}
         className={`bg-primary rounded-lg p-2 w-full h-screen max-h-56 focus-visible:show-ring ${className}`}
+        required={required}
         {...rest}
       />
     </div>
