ebc388b215d382914a3d06d4512222cc29c171b7
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-20 14:21:11 -0700

    removed comments

--- apps/remix-app/app/components/Heading.tsx
+++ apps/remix-app/app/components/Heading.tsx
@@ -4,16 +4,7 @@ export default function Heading({
   children: string;
 }) {
   return (
-    <div
-      // className={`mt-4
-      // after:w-full after:max-w-[80px] after:h-[2px] after:block after:relative
-      // after:top-0 after:left-0 after:bg-secondary
-      // after:-translate-y-4 after:-translate-x-[110%]
-
-      // before:w-full before:max-w-[80px] before:h-[2px] before:block before:relative
-      // before:top-0 before:right-0 before:bg-secondary
-      // before:translate-y-4 before:translate-x-[110%]`}
-    >
+    <div>
       <h2 className="text-secondary text-2xl">{children}</h2>
     </div>
   );
