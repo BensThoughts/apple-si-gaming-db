731626e1c17a9b20f3a0f93cd717d1164471bfb1
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-23 11:41:16 -0700

    commented out backup-components

--- apps/remix-app/backup-components/PageWrapper.tsx
+++ apps/remix-app/backup-components/PageWrapper.tsx
@@ -1,42 +1,43 @@
-import { useContext } from 'react';
-import { ThemeContext } from '~/lib/context/colorMode';
-import PageFooter from '~/components/Layout/PageFooter';
-import PageHeader from '~/components/Layout/PageHeader';
-import PageMain from '~/components/Layout/PageMain';
+export {};
+// import { useContext } from 'react';
+// import { ThemeContext } from '~/lib/context/colorMode';
+// import PageFooter from '~/components/Layout/PageFooter';
+// import PageHeader from '~/components/Layout/PageHeader';
+// import PageMain from '~/components/Layout/PageMain';
 
-type PageProps = { title?: string; } & React.HTMLAttributes<HTMLDivElement>
+// type PageProps = { title?: string; } & React.HTMLAttributes<HTMLDivElement>
 
-function Background() {
-  const { colorMode } = useContext(ThemeContext);
-  let bgImageCss = `bg-[url('/svg-images/layered-waves-light.svg')]`;
-  if (colorMode === 'dark') {
-    bgImageCss = `bg-[url('/svg-images/layered-waves-dark.svg')]`;
-  }
-  return (
-    <>
-      <div
-        className={`absolute top-0 bg-no-repeat bg-cover
-                    max-h-[calc(100vh_-_3.5rem)] min-h-[540px] w-screen
-                    -z-50 ${bgImageCss} bg-cover`}
-      />
-    </>
-  );
-}
+// function Background() {
+//   const { colorMode } = useContext(ThemeContext);
+//   let bgImageCss = `bg-[url('/svg-images/layered-waves-light.svg')]`;
+//   if (colorMode === 'dark') {
+//     bgImageCss = `bg-[url('/svg-images/layered-waves-dark.svg')]`;
+//   }
+//   return (
+//     <>
+//       <div
+//         className={`absolute top-0 bg-no-repeat bg-cover
+//                     max-h-[calc(100vh_-_3.5rem)] min-h-[540px] w-screen
+//                     -z-50 ${bgImageCss} bg-cover`}
+//       />
+//     </>
+//   );
+// }
 
-export default function PageWrapper({
-  title,
-  children,
-}: PageProps) {
-  return (
-    <div className="flex flex-col justify-between h-[calc(100vh_-_3.5rem)]">
-      <div className="relative">
-        {title && <PageHeader title={title} titlePosition="left" className="mb-4 md:mb-12" />}
-        <PageMain>
-          {children}
-        </PageMain>
-        <Background />
-      </div>
-      <PageFooter className="mt-20" />
-    </div>
-  );
-}
+// export default function PageWrapper({
+//   title,
+//   children,
+// }: PageProps) {
+//   return (
+//     <div className="flex flex-col justify-between h-[calc(100vh_-_3.5rem)]">
+//       <div className="relative">
+//         {title && <PageHeader title={title} titlePosition="left" className="mb-4 md:mb-12" />}
+//         <PageMain>
+//           {children}
+//         </PageMain>
+//         <Background />
+//       </div>
+//       <PageFooter className="mt-20" />
+//     </div>
+//   );
+// }
--- apps/remix-app/backup-components/root.tsx
+++ apps/remix-app/backup-components/root.tsx
@@ -1,259 +1,260 @@
-import { Suspense, lazy, useEffect, useState } from 'react';
-import type {
-  LinksFunction,
-  LoaderArgs,
-  MetaFunction,
-} from '@remix-run/node';
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useCatch,
-  useLoaderData,
-  useTransition,
-} from '@remix-run/react';
-import { json } from '@remix-run/node';
-// import { getProfileSession, commitProfileSession } from './lib/sessions/cookie-sessions.server';
-import type { SteamGenre } from '~/interfaces/database';
-import tailwindStylesheetUrl from './styles/tailwind.css';
-import { metaTags } from '~/lib/meta-tags';
-import { extractAppLoadContext } from '~/lib/data-utils/appLoadContext.server';
-import Navbar from '~/components/Layout/Navbar';
-import { Toaster } from 'react-hot-toast';
-import { findUserProfileData, updateUserOwnedApps, upsertSteamUser } from '~/models/steamUser.server';
+export {};
+// import { Suspense, lazy, useEffect, useState } from 'react';
+// import type {
+//   LinksFunction,
+//   LoaderArgs,
+//   MetaFunction,
+// } from '@remix-run/node';
+// import {
+//   Links,
+//   LiveReload,
+//   Meta,
+//   Outlet,
+//   Scripts,
+//   ScrollRestoration,
+//   useCatch,
+//   useLoaderData,
+//   useTransition,
+// } from '@remix-run/react';
+// import { json } from '@remix-run/node';
+// // import { getProfileSession, commitProfileSession } from './lib/sessions/cookie-sessions.server';
+// import type { SteamGenre } from '~/interfaces/database';
+// import tailwindStylesheetUrl from './styles/tailwind.css';
+// import { metaTags } from '~/lib/meta-tags';
+// import { extractAppLoadContext } from '~/lib/data-utils/appLoadContext.server';
+// import Navbar from '~/components/Layout/Navbar';
+// import { Toaster } from 'react-hot-toast';
+// import { findUserProfileData, updateUserOwnedApps, upsertSteamUser } from '~/models/steamUser.server';
 
-import type { SerializeFrom } from '@remix-run/node';
-import { commitProfileSession, getProfileSession } from '~/lib/sessions/profile-session.server';
+// import type { SerializeFrom } from '@remix-run/node';
+// import { commitProfileSession, getProfileSession } from '~/lib/sessions/profile-session.server';
 
-const ThemeProvider = lazy(() => import('~/lib/context/colorMode'));
+// const ThemeProvider = lazy(() => import('~/lib/context/colorMode'));
 
-export function ClientOnly({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-  return mounted ? <>{children}</> : null;
-}
+// export function ClientOnly({ children }: { children: React.ReactNode }) {
+//   const [mounted, setMounted] = useState(false);
+//   useEffect(() => {
+//     setMounted(true);
+//   }, []);
+//   return mounted ? <>{children}</> : null;
+// }
 
-export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: tailwindStylesheetUrl }];
-};
+// export const links: LinksFunction = () => {
+//   return [{ rel: 'stylesheet', href: tailwindStylesheetUrl }];
+// };
 
-export const meta: MetaFunction = () => ({
-  'charset': 'utf-8',
-  'viewport': 'width=device-width,initial-scale=1',
-  ...metaTags,
-});
+// export const meta: MetaFunction = () => ({
+//   'charset': 'utf-8',
+//   'viewport': 'width=device-width,initial-scale=1',
+//   ...metaTags,
+// });
 
-type RootLoaderData = {
-  isLoggedIn: boolean;
-  contextData: {
-    steamUserId?: string | null;
-    displayName?: string | null;
-    avatarFull?: string | null,
-  };
-  prismaData?: {
-    ownedApps: {
-      steamAppId: number;
-      name: string;
-      headerImage: string | null;
-      platformMac: boolean | null;
-      genres: SteamGenre[];
-    }[],
-    systemSpecs: {
-      systemName: string;
-      manufacturer: string | null;
-      model: string | null;
-      cpuBrand: string | null;
-      osVersion: string | null;
-      videoDriver: string | null;
-      videoDriverVersion: string | null;
-      videoPrimaryVRAM: string | null;
-      memoryRAM: string | null;
-    }[],
-  }
-}
+// type RootLoaderData = {
+//   isLoggedIn: boolean;
+//   contextData: {
+//     steamUserId?: string | null;
+//     displayName?: string | null;
+//     avatarFull?: string | null,
+//   };
+//   prismaData?: {
+//     ownedApps: {
+//       steamAppId: number;
+//       name: string;
+//       headerImage: string | null;
+//       platformMac: boolean | null;
+//       genres: SteamGenre[];
+//     }[],
+//     systemSpecs: {
+//       systemName: string;
+//       manufacturer: string | null;
+//       model: string | null;
+//       cpuBrand: string | null;
+//       osVersion: string | null;
+//       videoDriver: string | null;
+//       videoDriverVersion: string | null;
+//       videoPrimaryVRAM: string | null;
+//       memoryRAM: string | null;
+//     }[],
+//   }
+// }
 
-export type SerializedRootLoaderData = SerializeFrom<RootLoaderData>
+// export type SerializedRootLoaderData = SerializeFrom<RootLoaderData>
 
-export async function loader({ request, context }: LoaderArgs) {
-  const { steamUser } = extractAppLoadContext(context);
-  const isLoggedIn = steamUser ? true : false;
-  const profileSession = await getProfileSession(
-      request.headers.get('Cookie'),
-  );
-  if (steamUser) {
-    const {
-      steamUserId,
-      displayName,
-      avatarFull,
-    } = steamUser;
-    // First time logging in need to add user to db
-    if (!profileSession.has('alreadyLoggedIn')) {
-      await upsertSteamUser(steamUser);
-      await updateUserOwnedApps(steamUser.steamUserId);
-      profileSession.set('alreadyLoggedIn', true);
-    }
-    const userProfile = await findUserProfileData(steamUser.steamUserId);
-    if (userProfile) {
-      const {
-        ownedApps,
-        systemSpecs,
-      } = userProfile;
-      // const systemNames = systemSpecs.map((systemSpec) => systemSpec.systemName);
-      return json<RootLoaderData>({
-        isLoggedIn,
-        contextData: {
-          steamUserId,
-          displayName,
-          avatarFull,
-        },
-        prismaData: {
-          ownedApps,
-          systemSpecs,
-        },
-      }, {
-        headers: {
-          'Set-Cookie': await commitProfileSession(profileSession),
-        },
-      });
-    } else {
-      return json<RootLoaderData>({
-        isLoggedIn,
-        contextData: {
-          steamUserId,
-          displayName,
-          avatarFull,
-        },
-      }, {
-        headers: {
-          'Set-Cookie': await commitProfileSession(profileSession),
-        },
-      });
-    }
-  }
+// export async function loader({ request, context }: LoaderArgs) {
+//   const { steamUser } = extractAppLoadContext(context);
+//   const isLoggedIn = steamUser ? true : false;
+//   const profileSession = await getProfileSession(
+//       request.headers.get('Cookie'),
+//   );
+//   if (steamUser) {
+//     const {
+//       steamUserId,
+//       displayName,
+//       avatarFull,
+//     } = steamUser;
+//     // First time logging in need to add user to db
+//     if (!profileSession.has('alreadyLoggedIn')) {
+//       await upsertSteamUser(steamUser);
+//       await updateUserOwnedApps(steamUser.steamUserId);
+//       profileSession.set('alreadyLoggedIn', true);
+//     }
+//     const userProfile = await findUserProfileData(steamUser.steamUserId);
+//     if (userProfile) {
+//       const {
+//         ownedApps,
+//         systemSpecs,
+//       } = userProfile;
+//       // const systemNames = systemSpecs.map((systemSpec) => systemSpec.systemName);
+//       return json<RootLoaderData>({
+//         isLoggedIn,
+//         contextData: {
+//           steamUserId,
+//           displayName,
+//           avatarFull,
+//         },
+//         prismaData: {
+//           ownedApps,
+//           systemSpecs,
+//         },
+//       }, {
+//         headers: {
+//           'Set-Cookie': await commitProfileSession(profileSession),
+//         },
+//       });
+//     } else {
+//       return json<RootLoaderData>({
+//         isLoggedIn,
+//         contextData: {
+//           steamUserId,
+//           displayName,
+//           avatarFull,
+//         },
+//       }, {
+//         headers: {
+//           'Set-Cookie': await commitProfileSession(profileSession),
+//         },
+//       });
+//     }
+//   }
 
-  return json<RootLoaderData>({
-    isLoggedIn,
-    contextData: {},
-  }, {
-    headers: {
-      'Set-Cookie': await commitProfileSession(profileSession),
-    },
-  });
-}
+//   return json<RootLoaderData>({
+//     isLoggedIn,
+//     contextData: {},
+//   }, {
+//     headers: {
+//       'Set-Cookie': await commitProfileSession(profileSession),
+//     },
+//   });
+// }
 
-function Document({
-  children,
-  title = 'Apple Silicon Gaming DB',
-  isLoggedIn,
-  isSearchSubmitting,
-}: {
-  children: React.ReactNode;
-  title?: string;
-  isLoggedIn?: boolean;
-  isSearchSubmitting?: boolean;
-}) {
-  const setInitialTheme = `
-  (function() {
-    function getInitialColorMode() {
-      const persistedColorPreference = window.localStorage.getItem('color-mode');
-      const hasPersistedPreference = typeof persistedColorPreference === 'string';
+// function Document({
+//   children,
+//   title = 'Apple Silicon Gaming DB',
+//   isLoggedIn,
+//   isSearchSubmitting,
+// }: {
+//   children: React.ReactNode;
+//   title?: string;
+//   isLoggedIn?: boolean;
+//   isSearchSubmitting?: boolean;
+// }) {
+//   const setInitialTheme = `
+//   (function() {
+//     function getInitialColorMode() {
+//       const persistedColorPreference = window.localStorage.getItem('color-mode');
+//       const hasPersistedPreference = typeof persistedColorPreference === 'string';
 
-      if (hasPersistedPreference) {
-        return persistedColorPreference;
-      }
+//       if (hasPersistedPreference) {
+//         return persistedColorPreference;
+//       }
 
-      const mql = window.matchMedia('(prefers-color-scheme: dark)');
-      const hasMediaQueryPreference = typeof mql.matches === 'boolean';
-      if (hasMediaQueryPreference) {
-        return mql.matches ? 'dark' : 'light';
-      }
+//       const mql = window.matchMedia('(prefers-color-scheme: dark)');
+//       const hasMediaQueryPreference = typeof mql.matches === 'boolean';
+//       if (hasMediaQueryPreference) {
+//         return mql.matches ? 'dark' : 'light';
+//       }
 
-      return 'light';
-    }
+//       return 'light';
+//     }
 
-    const colorMode = getInitialColorMode();
-    document.body.dataset.theme = colorMode;
-  })()`;
+//     const colorMode = getInitialColorMode();
+//     document.body.dataset.theme = colorMode;
+//   })()`;
 
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        {title ? <title>title</title> : null}
-        <Links />
-      </head>
-      <body className="min-h-screen bg-app-bg">
-        <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
-        <ClientOnly>
-          <Suspense>
-            <ThemeProvider>
-              <Navbar
-                isLoggedIn={isLoggedIn ? isLoggedIn : false}
-                isSearchSubmitting={isSearchSubmitting ? isSearchSubmitting : false}
-                className="h-14"
-              />
-              <div className="absolute top-14 w-full z-10">
-                {children}
-              </div>
-            </ThemeProvider>
-            <Toaster />
-          </Suspense>
-        </ClientOnly>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
+//   return (
+//     <html lang="en">
+//       <head>
+//         <Meta />
+//         {title ? <title>title</title> : null}
+//         <Links />
+//       </head>
+//       <body className="min-h-screen bg-app-bg">
+//         <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
+//         <ClientOnly>
+//           <Suspense>
+//             <ThemeProvider>
+//               <Navbar
+//                 isLoggedIn={isLoggedIn ? isLoggedIn : false}
+//                 isSearchSubmitting={isSearchSubmitting ? isSearchSubmitting : false}
+//                 className="h-14"
+//               />
+//               <div className="absolute top-14 w-full z-10">
+//                 {children}
+//               </div>
+//             </ThemeProvider>
+//             <Toaster />
+//           </Suspense>
+//         </ClientOnly>
+//         <ScrollRestoration />
+//         <Scripts />
+//         <LiveReload />
+//       </body>
+//     </html>
+//   );
+// }
 
-export default function App() {
-  // TODO: Getting cannot use loaderData in an error boundary errors,
-  // TODO: with error being thrown on /profile, prob. because of this
-  const { isLoggedIn }= useLoaderData<RootLoaderData>();
-  const transition = useTransition();
-  const isSearchSubmitting =
-    transition.state === 'submitting' &&
-    transition.location.pathname === '/search';
-  return (
-    <Document
-      isLoggedIn={isLoggedIn}
-      isSearchSubmitting={isSearchSubmitting}
-    >
-      <Outlet />
-    </Document>
-  );
-}
+// export default function App() {
+//   // TODO: Getting cannot use loaderData in an error boundary errors,
+//   // TODO: with error being thrown on /profile, prob. because of this
+//   const { isLoggedIn }= useLoaderData<RootLoaderData>();
+//   const transition = useTransition();
+//   const isSearchSubmitting =
+//     transition.state === 'submitting' &&
+//     transition.location.pathname === '/search';
+//   return (
+//     <Document
+//       isLoggedIn={isLoggedIn}
+//       isSearchSubmitting={isSearchSubmitting}
+//     >
+//       <Outlet />
+//     </Document>
+//   );
+// }
 
-export function ErrorBoundary({ error }: { error: Error }) {
-  return (
-    <Document title="Error">
-      <div>
-        <h1>App Error</h1>
-        <pre>{error.message}</pre>
-      </div>
-    </Document>
-  );
-}
+// export function ErrorBoundary({ error }: { error: Error }) {
+//   return (
+//     <Document title="Error">
+//       <div>
+//         <h1>App Error</h1>
+//         <pre>{error.message}</pre>
+//       </div>
+//     </Document>
+//   );
+// }
 
-export function CatchBoundary() {
-  const caught = useCatch();
-  return (
-    <Document title="Oops!">
-      <div>
-        <h1>Oops! - {caught.status} {caught.statusText}</h1>
-        {caught.status === 404 && (
-          <img
-            src="/svg-images/four-oh-four-error.svg"
-            alt="Four oh four error"
-          />
-        )}
-      </div>
-    </Document>
-  );
-}
+// export function CatchBoundary() {
+//   const caught = useCatch();
+//   return (
+//     <Document title="Oops!">
+//       <div>
+//         <h1>Oops! - {caught.status} {caught.statusText}</h1>
+//         {caught.status === 404 && (
+//           <img
+//             src="/svg-images/four-oh-four-error.svg"
+//             alt="Four oh four error"
+//           />
+//         )}
+//       </div>
+//     </Document>
+//   );
+// }
