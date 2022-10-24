c69bc8286f4c0db563e7efeeb3c6db826be045e7
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-24 11:05:10 -0700

    added splat route for 404 page

---
+++ apps/remix-app/app/routes/$.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from '~/components/Layout/PageWrapper';
+
+export default function FourOhFourSplatRoute() {
+  return (
+    <PageWrapper>
+      <div className="flex w-full h-[calc(100vh_-_14rem)] items-center justify-center">
+        <h1 className="text-4xl md:text-6xl text-primary-highlight">Oops! - 404 Error</h1>
+      </div>
+    </PageWrapper>
+  );
+}
