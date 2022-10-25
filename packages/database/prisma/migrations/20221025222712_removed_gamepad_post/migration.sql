4fe34bf4baf3a6808471f532f285831e69a6907a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-25 15:53:35 -0700

    removed SteamGamepadPost

---
+++ packages/database/prisma/migrations/20221025222712_removed_gamepad_post/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `SteamGamepadPost` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `_PostTagToSteamGamepadPost` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "SteamGamepadPost";
+
+-- DropTable
+DROP TABLE "_PostTagToSteamGamepadPost";
