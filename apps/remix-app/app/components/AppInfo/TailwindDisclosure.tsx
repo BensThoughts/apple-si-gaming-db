bc4f7905c0e2a29883ba8cf8a393d513f19d65c2
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-10-24 21:15:53 -0700

    added useEffect to control disclosure open on isWide

--- apps/remix-app/app/components/AppInfo/TailwindDisclosure.tsx
+++ apps/remix-app/app/components/AppInfo/TailwindDisclosure.tsx
@@ -1,4 +1,5 @@
 import { Disclosure } from '@headlessui/react';
+import { useEffect, useState } from 'react';
 import { ChevronUpIcon } from '~/components/Icons';
 import { useMediaIsWide } from '~/lib/hooks/useMedia';
 
@@ -10,30 +11,39 @@ export default function TailwindDisclosure({
   children: React.ReactNode;
 }) {
   const { isWide } = useMediaIsWide();
+  const [isOpen, setIsOpen] = useState(true);
+
+  // TODO: Maybe remove this useEffect and isWide
+  // it causes potential pop (is there a way for the server
+  // to know if screen is wide? or at least mobile?)
+  useEffect(() => {
+    setIsOpen(isWide);
+  }, [isWide]);
+
   return (
-    <Disclosure defaultOpen={isWide}>
-      {({ open }) => (
-        <>
-          <Disclosure.Button
-            className={`peer flex w-full justify-between px-4 py-2 text-left text-sm
+    <Disclosure>
+      <Disclosure.Button
+        onClick={() => setIsOpen(!isOpen)}
+        className={`peer flex w-full justify-between px-4 py-2 text-left text-sm
                         font-medium focus-visible:show-ring transition-colors
                         bg-primary hover:bg-primary-highlight
-                        ${open ? `rounded-t-lg border-t-secondary border-x-secondary
+                        ${isOpen ? `rounded-t-lg border-t-secondary border-x-secondary
                                   border-x-1 border-t-1`
                                : 'rounded-lg border-secondary border-1'}`}
-          >
-            <span>{title}</span>
-            <ChevronUpIcon
-              className={`${open ? 'rotate-180 transform' : 'rotate-0 transform'} h-5 w-5 text-primary`}
-            />
-          </Disclosure.Button>
-          <Disclosure.Panel
-            className={`peer-focus-visible:show-ring border-secondary border-x-1 border-b-1 p-3 bg-tertiary rounded-b-md`}
-          >
-            {children}
-          </Disclosure.Panel>
-        </>
-      )}
+      >
+        <span>{title}</span>
+        <ChevronUpIcon
+          className={`${isOpen ? 'rotate-180 transform' : 'rotate-0 transform'} h-5 w-5 text-primary`}
+        />
+      </Disclosure.Button>
+      {isOpen &&
+        <Disclosure.Panel
+          static
+          className={`peer-focus-visible:show-ring border-secondary border-x-1 border-b-1 p-3 bg-tertiary rounded-b-md`}
+        >
+          {children}
+        </Disclosure.Panel>
+      }
     </Disclosure>
   );
 }
