e0c411bf3a5c54bef06645107ce65a2c79e237f0
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-11-04 07:57:32 -0700

    created rating conversion functions

---
+++ apps/remix-app/app/lib/rating-conversions/index.ts
@@ -0,0 +1,155 @@
+import type {
+  RatingMedal,
+  FrameRate,
+  GamepadRating,
+} from '~/interfaces/database';
+
+export function convertRatingMedalToText(ratingMedal: RatingMedal) {
+  switch (ratingMedal) {
+    case 'Platinum':
+      return 'Runs [ perfect ]';
+    case 'Gold':
+      return 'Runs [ perfect after tweaks ]';
+    case 'Silver':
+      return 'Runs [ with minor issues ]';
+    case 'Bronze':
+      return 'Runs [ often crashes ]';
+    case 'Borked':
+      return `Doesn't Run`;
+  }
+}
+
+export function convertFrameRateToText(frameRateAverage: FrameRate) {
+  switch (frameRateAverage) {
+    case 'VeryLow':
+      return '<25 FPS';
+    case 'Low':
+      return '25 - 40 FPS';
+    case 'Medium':
+      return '40 - 60 FPS';
+    case 'High':
+      return '60 - 120 FPS';
+    case 'VeryHigh':
+      return '120+ FPS';
+  }
+}
+
+export function convertGamepadRatingToText(gamepadRating: GamepadRating) {
+  switch (gamepadRating) {
+    case 'GamepadBorked':
+      return `Doesn't work`;
+    case 'GamepadBronze':
+      return `Works with major issues`;
+    case 'GamepadSilver':
+      return `Works with minor issues`;
+    case 'GamepadGold':
+      return `Works perfect after tweaks`;
+    case 'GamepadPlatinum':
+      return `Works perfect`;
+  }
+}
+
+export function convertGamepadRatingToMedalText(gamepadRating: GamepadRating) {
+  switch (gamepadRating) {
+    case 'GamepadBorked':
+      return `Borked`;
+    case 'GamepadBronze':
+      return `Bronze`;
+    case 'GamepadSilver':
+      return `Silver`;
+    case 'GamepadGold':
+      return `Gold`;
+    case 'GamepadPlatinum':
+      return `Platinum`;
+  }
+}
+
+export function convertRatingMedalToNumber(ratingMedal: RatingMedal) {
+  switch (ratingMedal) {
+    case 'Borked':
+      return 0;
+    case 'Bronze':
+      return 1;
+    case 'Silver':
+      return 2;
+    case 'Gold':
+      return 3;
+    case 'Platinum':
+      return 4;
+  }
+}
+
+export function convertNumberToRatingMedal(ratingMedalNumber: 0 | 1 | 2 | 3 | 4): RatingMedal {
+  switch (ratingMedalNumber) {
+    case 0:
+      return 'Borked';
+    case 1:
+      return 'Bronze';
+    case 2:
+      return 'Silver';
+    case 3:
+      return 'Gold';
+    case 4:
+      return 'Platinum';
+  }
+}
+
+export function convertFrameRateToNumber(frameRate: FrameRate) {
+  switch (frameRate) {
+    case 'VeryLow':
+      return 0;
+    case 'Low':
+      return 1;
+    case 'Medium':
+      return 2;
+    case 'High':
+      return 3;
+    case 'VeryHigh':
+      return 4;
+  }
+}
+
+export function convertNumberToFrameRate(frameRateNumber: 0 | 1 | 2 | 3 | 4): FrameRate {
+  switch (frameRateNumber) {
+    case 0:
+      return 'VeryLow';
+    case 1:
+      return 'Low';
+    case 2:
+      return 'Medium';
+    case 3:
+      return 'High';
+    case 4:
+      return 'VeryHigh';
+  }
+}
+
+export function convertGamepadRatingToNumber(gamepadRating: GamepadRating) {
+  switch (gamepadRating) {
+    case 'GamepadBorked':
+      return 0;
+    case 'GamepadBronze':
+      return 1;
+    case 'GamepadSilver':
+      return 2;
+    case 'GamepadGold':
+      return 3;
+    case 'GamepadPlatinum':
+      return 4;
+  }
+}
+
+export function convertNumberToGamepadRating(gamepadRatingNumber: 0 | 1 | 2 | 3 | 4): GamepadRating {
+  switch (gamepadRatingNumber) {
+    case 0:
+      return 'GamepadBorked';
+    case 1:
+      return 'GamepadBronze';
+    case 2:
+      return 'GamepadSilver';
+    case 3:
+      return 'GamepadGold';
+    case 4:
+      return 'GamepadPlatinum';
+  }
+}
