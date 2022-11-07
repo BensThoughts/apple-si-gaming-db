533bf74cc80b9e9435dc11000f3cb12f60fb0d23
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-11-07 13:56:42 -0700

    added page layout for /apps index route

--- apps/remix-app/app/routes/apps/index.tsx
+++ apps/remix-app/app/routes/apps/index.tsx
@@ -1,16 +1,110 @@
-import type { LoaderArgs } from '@remix-run/node';
-import { redirect } from '@remix-run/node';
-import { useCatch } from '@remix-run/react';
+import { json } from '@remix-run/node';
+import { useCatch, useLoaderData } from '@remix-run/react';
 import PageWrapper from '~/components/Layout/PageWrapper';
-import { validateSteamAppId } from '~/lib/loader-functions/params-validators.server';
+import { findTrendingSteamApps } from '~/models/steamApp.server';
+import { findNewestPerformancePosts } from '~/models/steamPerformancePost.server';
+import type { TrendingSteamApp } from '~/models/steamApp.server';
+import type { RatingMedal } from '~/interfaces/database';
+import { Fragment } from 'react';
+import NewPerformancePostCard from '~/components/Cards/NewPerformancePostCard';
+import TrendingSteamAppCard from '~/components/Cards/TrendingSteamAppCard';
 
-export async function loader({ params }: LoaderArgs) {
-  const steamAppId = validateSteamAppId(params);
-  return redirect(`/apps/${steamAppId}/performance-posts`);
+interface LoaderData {
+  trendingSteamApps: TrendingSteamApp[];
+  newPerformancePosts: {
+    id: string;
+    steamAppId: number;
+    steamApp: {
+      name: string;
+    };
+    postText: string;
+    displayName: string | null;
+    avatarMedium: string | null;
+    ratingMedal: RatingMedal;
+  }[]
+}
+
+export async function loader() {
+  const NUM_TRENDING_APPS = 10;
+  const NUM_RECENT_POSTS = 15;
+  const trendingSteamApps = await findTrendingSteamApps(NUM_TRENDING_APPS);
+  const newPerformancePosts = await findNewestPerformancePosts(NUM_RECENT_POSTS);
+  return json<LoaderData>({
+    trendingSteamApps,
+    newPerformancePosts,
+  });
 }
 
 export default function SteamAppIdIndexRoute() {
-  return null;
+  const { trendingSteamApps, newPerformancePosts } = useLoaderData<typeof loader>();
+  return (
+    <PageWrapper currentRoute="/apps">
+      <div className="flex flex-col items-center gap-12 w-full mt-6">
+        {(trendingSteamApps.length > 0) && (
+          <div className="flex flex-col items-center gap-6 px-6 py-8 bg-tertiary rounded-lg shadow-md w-full max-w-2xl">
+            <h2 className="text-secondary text-2xl">Top 10 Trending Apps</h2>
+            <div className="flex flex-col items-center gap-4 w-full">
+              {trendingSteamApps.map(({
+                steamAppId,
+                name,
+                headerImage,
+                releaseDate,
+                _count,
+              }) => (
+                <Fragment key={steamAppId}>
+                  <TrendingSteamAppCard
+                    steamAppId={steamAppId}
+                    name={name}
+                    headerImage={headerImage}
+                    releaseDate={releaseDate}
+                    numNewPerformancePosts={_count.performancePosts}
+                  />
+                </Fragment>
+              ))}
+            </div>
+          </div>
+        )}
+        {(newPerformancePosts.length > 0) && (
+          <div className="flex flex-col items-center gap-6 px-6 py-8 bg-tertiary rounded-lg shadow-md w-full max-w-2xl">
+            <h2 className="text-secondary text-2xl">Top 15 New Posts</h2>
+            <div className="w-full flex flex-col items-center gap-4">
+              {newPerformancePosts.map(({
+                id,
+                steamAppId,
+                steamApp,
+                postText,
+                displayName,
+                avatarMedium,
+                ratingMedal,
+              }) => (
+                <Fragment key={id}>
+                  <NewPerformancePostCard
+                    steamAppId={steamAppId}
+                    steamApp={steamApp}
+                    postText={postText}
+                    displayName={displayName}
+                    avatarMedium={avatarMedium}
+                    ratingMedal={ratingMedal}
+                  />
+                </Fragment>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </PageWrapper>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <PageWrapper currentRoute="/apps" topSpacer>
+      <div>
+        <h1>Error in /apps route</h1>
+        <div>{error.message}</div>
+      </div>
+    </PageWrapper>
+  );
 }
 
 export function CatchBoundary() {
