23b15553f4885aa2f6ab39c96e06c3dee55199bf
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-11-07 12:29:31 -0700

    moved new domain banner to PageWrapper

--- apps/remix-app/app/components/Layout/PageWrapper/index.tsx
+++ apps/remix-app/app/components/Layout/PageWrapper/index.tsx
@@ -1,4 +1,6 @@
+import NewDomainBannerAlert from '~/components/Banners/NewDomainBannerAlert';
 import { useTheme } from '~/lib/context/theme-provider';
+import { useBannerData } from '~/lib/hooks/useMatchesData';
 import PageFooter from './PageFooter';
 import PageHeader from './PageHeader';
 import PageMain from './PageMain';
@@ -22,19 +24,31 @@ function Background() {
 }
 
 type PageProps = {
+  currentRoute: string;
   title?: string;
   topSpacer?: boolean;
 } & React.HTMLAttributes<HTMLDivElement>
 
 export default function PageWrapper({
+  currentRoute,
   title,
   topSpacer = false,
   children,
 }: PageProps) {
+  const bannerData = useBannerData();
+  const showNewDomainBanner = bannerData ? bannerData.showNewDomainBanner : true;
+
   return (
     <div className="flex flex-col justify-between h-[calc(100vh_-_3.5rem)]">
       <div className="relative">
         {title && <PageHeader title={title} titlePosition="left" />}
+        <div className="px-4">
+          <NewDomainBannerAlert
+            showBanner={showNewDomainBanner}
+            redirectTo={currentRoute}
+          />
+        </div>
+
         {topSpacer && <TopSpacer />}
         <PageMain>
           {children}
