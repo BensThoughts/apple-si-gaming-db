947ba1a5a574120cacfc85be69c904068375dde4
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-11-28 17:23:53 -0700

    added PrivacyCard

---
+++ apps/remix-app/app/components/Profile/Login/PrivacyCard.tsx
@@ -0,0 +1,21 @@
+import AsideCard from '~/components/Cards/AsideCard';
+import ExternalLink from '~/components/ExternalLink';
+
+export default function PrivacyCard() {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <AsideCard title="Note" iconBackground="bg-tertiary" className="max-w-md">
+    Within your&nbsp;
+        <ExternalLink
+          href="https://steamcommunity.com/my/edit/settings"
+          className="underline-offset-2"
+        >
+      Steam Privacy Settings
+        </ExternalLink>
+    , &apos;My profile&apos; and &apos;Game Details&apos;
+    must be set to public to synchronize your Steam library
+    with this site.
+      </AsideCard>
+    </div>
+  );
+}
