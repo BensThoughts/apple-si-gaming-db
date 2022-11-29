e602ea3b3325094f00afd86285ab94a76a3256e9
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-11-28 18:43:26 -0700

    added temp AchievementLayout

---
+++ apps/remix-app/app/components/Profile/Achievements/AchievementsLayout.tsx
@@ -0,0 +1,10 @@
+import Heading from '~/components/Heading';
+
+export default function AchievementsLayout() {
+  return (
+    <div className="flex flex-col gap-8 items-center w-full p-4 bg-tertiary rounded-lg border-1 border-secondary-highlight">
+      <Heading>Achievements</Heading>
+        Earn achievements by posting on the site. Coming Soon!
+    </div>
+  );
+}
