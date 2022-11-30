e814b6af19a6e41664c129e7e62d68f0eeec7352
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-11-29 18:29:13 -0700

    extracted Create System instructions

---
+++ apps/remix-app/app/components/Profile/Systems/CreateSystemInstructions.tsx
@@ -0,0 +1,28 @@
+import AsideCard from '~/components/Cards/AsideCard';
+
+export default function CreateSystemInstructions() {
+  return (
+    <AsideCard title="How To Create a System" iconBackground="bg-tertiary" className="max-w-md">
+      <div className="flex flex-col gap-3">
+        <div>
+          Copy and paste your system information directly from Steam
+          to create a system
+        </div>
+        <ul className="list-disc ml-6">
+          <li>
+            In Steam go to the help menu
+          </li>
+          <li>
+            Select "System Information"
+          </li>
+          <li>
+            Right click and select "copy all text to clipboard"
+          </li>
+          <li>
+            Paste that into the system info field
+          </li>
+        </ul>
+      </div>
+    </AsideCard>
+  );
+}
