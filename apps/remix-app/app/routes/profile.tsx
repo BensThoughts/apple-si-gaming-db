85ab3cf08466c1e6ad469f88bf9f5bdd8bbdd48e
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-11-29 18:29:53 -0700

    fixed center alignment on md screens for /profile menu

--- apps/remix-app/app/routes/profile.tsx
+++ apps/remix-app/app/routes/profile.tsx
@@ -96,8 +96,8 @@ export default function ProfilePage() {
       <PageWrapper currentRoute="/profile" title="Profile" topSpacer>
         <div className="grid grid-cols-1 md:grid-cols-12 gap-4 min-h-screen">
 
-          <div className="col-start-1 col-span-1 md:col-span-4 xl:col-span-3">
-            <div className="flex flex-col gap-2 items-center w-full max-w-xs bg-tertiary rounded-md p-4">
+          <div className="col-start-1 col-span-1 justify-items-center md:col-span-4 xl:col-span-3">
+            <div className="flex flex-col gap-2 items-center w-full md:max-w-xs bg-tertiary rounded-md p-4">
               <h2 className="text-lg text-secondary">Menu</h2>
               <Link to="/profile" className="focus-visible:show-ring px-1 rounded-sm font-medium text-primary">
                 <AnimatedUnderline>
