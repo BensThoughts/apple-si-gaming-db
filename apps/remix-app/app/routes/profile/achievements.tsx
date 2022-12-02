89c43b1d76795287b418db002ab308b4d8551ca9
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-02 06:15:16 -0700

    added logic to load user achievements

--- apps/remix-app/app/routes/profile/achievements.tsx
+++ apps/remix-app/app/routes/profile/achievements.tsx
@@ -1,7 +1,41 @@
+import type { LoaderArgs } from '@remix-run/node';
+import { redirect, json } from '@remix-run/node';
+import type { SiteAchievement } from '~/interfaces/database';
 import AchievementsLayout from '~/components/Profile/Achievements/AchievementsLayout';
+import { extractAppLoadContext } from '~/lib/data-utils/appLoadContext.server';
+import { findSiteAchievementsForSteamUser } from '~/models/siteAchievements';
+import { useLoaderData } from '@remix-run/react';
+
+type ProfileAchievementsLoaderData = {
+  siteAchievements: {
+    achievementId: SiteAchievement['achievementId'];
+    title: SiteAchievement['title'];
+    description: SiteAchievement['description'];
+    imageUrl: SiteAchievement['imageUrl'];
+    svgCode: SiteAchievement['svgCode'];
+  }[]
+}
+
+export async function loader({ context }: LoaderArgs) {
+  const { steamUser } = extractAppLoadContext(context);
+  if (!steamUser) {
+    return redirect('/profile');
+  }
+  const userData = await findSiteAchievementsForSteamUser(steamUser.steamUserId);
+  if (!userData) {
+    return json<ProfileAchievementsLoaderData>({
+      siteAchievements: [],
+    });
+  }
+  const { siteAchievements } = userData;
+  return json<ProfileAchievementsLoaderData>({
+    siteAchievements,
+  });
+}
 
 export default function ProfileAchievementsRoute() {
+  const { siteAchievements } = useLoaderData<ProfileAchievementsLoaderData>();
   return (
-    <AchievementsLayout />
+    <AchievementsLayout siteAchievements={siteAchievements} />
   );
 }
