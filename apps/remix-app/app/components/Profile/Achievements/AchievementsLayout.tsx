6f700191bd27db0054e1cc3722da558d5049695e
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-02 06:13:49 -0700

    started work on achievement profile page

--- apps/remix-app/app/components/Profile/Achievements/AchievementsLayout.tsx
+++ apps/remix-app/app/components/Profile/Achievements/AchievementsLayout.tsx
@@ -1,10 +1,46 @@
 import Heading from '~/components/Heading';
 
-export default function AchievementsLayout() {
+interface AchievementsLayoutProps {
+  siteAchievements: {
+    achievementId: number;
+    title: string;
+    description: string;
+    imageUrl: string;
+    svgCode: string;
+  }[]
+}
+
+export default function AchievementsLayout({
+  siteAchievements,
+}: AchievementsLayoutProps) {
   return (
     <div className="flex flex-col gap-8 items-center w-full p-4 bg-tertiary rounded-lg border-1 border-secondary-highlight">
       <Heading>Achievements</Heading>
-        Earn achievements by posting on the site. Coming Soon!
+       Earn achievements by posting on the site. Coming Soon!
+      {/*
+      {siteAchievements.map(({
+        achievementId,
+        title,
+        description,
+        imageUrl,
+        svgCode,
+      }) => (
+        <div key={achievementId}>
+          <div className="text-secondary">
+            <div className="" />
+            <div className="text-secondary stroke-[0.5]">
+              <div dangerouslySetInnerHTML={{ __html: svgCode }} />
+              <img
+                src={imageUrl}
+                alt={title}
+                className="text-secondary stroke-secondary fill-secondary"
+              />
+            </div>
+          </div>
+          {title} - {description}
+        </div>
+      ))}
+    */}
     </div>
   );
 }
