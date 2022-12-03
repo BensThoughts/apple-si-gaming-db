ca290ab844cb56c98d2ac8a919e5f5da249f05ec
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-03 13:36:13 -0700

    added rounded-full to underline after bar

--- apps/remix-app/app/components/AnimatedUnderline.tsx
+++ apps/remix-app/app/components/AnimatedUnderline.tsx
@@ -10,7 +10,7 @@ export default function AnimatedUnderline({
       className={`relative px-0 py-[0.1em] overflow-hidden no-underline
                   after:block after:absolute after:bottom-[-3px] after:left-0
                   after:w-0 after:h-[0.2em] after:bg-secondary after:transition-transform
-                  after:hover:w-full text-primary-highlight ${className}`}
+                  after:hover:w-full after:rounded-full text-primary-highlight ${className}`}
       {...rest}
     >
       {children}
