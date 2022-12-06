0ea98c2c131341c5309c3f83444501e527dbe36f
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-06 13:20:23 -0700

    updated to use like data and show like button

--- apps/remix-app/app/components/Profile/Posts/UsersPostsLayout.tsx
+++ apps/remix-app/app/components/Profile/Posts/UsersPostsLayout.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@remix-run/react';
 import AppHeaderImage from '~/components/ImageWrappers/AppHeaderImage';
 import PerformancePostMetaBar from '~/components/AppInfo/PerformancePosts/PerformancePostMetaBar';
 import Heading from '~/components/Heading';
@@ -6,10 +5,13 @@ import type { RatingMedal, FrameRate } from '~/interfaces/database';
 import RemixUnderlineLink from '~/components/RemixUnderlineLink';
 
 interface UsersPostsLayoutProps {
+  isUserLoggedIn: boolean;
+  likedPerformancePostIds: string[];
   steamUsersPosts: {
     id: string;
     steamAppId: number;
     createdAt: Date;
+    numLikes: number;
     ratingMedal: RatingMedal;
     frameRateAverage?: FrameRate | null;
     frameRateStutters?: boolean | null;
@@ -22,22 +24,25 @@ interface UsersPostsLayoutProps {
 }
 
 export default function UsersPostsLayout({
+  isUserLoggedIn,
+  likedPerformancePostIds,
   steamUsersPosts,
 }: UsersPostsLayoutProps) {
   if (steamUsersPosts.length < 1) {
     return (
       <div className="flex flex-col gap-8 items-center w-full p-4 bg-tertiary rounded-lg border-1 border-secondary-highlight">
-        <Heading>Posts</Heading>
-        <span>
-        It look like you haven't posted yet.  Post a report for a game in your{` `}
-          <RemixUnderlineLink
-            to="/profile/library"
-          >
-            library
-          </RemixUnderlineLink>
-          {` `}to see it appear here.
-        </span>
-
+        <div><Heading>Posts</Heading></div>
+        <div className="w-full max-w-sm">
+          <span>
+            It look like you haven't posted yet.  Post a report for a game in your{` `}
+            <RemixUnderlineLink
+              to="/profile/library"
+            >
+              library
+            </RemixUnderlineLink>
+            {` `}to see it appear here.
+          </span>
+        </div>
       </div>
     );
   }
@@ -48,6 +53,7 @@ export default function UsersPostsLayout({
         id,
         steamAppId,
         createdAt,
+        numLikes,
         ratingMedal,
         postText,
         frameRateAverage,
@@ -57,11 +63,10 @@ export default function UsersPostsLayout({
           headerImage,
         },
       }) => (
-        <Link
-          to={`/apps/${steamAppId}/performance-posts#${id}`}
+        <div
           key={id}
           className="border-1 border-secondary-highlight rounded-md
-                   p-3 md:px-4 md:py-3 bg-tertiary hover:bg-tertiary-highlight
+                   p-3 md:px-4 md:py-3 bg-tertiary
                    focus:show-ring w-full max-w-4xl
                    flex items-center justify-start"
         >
@@ -77,11 +82,20 @@ export default function UsersPostsLayout({
                 // </div>
               )}
               <div className="flex flex-col gap-2 text-sm w-full">
-                <div>
-                  <span className="font-semibold">
-                    {name}
-                  </span>
-                  {` - `}<i className="italic">{ratingMedal}</i>
+                <div className="flex justify-between">
+                  <div>
+                    <span className="font-semibold">
+                      {name}
+                    </span>
+                    {` - `}<i className="italic">{ratingMedal}</i>
+                  </div>
+                  <div>
+                    <RemixUnderlineLink
+                      to={`/apps/${steamAppId}/performance-posts#${id}`}
+                    >
+                    View Post
+                    </RemixUnderlineLink>
+                  </div>
                 </div>
                 <div className="line-clamp-3">
                   {postText}
@@ -94,6 +108,12 @@ export default function UsersPostsLayout({
                 frameRateAverage={frameRateAverage}
                 frameRateStutters={frameRateStutters}
                 createdAt={createdAt}
+                likeButtonData={{
+                  postId: id,
+                  numLikes,
+                  isUserLoggedIn,
+                  hasLoggedInUserLiked: likedPerformancePostIds.includes(id),
+                }}
               />
             </div>
           </div>
@@ -103,12 +123,19 @@ export default function UsersPostsLayout({
           <div
             className="md:hidden flex flex-col gap-2 justify-center w-full"
           >
-            <div className="flex flex-col justify-center">
+            <div className="flex justify-between items-center">
               <div>
                 <span className="font-semibold">
                   {name}
                 </span>
                 {` - `}<i className="italic">{ratingMedal}</i>
+              </div>
+              <div>
+                <RemixUnderlineLink
+                  to={`/apps/${steamAppId}/performance-posts#${id}`}
+                >
+                    View Post
+                </RemixUnderlineLink>
               </div>
             </div>
             <div className="line-clamp-4">
@@ -120,10 +147,16 @@ export default function UsersPostsLayout({
                 frameRateAverage={frameRateAverage}
                 frameRateStutters={frameRateStutters}
                 createdAt={createdAt}
+                likeButtonData={{
+                  postId: id,
+                  numLikes,
+                  isUserLoggedIn,
+                  hasLoggedInUserLiked: likedPerformancePostIds.includes(id),
+                }}
               />
             </div>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
