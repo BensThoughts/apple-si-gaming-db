b37418f4d14239a373c596715d81883903fe2796
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-06 13:18:23 -0700

    added like button/data and uses container query

--- apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostMetaBar.tsx
+++ apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostMetaBar.tsx
@@ -1,3 +1,4 @@
+import LikeButton from '~/components/LikeButton';
 import type { FrameRate, RatingMedal } from '~/interfaces';
 import { convertFrameRateToDescription, convertRatingMedalToDescription } from '~/lib/rating-conversions';
 
@@ -6,6 +7,12 @@ interface PerformancePostMetaBarProps {
   ratingMedal: RatingMedal;
   frameRateAverage?: FrameRate | null;
   frameRateStutters?: boolean | null;
+  likeButtonData: {
+    numLikes: number;
+    postId: string;
+    isUserLoggedIn: boolean;
+    hasLoggedInUserLiked: boolean;
+  }
 }
 
 export default function PerformancePostMetaBar({
@@ -13,32 +20,67 @@ export default function PerformancePostMetaBar({
   ratingMedal,
   frameRateAverage,
   frameRateStutters,
+  likeButtonData,
 }: PerformancePostMetaBarProps) {
+  const {
+    postId,
+    isUserLoggedIn,
+    hasLoggedInUserLiked,
+    numLikes,
+  } = likeButtonData;
   return (
-    <div className="flex flex-col gap-1 md:gap-0 md:flex-row items-start md:items-center justify-between text-sm px-3 py-1 rounded-sm bg-primary w-full text-primary-faded">
-      <div className="flex flex-col gap-1 md:flex-row md:gap-2">
-        <div>
-          <span className="text-primary-highlight">
-            {ratingMedal}
-          </span>
-          {` - `}
-          {convertRatingMedalToDescription(ratingMedal)}
-        </div>
-        <div>
-          {/* {(frameRateAverage || frameRateStutters) && <span>Frame Rate: </span>} */}
-          {frameRateAverage &&
+    <div className="@container">
+      <div className="flex flex-col gap-1 items-start justify-between text-sm px-3 py-1
+                      rounded-sm bg-primary w-full text-primary-faded
+                      @[600px]:gap-0 @[600px]:flex-row @[600px]:items-center
+                      supports-[not(container-type:inline-size)]:postMetaBarQuery:gap-0
+                      supports-[not(container-type:inline-size)]:postMetaBarQuery:flex-row
+                      supports-[not(container-type:inline-size)]:postMetaBarQuery:items-center"
+      >
+        <div className="flex flex-col gap-2 items-start justify-center
+                        @[600px]:items-center @[600px]:flex-row @[600px]:gap-3
+                        supports-[not(container-type:inline-size)]:postMetaBarQuery:gap-3
+                        supports-[not(container-type:inline-size)]:postMetaBarQuery:flex-row
+                        supports-[not(container-type:inline-size)]:postMetaBarQuery:items-center"
+        >
+          {/* <Form
+            method="post"
+            action="/actions/like-post"
+          > */}
+          <LikeButton
+            postId={postId}
+            numLikes={numLikes}
+            isUserLoggedIn={isUserLoggedIn}
+            hasLoggedInUserLiked={hasLoggedInUserLiked}
+          />
+          {/* </Form> */}
+          <div className="flex flex-col gap-1 md:flex-row md:gap-2">
+            <div>
+              <span className="text-primary-highlight">
+                {ratingMedal}
+              </span>
+              {` - `}
+              {convertRatingMedalToDescription(ratingMedal)}
+            </div>
+            <div>
+              {/* {(frameRateAverage || frameRateStutters) && <span>Frame Rate: </span>} */}
+              {frameRateAverage &&
             <span className="text-primary-highlight">
               {convertFrameRateToDescription(frameRateAverage)}
             </span>}
-          {frameRateStutters &&
+              {frameRateStutters &&
             <span className="text-primary-highlight">
               {frameRateStutters ? ' [ Stutters ]' : null }
             </span>}
+            </div>
+          </div>
         </div>
+
+        <i className="italic">
+          {createdAt.toDateString()}
+        </i>
       </div>
-      <i className="italic">
-        {createdAt.toDateString()}
-      </i>
     </div>
+
   );
 }
