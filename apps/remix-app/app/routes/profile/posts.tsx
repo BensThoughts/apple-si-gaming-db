72384923047f1be1a112e92cda80cc4746df377c
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-06 13:23:07 -0700

    added like count and likedPostIds to posts route

--- apps/remix-app/app/routes/profile/posts.tsx
+++ apps/remix-app/app/routes/profile/posts.tsx
@@ -5,9 +5,13 @@ import { extractAppLoadContext } from '~/lib/data-utils/appLoadContext.server';
 import { findSteamUsersPerformancePosts } from '~/models/steamUser.server';
 import type { SteamPerformancePost, SteamApp } from '~/interfaces/database';
 import { useLoaderData } from '@remix-run/react';
+import { useSteamUserLikedPostIds } from '~/lib/hooks/useMatchesData';
 
 interface ProfilePostsRouteLoaderData {
   steamUsersPosts: {
+    _count: {
+      usersWhoLiked: number;
+    },
     id: SteamPerformancePost['id'];
     steamAppId: SteamPerformancePost['steamAppId'];
     createdAt: SteamPerformancePost['createdAt'];
@@ -41,10 +45,14 @@ export async function loader({ context }: LoaderArgs) {
 
 export default function ProfilePostsRoute() {
   const { steamUsersPosts } = useLoaderData<ProfilePostsRouteLoaderData>();
+  const likedPerformancePostIds = useSteamUserLikedPostIds();
   return (
     <UsersPostsLayout
+      isUserLoggedIn={true}
+      likedPerformancePostIds={likedPerformancePostIds ? likedPerformancePostIds : []}
       steamUsersPosts={steamUsersPosts.map((performancePost) => ({
         ...performancePost,
+        numLikes: performancePost._count.usersWhoLiked,
         createdAt: new Date(performancePost.createdAt),
       }))}
     />
