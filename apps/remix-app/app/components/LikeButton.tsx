732282c30a0ec569b6b7dd51fcc6deeebb44e1f9
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-06 13:15:50 -0700

    created LikeButton

---
+++ apps/remix-app/app/components/LikeButton.tsx
@@ -0,0 +1,68 @@
+import { useFetcher } from '@remix-run/react';
+import { useState } from 'react';
+import { BleedingHeartOutlineIcon } from './Icons/FlatIcons';
+import { errorToast } from './Toasts';
+
+export default function LikeButton({
+  postId,
+  numLikes,
+  isUserLoggedIn,
+  hasLoggedInUserLiked,
+}: {
+  postId: string;
+  numLikes: number;
+  isUserLoggedIn: boolean;
+  hasLoggedInUserLiked: boolean;
+}) {
+  const [likedPostRaw, setLikedPostRaw] = useState(hasLoggedInUserLiked);
+  const [numLikesRaw, setNumLikesRaw] = useState(numLikes);
+
+  const fetcher = useFetcher();
+
+  const isSubmittingLike =
+    fetcher.state === 'submitting' &&
+    fetcher.submission.action === '/actions/like-post';
+  // const isLoading = fetcher.state === 'loading';
+
+  function onLikeClick() {
+    if (isUserLoggedIn) {
+      if (!likedPostRaw) {
+        setNumLikesRaw(numLikes + 1);
+        setLikedPostRaw(true);
+        fetcher.submit({
+          postId,
+        }, { action: '/actions/like-post', method: 'post' });
+      } else {
+        setNumLikesRaw(numLikesRaw - 1);
+        setLikedPostRaw(false);
+        fetcher.submit({
+          postId,
+        }, { action: '/actions/unlike-post', method: 'post' });
+      }
+    } else {
+      errorToast('You must log in to like posts');
+    }
+  }
+
+  return (
+    <button
+      onClick={onLikeClick}
+      disabled={isSubmittingLike}
+      type="submit"
+      className={`border-1 border-secondary-highlight rounded-full px-3 py-0.5
+                  min-w-[75px]`}
+      aria-label={likedPostRaw ? 'unlike post' : 'like post'}
+    >
+      <div className="flex justify-between items-center gap-2">
+        <BleedingHeartOutlineIcon
+          size={30}
+          className={`inline ${likedPostRaw ? 'text-primary' : 'text-primary-highlight'}`}
+          userLiked={likedPostRaw}
+        />
+        <span className={`${isSubmittingLike ? 'text-primary' : 'text-primary-highlight'}`}>
+          {numLikesRaw}
+        </span>
+      </div>
+    </button>
+  );
+}
