33f54f378e352debeff7a74898f56d9812dc02c4
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-06 13:20:44 -0700

    updated to use new snackbar specific colors

--- apps/remix-app/app/components/Toasts/index.tsx
+++ apps/remix-app/app/components/Toasts/index.tsx
@@ -13,7 +13,7 @@ function ErrorToast({ message, t }: ToastProps) {
     <Transition
       appear
       show={t.visible}
-      className="w-full max-w-md bg-color-error rounded-md
+      className="w-full max-w-md bg-error text-color-snackbar-error rounded-md
                  flex justify-between items-center px-4 py-2"
       enter="transform transition duration-[400ms]"
       enterFrom="opacity-0 translate-y-6"
