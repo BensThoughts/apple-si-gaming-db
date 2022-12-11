57275566533dd4cc3c5a5caa3adabbb9e45a56b5
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-11 15:44:11 -0700

    added status 400 to thrown Responses

--- apps/remix-app/app/lib/loader-functions/params-validators.server.ts
+++ apps/remix-app/app/lib/loader-functions/params-validators.server.ts
@@ -2,11 +2,11 @@ import type { Params } from '@remix-run/react';
 
 export function validateSteamAppId(params: Params) {
   if (!params.steamAppId) {
-    throw new Response('Expected params.steamAppid');
+    throw new Response('Expected params.steamAppid', { status: 400 });
   }
   const steamAppId = Number(params.steamAppId);
   if (!isFinite(steamAppId) || steamAppId < 0) {
-    throw new Response('steam appid must be a valid positive number');
+    throw new Response('steam appid must be a valid positive number', { status: 400 });
   }
   return steamAppId;
 }
