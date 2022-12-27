65acadf0d49db9c39029dca094c4c50ec689f715
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-27 13:02:26 -0700

    created MaterialSolid versions of Input and TextArea

---
+++ apps/remix-app/app/components/FormComponents/MaterialTextAreaSolid/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default function MaterialTextAreaSolid({
+  name,
+  label,
+  errorMessage,
+}: {
+  label: string;
+  errorMessage?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <div className="overflow-hidden relative w-full rounded bg-primary" key={name}>
+      <label
+        htmlFor={`${name}-input`}
+        className={`text-primary block w-full font-normal left-0 top-0 m-0 cursor-text
+                    pt-[18px] px-[12px] pb-0 absolute transition-all ease-linear duration-200
+                    peer-placeholder-shown:text-[1.2rem] peer-placeholder-shown:translate-y-0
+                    text-[0.75rem] -translate-y-[14px]
+                    ${errorMessage ? 'text-error' : 'text-primary'}
+        `}
+      >
+        <span className="text-primary">{label}</span>
+        {errorMessage && (
+          <span role="alert" id={`${name}-error`} className="error-message">
+            &nbsp;({errorMessage})
+          </span>
+        )}
+      </label>
+
+      <textarea
+        id={`${name}-input`}
+        name={name}
+        required
+        aria-describedby={errorMessage ? `${name}-error` : undefined}
+        // displayError={displayError as boolean}
+        className={`peer outline-none bg-transparent border-0
+                      text-primary block text-[1.2rem] mt-[24px] pt-0 px-[12px] pb-[10px] w-full
+                      focus-visible:outline-none
+                      border-b-2 min-h-[250px]
+                      ${errorMessage ? 'border-b-error' : 'border-b-secondary'}
+          `}
+      />
+      <div
+        className={`right-0 bottom-0 left-0 content-[''] block my-0 mx-auto
+                    absolute transition-all duration-300 border-b-4
+                    peer-focus:scale-150 w-full scale-0
+          ${errorMessage ? 'border-b-error' : 'border-b-secondary'}
+        `}
+      ></div>
+    </div>
+  );
+};
