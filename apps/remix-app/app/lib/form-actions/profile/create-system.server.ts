9b8d293e501233e782e0c9e77c340fec984f98d8
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-27 12:50:11 -0700

    moved extract system specs to form validation utils

--- apps/remix-app/app/lib/form-actions/profile/create-system.server.ts
+++ apps/remix-app/app/lib/form-actions/profile/create-system.server.ts
@@ -2,8 +2,7 @@ import { redirect, json } from '@remix-run/node';
 import { createSystemSpecs, findSystemSpecSystemNames } from '~/models/steamUserSystemSpecs.server';
 import type { SteamUserSystemSpecs } from '~/interfaces/database';
 import type { CreateSystemSpecActionData, ProfileSystemsActionData } from '~/routes/profile/systems';
-import { validateNewSystemName, validateSystemInfo } from '~/lib/form-validators/profile';
-import type { SystemSpec } from '~/interfaces';
+import { validateNewSystemName, validateSystemInfo, extractSystemSpecs } from '~/lib/form-validators/profile';
 
 const badRequest = (data: CreateSystemSpecActionData) => (
   json<ProfileSystemsActionData>({
@@ -12,120 +11,6 @@ const badRequest = (data: CreateSystemSpecActionData) => (
     },
   }, { status: 400 })
 );
-
-function extractComputerInformation(compInfo: string) {
-  const manufacturerRe = /Manufacturer:\s*([^\r\n]+)/i;
-  const modelRe = /Model:\s*([^\r\n]+)/i;
-  const formFactorRe = /Form Factor:\s*([^\r\n]+)/i;
-  const manufacturerReMatch = compInfo.match(manufacturerRe);
-  const modelReMatch = compInfo.match(modelRe);
-  const formFactorReMatch = compInfo.match(formFactorRe);
-  return {
-    manufacturer: manufacturerReMatch ? manufacturerReMatch[1] : null,
-    model: modelReMatch ? modelReMatch[1] : null,
-    formFactor: formFactorReMatch ? formFactorReMatch[1] : null,
-  };
-}
-
-function extractProcessorInformation(procInfo: string) {
-  const cpuVendorRe = /CPU Vendor:\s*([^\r\n]+)/i;
-  const cpuBrandRe = /CPU Brand:\s*([^\r\n]+)/i;
-  const cpuFamilyRe = /CPU Family:\s*([^\r\n]+)/i;
-  const cpuModelRe = /CPU Model:\s*([^\r\n]+)/i;
-  const cpuSteppingRe = /CPU Stepping:\s*([^\r\n]+)/i;
-  const cpuTypeRe = /CPU Type:\s*([^\r\n]+)/i;
-  const cpuSpeedRe = /Speed:\s*([^\r\n]+)/i;
-  const cpuVendorReMatch = procInfo.match(cpuVendorRe);
-  const cpuBrandReMatch = procInfo.match(cpuBrandRe);
-  const cpuFamilyReMatch = procInfo.match(cpuFamilyRe);
-  const cpuModelReMatch = procInfo.match(cpuModelRe);
-  const cpuSteppingReMatch = procInfo.match(cpuSteppingRe);
-  const cpuTypeReMatch = procInfo.match(cpuTypeRe);
-  const cpuSpeedReMatch = procInfo.match(cpuSpeedRe);
-  return {
-    cpuVendor: cpuVendorReMatch ? cpuVendorReMatch[1] : null,
-    cpuBrand: cpuBrandReMatch ? cpuBrandReMatch[1] : null,
-    cpuFamily: cpuFamilyReMatch ? cpuFamilyReMatch[1] : null,
-    cpuModel: cpuModelReMatch ? cpuModelReMatch[1] : null,
-    cpuStepping: cpuSteppingReMatch ? cpuSteppingReMatch[1] : null,
-    cpuType: cpuTypeReMatch ? cpuTypeReMatch[1] : null,
-    cpuSpeed: cpuSpeedReMatch ? cpuSpeedReMatch[1] : null,
-  };
-}
-
-function extractVideoCard(vidCardInfo: string) {
-  const videoDriverRe = /Driver:\s*([^\r\n]+)/i;
-  const videoDriverVersionRe = /Driver Version:\s*([^\r\n]+)/i;
-  const videoPrimaryVRAMRe = /Primary VRAM:\s*([^\r\n]+)/i;
-  const videoDriverReMatch = vidCardInfo.match(videoDriverRe);
-  const videoDriverVersionReMatch = vidCardInfo.match(videoDriverVersionRe);
-  const videoPrimaryVRAMReMatch = vidCardInfo.match(videoPrimaryVRAMRe);
-  return {
-    videoDriver: videoDriverReMatch ? videoDriverReMatch[1] : null,
-    videoDriverVersion: videoDriverVersionReMatch ? videoDriverVersionReMatch[1] : null,
-    videoPrimaryVRAM: videoPrimaryVRAMReMatch ? videoPrimaryVRAMReMatch[1] : null,
-  };
-}
-
-function extractSystemSpecs(
-    systemData: string,
-): SystemSpec {
-  // TODO: this could still get messed up if someone puts Video Card: section
-  // TODO: between Computer Information: and Processor Information: section
-
-  // Capture Computer Information: section
-  const compInfoRe = /(?<=Computer Information:[\n\r])[\S\s]*(?=Processor Information:)/ism;
-  const compInfoReMatch = systemData.match(compInfoRe);
-  let computerInformation;
-  if (!compInfoReMatch || !compInfoReMatch[0]) {
-    computerInformation = null;
-  } else {
-    const compInfo = compInfoReMatch[0];
-    computerInformation = extractComputerInformation(compInfo);
-  }
-  // Capture Processor Information: section
-  const processorInfoRe = /(?<=Processor Information:[\n\r])[\S\s]*(?=Operating System Version:)/ism;
-  const processorInfoReMatch = systemData.match(processorInfoRe);
-  let processorInformation;
-  if (!processorInfoReMatch || !processorInfoReMatch[0]) {
-    processorInformation = null;
-  } else {
-    const procInfo = processorInfoReMatch[0];
-    processorInformation = extractProcessorInformation(procInfo);
-  }
-
-  // Capture osVersion
-  const operatingSystemVersionRe = /Operating System Version:[\r\n]+\s*([^\r\n]+)/i;
-  const osVersion = systemData.match(operatingSystemVersionRe);
-
-  // Capture Video Card section
-  const videoCardRe = /(?<=Video Card:[\n\r])[\S\s]*(?=Memory:)/ism;
-  const videoCardReMatch = systemData.match(videoCardRe);
-  let videoCard;
-  if (!videoCardReMatch || !videoCardReMatch[0]) {
-    videoCard = null;
-  } else {
-    const vidCardInfo = videoCardReMatch[0];
-    videoCard = extractVideoCard(vidCardInfo);
-  }
-
-  // Capture RAM
-  // TODO: Maybe make it match after Memory: section
-  const memoryRAMRe = /RAM:\s*([^\r\n]+)/i;
-  const memoryRAM = systemData.match(memoryRAMRe);
-
-  return {
-    computerInformation,
-    processorInformation,
-    os: {
-      osVersion: osVersion ? osVersion[1] : null,
-    },
-    videoCard,
-    memory: {
-      memoryRAM: memoryRAM ? memoryRAM[1] : null,
-    },
-  };
-}
 
 export async function createSystem(
     steamUserId: SteamUserSystemSpecs['steamUserId'],
@@ -139,14 +24,13 @@ export async function createSystem(
   ) {
     return badRequest({ formError: `Form not submitted correctly.` });
   }
-  const systemSpecs = extractSystemSpecs(systemInfo);
   const systemNames = await findSystemSpecSystemNames(steamUserId);
   if (!systemNames) {
     return badRequest({ formError: `Could not find system names. Does user exist? steamUserId: ${steamUserId}` });
   }
   const fieldErrors = {
     systemName: validateNewSystemName(systemName, systemNames),
-    systemInfo: validateSystemInfo(systemSpecs),
+    systemInfo: validateSystemInfo(systemInfo),
   };
   const fields = {
     systemName,
@@ -157,7 +41,8 @@ export async function createSystem(
     return badRequest({ fieldErrors, fields });
   }
 
-  await createSystemSpecs(steamUserId, systemName, systemSpecs);
+  const systemSpecs = extractSystemSpecs(systemInfo);
+  await createSystemSpecs(steamUserId, systemName.trim(), systemSpecs);
 
   return redirect(`/profile/systems`);
 }
--- apps/remix-app/app/lib/form-validators/profile/index.ts
+++ apps/remix-app/app/lib/form-validators/profile/index.ts
@@ -1,26 +1,32 @@
 import type { SystemSpec } from '~/interfaces';
 
 export function validateSystemName(systemName: string) {
-  if (systemName.length < 3) {
-    return `The system name was too short (3 character minimum)`;
+  const systemNameTrimmed = systemName.trim();
+  if (systemNameTrimmed.length < 3) {
+    return `The system name is too short, 3 character minimum`;
   }
-  if (systemName.length > 25) {
-    return `The system name was too long (25 character maximum)`;
+  if (systemNameTrimmed.length > 25) {
+    return `The system name is too long, 25 character maximum`;
   }
 }
 
 export function validateNewSystemName(systemName: string, systemNames: string[]) {
-  validateSystemName(systemName);
-  if (systemNames.includes(systemName)) {
+  const systemNameTrimmed = systemName.trim();
+  const systemNameError = validateSystemName(systemNameTrimmed);
+  if (systemNameError) {
+    return systemNameError;
+  }
+  if (systemNames.includes(systemNameTrimmed)) {
     return `The system name ${systemName} is already taken`;
   }
   // ! Added to allow for no system specs on a post
-  if (systemName === 'None') {
+  if (systemNameTrimmed === 'None') {
     return `None is a reserved name and cannot be used`;
   }
 }
 
-export function validateSystemInfo(systemSpec: SystemSpec) {
+export function validateSystemInfo(systemInfo: string) {
+  const systemSpec = extractSystemSpecs(systemInfo);
   if (!systemSpec.computerInformation) {
     return `Could not find "Computer Information:" section. It must be at the top and followed by "Processor Information:" section.`;
   }
@@ -75,4 +81,119 @@ export function validateSystemInfo(systemSpec: SystemSpec) {
   if (!systemSpec.memory.memoryRAM) {
     return `Cannot validate RAM property`;
   }
+}
+
+
+function extractComputerInformation(compInfo: string) {
+  const manufacturerRe = /Manufacturer:\s*([^\r\n]+)/i;
+  const modelRe = /Model:\s*([^\r\n]+)/i;
+  const formFactorRe = /Form Factor:\s*([^\r\n]+)/i;
+  const manufacturerReMatch = compInfo.match(manufacturerRe);
+  const modelReMatch = compInfo.match(modelRe);
+  const formFactorReMatch = compInfo.match(formFactorRe);
+  return {
+    manufacturer: manufacturerReMatch ? manufacturerReMatch[1] : null,
+    model: modelReMatch ? modelReMatch[1] : null,
+    formFactor: formFactorReMatch ? formFactorReMatch[1] : null,
+  };
+}
+
+function extractProcessorInformation(procInfo: string) {
+  const cpuVendorRe = /CPU Vendor:\s*([^\r\n]+)/i;
+  const cpuBrandRe = /CPU Brand:\s*([^\r\n]+)/i;
+  const cpuFamilyRe = /CPU Family:\s*([^\r\n]+)/i;
+  const cpuModelRe = /CPU Model:\s*([^\r\n]+)/i;
+  const cpuSteppingRe = /CPU Stepping:\s*([^\r\n]+)/i;
+  const cpuTypeRe = /CPU Type:\s*([^\r\n]+)/i;
+  const cpuSpeedRe = /Speed:\s*([^\r\n]+)/i;
+  const cpuVendorReMatch = procInfo.match(cpuVendorRe);
+  const cpuBrandReMatch = procInfo.match(cpuBrandRe);
+  const cpuFamilyReMatch = procInfo.match(cpuFamilyRe);
+  const cpuModelReMatch = procInfo.match(cpuModelRe);
+  const cpuSteppingReMatch = procInfo.match(cpuSteppingRe);
+  const cpuTypeReMatch = procInfo.match(cpuTypeRe);
+  const cpuSpeedReMatch = procInfo.match(cpuSpeedRe);
+  return {
+    cpuVendor: cpuVendorReMatch ? cpuVendorReMatch[1] : null,
+    cpuBrand: cpuBrandReMatch ? cpuBrandReMatch[1] : null,
+    cpuFamily: cpuFamilyReMatch ? cpuFamilyReMatch[1] : null,
+    cpuModel: cpuModelReMatch ? cpuModelReMatch[1] : null,
+    cpuStepping: cpuSteppingReMatch ? cpuSteppingReMatch[1] : null,
+    cpuType: cpuTypeReMatch ? cpuTypeReMatch[1] : null,
+    cpuSpeed: cpuSpeedReMatch ? cpuSpeedReMatch[1] : null,
+  };
+}
+
+function extractVideoCard(vidCardInfo: string) {
+  const videoDriverRe = /Driver:\s*([^\r\n]+)/i;
+  const videoDriverVersionRe = /Driver Version:\s*([^\r\n]+)/i;
+  const videoPrimaryVRAMRe = /Primary VRAM:\s*([^\r\n]+)/i;
+  const videoDriverReMatch = vidCardInfo.match(videoDriverRe);
+  const videoDriverVersionReMatch = vidCardInfo.match(videoDriverVersionRe);
+  const videoPrimaryVRAMReMatch = vidCardInfo.match(videoPrimaryVRAMRe);
+  return {
+    videoDriver: videoDriverReMatch ? videoDriverReMatch[1] : null,
+    videoDriverVersion: videoDriverVersionReMatch ? videoDriverVersionReMatch[1] : null,
+    videoPrimaryVRAM: videoPrimaryVRAMReMatch ? videoPrimaryVRAMReMatch[1] : null,
+  };
+}
+
+export function extractSystemSpecs(
+    systemData: string,
+): SystemSpec {
+  // TODO: this could still get messed up if someone puts Video Card: section
+  // TODO: between Computer Information: and Processor Information: section
+
+  // Capture Computer Information: section
+  const compInfoRe = /(?<=Computer Information:[\n\r])[\S\s]*(?=Processor Information:)/ism;
+  const compInfoReMatch = systemData.match(compInfoRe);
+  let computerInformation;
+  if (!compInfoReMatch || !compInfoReMatch[0]) {
+    computerInformation = null;
+  } else {
+    const compInfo = compInfoReMatch[0];
+    computerInformation = extractComputerInformation(compInfo);
+  }
+  // Capture Processor Information: section
+  const processorInfoRe = /(?<=Processor Information:[\n\r])[\S\s]*(?=Operating System Version:)/ism;
+  const processorInfoReMatch = systemData.match(processorInfoRe);
+  let processorInformation;
+  if (!processorInfoReMatch || !processorInfoReMatch[0]) {
+    processorInformation = null;
+  } else {
+    const procInfo = processorInfoReMatch[0];
+    processorInformation = extractProcessorInformation(procInfo);
+  }
+
+  // Capture osVersion
+  const operatingSystemVersionRe = /Operating System Version:[\r\n]+\s*([^\r\n]+)/i;
+  const osVersion = systemData.match(operatingSystemVersionRe);
+
+  // Capture Video Card section
+  const videoCardRe = /(?<=Video Card:[\n\r])[\S\s]*(?=Memory:)/ism;
+  const videoCardReMatch = systemData.match(videoCardRe);
+  let videoCard;
+  if (!videoCardReMatch || !videoCardReMatch[0]) {
+    videoCard = null;
+  } else {
+    const vidCardInfo = videoCardReMatch[0];
+    videoCard = extractVideoCard(vidCardInfo);
+  }
+
+  // Capture RAM
+  // TODO: Maybe make it match after Memory: section
+  const memoryRAMRe = /RAM:\s*([^\r\n]+)/i;
+  const memoryRAM = systemData.match(memoryRAMRe);
+
+  return {
+    computerInformation,
+    processorInformation,
+    os: {
+      osVersion: osVersion ? osVersion[1] : null,
+    },
+    videoCard,
+    memory: {
+      memoryRAM: memoryRAM ? memoryRAM[1] : null,
+    },
+  };
 }
