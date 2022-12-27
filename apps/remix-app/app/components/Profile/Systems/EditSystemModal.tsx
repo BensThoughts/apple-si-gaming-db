a45ce84c8545ef97cd299672af4689e598078604
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-27 12:48:55 -0700

    updated EditSystemModal to use controlled V2 name input

--- apps/remix-app/app/components/Profile/Systems/EditSystemModal.tsx
+++ apps/remix-app/app/components/Profile/Systems/EditSystemModal.tsx
@@ -1,12 +1,14 @@
 import { Form, useSubmit } from '@remix-run/react';
-import React from 'react';
-import MaterialInputOutlined from '~/components/FormComponents/MaterialInputOutlined';
+import React, { useState } from 'react';
+import MaterialInputOutlinedV2 from '~/components/FormComponents/MaterialInputOutlinedV2';
+// import MaterialInputOutlinedNew from '~/components/FormComponents/MaterialInputOutlinedNew';
 import RoundedButton from '~/components/RoundedButton';
+import { validateNewSystemName } from '~/lib/form-validators/profile';
 import SystemModal from './SystemModal';
 
 interface EditSystemModalProps {
   systemName: string,
-  systemNames: string[],
+  currentSystemNames: string[],
   title: string,
   description?: string,
   isOpen: boolean
@@ -15,24 +17,35 @@ interface EditSystemModalProps {
 
 export default function EditSystemModal({
   systemName,
-  systemNames,
+  currentSystemNames,
   title,
   isOpen,
   setIsOpen,
 }: EditSystemModalProps) {
   const submit = useSubmit();
+  const [systemNameValue, setSystemNameValue] = useState('');
+  const [systemNameTouched, setSystemNameTouched] = useState(false);
+  const systemNameError = validateNewSystemName(systemNameValue, currentSystemNames);
+  const displayError = systemNameTouched && Boolean(systemNameError);
 
   // Form must be submitted prior to closing the modal
   // or errors occur.  So we use useSubmit()
   function handleSubmitEvent(event: React.FormEvent<HTMLFormElement>) {
     submit(event.currentTarget, { replace: true });
+    setSystemNameValue('');
+    setSystemNameTouched(false);
     setIsOpen(false);
   }
 
   return (
     <SystemModal
       isOpen={isOpen}
-      setIsOpen={setIsOpen}
+      onClose={() => {
+        setSystemNameValue('');
+        setSystemNameTouched(false);
+        setIsOpen(false);
+      }}
+      // setIsOpen={setIsOpen}
       title={title}
     >
       <div className="flex flex-col gap-4">
@@ -48,26 +61,46 @@ export default function EditSystemModal({
           <div>
             <input type="hidden" name="_profileAction" value="editSystem" />
             <input type="hidden" name="systemName" value={systemName} />
-            <input type="hidden" name="systemNames" value={systemNames} />
-            <MaterialInputOutlined
+            <input type="hidden" name="systemNames" value={currentSystemNames} />
+            {/* <MaterialInputOutlined
               name="updatedSystemName"
-              label="System Name..."
+              label="System Name"
+              defaultValue={systemName}
+              // getFieldError={(systemName) => validateNewSystemName(systemName, currentSystemNames)}
+              // wasSubmitted={false}
+              type="input"
               required
-              maxLength={25}
               minLength={3}
+              maxLength={25}
+            /> */}
+            <MaterialInputOutlinedV2
+              name="updatedSystemName"
+              value={systemNameValue}
+              label="System Name"
+              onChange={(e) => setSystemNameValue(e.currentTarget.value)}
+              onBlur={() => setSystemNameTouched(true)}
+              errorMessage={displayError ? systemNameError : undefined}
+              type="text"
+              required
+              minLength={3}
+              maxLength={25}
             />
           </div>
           <div className="flex gap-2">
             <RoundedButton
               type="submit"
-              className="text-primary-highlight inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 rounded-md border border-transparent text-primary-highlight hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
             >
               Edit
             </RoundedButton>
             <RoundedButton
               type="button"
-              className="text-primary-highlight inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-              onClick={() => setIsOpen(false)}
+              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 rounded-md border border-transparent text-primary-highlight hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              onClick={() => {
+                setSystemNameValue('');
+                setSystemNameTouched(false);
+                setIsOpen(false);
+              }}
             >
             Cancel
             </RoundedButton>
