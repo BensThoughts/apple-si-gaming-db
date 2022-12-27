ec218ec7d6967e0b46ebfc3df08bdda7fcfee45e
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-27 12:46:30 -0700

    added sourmaps: true to remix config, removed ignoredRouteFiles

--- apps/remix-app/remix.config.js
+++ apps/remix-app/remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
+  // ignoredRouteFiles: ['**/.*'],
+  sourcemaps: true,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
