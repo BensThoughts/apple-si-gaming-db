f604c0c1aefe9a09941f612efb4b70f0d1d6f6ec
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-02-05 10:45:27 -0700

    created small apps card to display apps with posts

---
+++ apps/remix-app/app/components/Cards/SmallAppsCard/SmallAppDisplayCard.tsx
@@ -0,0 +1,45 @@
+import { Link } from '@remix-run/react';
+import AppHeaderImage from '~/components/ImageWrappers/AppHeaderImage';
+
+export default function SmallAppDisplayCard({
+  name,
+  steamAppId,
+  headerImgSrc,
+  className = '',
+}: {
+  name: string;
+  steamAppId: number;
+  headerImgSrc: string | undefined | null;
+  className?: string;
+}) {
+  return (
+    <Link
+      to={`/apps/${steamAppId}/performance-posts`}
+      className={`flex flex-col justify-center items-center gap-1 bg-tertiary p-2
+                  hover:bg-tertiary-highlight rounded-lg w-full md:max-w-[210px]
+                  border-1 border-secondary-highlight focus-visible:show-ring
+                  ${className}`}
+    >
+      <div
+        className={`w-[276px] md:w-[184px] whitespace-nowrap overflow-hidden
+                    text-ellipsis text-primary-highlight`}
+        aria-label={name}
+      >
+        {name}
+      </div>
+      {headerImgSrc ? (
+        <div className={`rounded-lg flex items-center justify-center
+                         max-w-[276px] md:max-w-[184px] max-h-[129px]
+                         md:max-h-[86px]`}>
+          <AppHeaderImage headerImageSrc={headerImgSrc} name={name} />
+        </div>
+      ): (
+        <div className={`rounded-lg flex items-center justify-center
+                         max-w-[276px] md:max-w-[184px] max-h-[129px]
+                         md:max-h-[86px]`}>
+          <AppHeaderImage headerImageSrc="/svg-images/no-image-placeholder.svg" name={name} />
+        </div>
+      )}
+    </Link>
+  );
+}
---
+++ apps/remix-app/app/components/Cards/SmallAppsCard/SmallAppsLayout.tsx
@@ -0,0 +1,111 @@
+import { useMemo, useState } from 'react';
+import SelectMenu from '~/components/FormComponents/SelectMenu';
+import type { SelectOption } from '~/components/FormComponents/SelectMenu';
+import ToggleSwitch from '~/components/FormComponents/ToggleSwitch';
+import MaterialInputOutlined from '~/components/FormComponents/MaterialInputOutlined';
+import OwnedAppDisplay from './SmallAppDisplayCard';
+import RoundedButton from '~/components/RoundedButton';
+import type { SteamAppForSmallDisplayCard } from '.';
+
+function computeGenreOptions(ownedApps: SteamAppForSmallDisplayCard [], ALL_FILTER: SelectOption) {
+  const genreSet = new Set<string>();
+  ownedApps.forEach((app) => {
+    app.genres.forEach((genre) => genreSet.add(genre.description));
+  });
+  const genreOptions: SelectOption[] =
+    [...genreSet]
+        .sort((a, b) => (a < b ? -1 : 0))
+        .map((genre) => ({ name: genre, value: genre }));
+
+  genreOptions.unshift(ALL_FILTER);
+
+  return genreOptions;
+}
+
+export default function SmallAppsLayout({
+  steamApps,
+}: {
+  steamApps: SteamAppForSmallDisplayCard []
+}) {
+  const ALL_FILTER = useMemo(() => ({ name: 'All', value: 'All' }), []);
+  const genreOptions = useMemo(() => computeGenreOptions(steamApps, ALL_FILTER), [steamApps, ALL_FILTER]);
+  const [nameQuery, setNameQuery] = useState('');
+  const [genreFilter, setGenreFilter] = useState(ALL_FILTER);
+  const [filterAppleOnly, setFilterAppleOnly] = useState(false);
+  const [paginate, setPaginate] = useState(20);
+
+  const loadMore = () => {
+    setPaginate((prevValue) => prevValue + 20);
+  };
+
+  function searchNames(ownedApps: SteamAppForSmallDisplayCard []) {
+    return ownedApps.filter(
+        (app) =>
+          (genreFilter.value != ALL_FILTER.value
+          ? app.genres.map((genre) => genre.description).includes(genreFilter.value)
+          : app) &&
+          (filterAppleOnly
+          ? app.platformMac
+          : app) &&
+          (app.name.toString().toLowerCase().includes(nameQuery.toLowerCase())))
+        .sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : -1);
+  }
+
+  const filteredSteamApps = searchNames(steamApps);
+  const paginatedSteamApps = filteredSteamApps.slice(0, paginate);
+  const lastPage = paginate >= steamApps.length;
+
+  return (
+    <div className="flex flex-col items-center gap-8">
+      <div
+        className="self-center flex flex-col xl:flex-row items-center justify-center gap-3
+                   bg-tertiary p-0 md:p-4 rounded-lg md:border-1 md:border-secondary-highlight"
+      >
+        <div>
+          <MaterialInputOutlined
+            id="profileSearch"
+            name="profileSearch"
+            label="Search by name..."
+            componentSize="large"
+            aria-label="Search games by name"
+            onChange={(e) => setNameQuery(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-2">
+          <label>Apple:</label>
+          <ToggleSwitch checked={filterAppleOnly} onChange={setFilterAppleOnly} />
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-2">
+          <label>Select Genre:</label>
+          <SelectMenu
+            options={genreOptions}
+            onChange={(e) => setGenreFilter(e)}
+            name="profileGenreFilter"
+            // label="Select Genre"
+            defaultValue={ALL_FILTER}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center self-center gap-8 w-full">
+
+        <div className="flex flex-wrap justify-center items-center gap-3 md:gap-1">
+          {paginatedSteamApps.map(({
+            steamAppId,
+            name,
+            headerImage,
+          }) => (
+            <div key={steamAppId}>
+              <OwnedAppDisplay
+                steamAppId={steamAppId}
+                name={name}
+                headerImgSrc={headerImage}
+              />
+            </div>
+          ))}
+        </div>
+        {!lastPage && <RoundedButton onClick={() => loadMore()}>Load More...</RoundedButton>}
+      </div>
+    </div>
+  );
+}
