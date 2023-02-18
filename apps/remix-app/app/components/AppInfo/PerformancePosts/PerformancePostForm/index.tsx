b24008d858f1bc1e3935af61e0bf6f8139eb17df
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-02-18 11:40:20 -0700

    extracted options from PerformancePostForm

--- apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostForm/index.tsx
+++ apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostForm/index.tsx
@@ -3,100 +3,18 @@ import RoundedButton from '~/components/RoundedButton';
 import SelectMenu from '~/components/FormComponents/SelectMenu';
 import type { SelectOption } from '~/components/FormComponents/SelectMenu';
 import type { CreatePerformancePostActionData } from '~/routes/apps/$steamAppId/performance-posts';
-import type { FrameRate, GamepadRating, RatingMedal } from '~/interfaces';
 import React, { useEffect, useRef, useState } from 'react';
 import TextArea from '~/components/FormComponents/TextArea';
 import ToggleSwitch from '~/components/FormComponents/ToggleSwitch';
 import MultiSelectMenu from '~/components/FormComponents/MultiSelectMenu';
 import type { MultiSelectOption } from '~/components/FormComponents/MultiSelectMenu';
 import { showToast } from '~/components/Toasts';
-import {
-  convertFrameRateToDescription,
-  convertRatingMedalToFullText,
-  convertGamepadRatingToFullText,
-} from '~/lib/conversions/rating-conversions';
 import RemixUnderlineLink from '~/components/RemixUnderlineLink';
-
-const steamAppRatingOptions: SelectOption<RatingMedal | 'None'>[] = [
-  {
-    name: 'None',
-    value: 'None',
-  },
-  {
-    name: convertRatingMedalToFullText('Platinum'),
-    value: 'Platinum',
-  },
-  {
-    name: convertRatingMedalToFullText('Gold'),
-    value: 'Gold',
-  },
-  {
-    name: convertRatingMedalToFullText('Silver'),
-    value: 'Silver',
-  },
-  {
-    name: convertRatingMedalToFullText('Bronze'),
-    value: 'Bronze',
-  },
-  {
-    name: convertRatingMedalToFullText('Borked'),
-    value: 'Borked',
-  },
-];
-
-const frameRateAverageOptions: SelectOption<FrameRate | 'None'>[] = [
-  {
-    name: 'Not Sure',
-    value: 'None',
-  },
-  {
-    name: convertFrameRateToDescription('VeryLow'),
-    value: 'VeryLow',
-  },
-  {
-    name: convertFrameRateToDescription('Low'),
-    value: 'Low',
-  },
-  {
-    name: convertFrameRateToDescription('Medium'),
-    value: 'Medium',
-  },
-  {
-    name: convertFrameRateToDescription('High'),
-    value: 'High',
-  },
-  {
-    name: convertFrameRateToDescription('VeryHigh'),
-    value: 'VeryHigh',
-  },
-];
-
-const gamepadRatingOptions: SelectOption<GamepadRating | 'None'>[] = [
-  {
-    name: 'None',
-    value: 'None',
-  },
-  {
-    name: convertGamepadRatingToFullText('GamepadPlatinum'),
-    value: 'GamepadPlatinum',
-  },
-  {
-    name: convertGamepadRatingToFullText('GamepadGold'),
-    value: 'GamepadGold',
-  },
-  {
-    name: convertGamepadRatingToFullText('GamepadSilver'),
-    value: 'GamepadSilver',
-  },
-  {
-    name: convertGamepadRatingToFullText('GamepadBronze'),
-    value: 'GamepadBronze',
-  },
-  {
-    name: convertGamepadRatingToFullText('GamepadBorked'),
-    value: `GamepadBorked`,
-  },
-];
+import {
+  steamAppRatingOptions,
+  frameRateAverageOptions,
+  gamepadRatingOptions,
+} from './formOptions';
 
 interface PerformancePostFormProps {
   steamAppId: number;
