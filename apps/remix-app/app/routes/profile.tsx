b137f0aa970c266572c1abc5cd990191659e1501
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-02-18 10:38:35 -0700

    commented out achievements link in profile

--- apps/remix-app/app/routes/profile.tsx
+++ apps/remix-app/app/routes/profile.tsx
@@ -124,11 +124,11 @@ export default function ProfilePage() {
                   Liked Posts
                 </AnimatedUnderline>
               </Link>
-              <Link to="/profile/achievements" className="focus-visible:show-ring px-1 rounded-sm font-medium text-primary">
+              {/* <Link to="/profile/achievements" className="focus-visible:show-ring px-1 rounded-sm font-medium text-primary">
                 <AnimatedUnderline>
                   Achievements
                 </AnimatedUnderline>
-              </Link>
+              </Link> */}
             </div>
           </div>
           <div className="md:col-start-5 xl:col-start-4 col-end-[-1]">
