159c70b2384b56c9ed571f27854576c57812bd34
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-02-18 08:31:36 -0700

    using universal front end Perf Post Rating interface

--- apps/remix-app/app/components/AppInfo/PerformancePosts/AppRatingOverview.tsx
+++ apps/remix-app/app/components/AppInfo/PerformancePosts/AppRatingOverview.tsx
@@ -1,4 +1,4 @@
-import type { GamepadRating, RatingMedal, FrameRate } from '~/interfaces';
+import type { GamepadRating, FrameRate } from '~/interfaces';
 import { isTypeFrameRateAverage, isTypeGamepadRating } from '~/lib/form-validators/posts';
 import {
   convertRatingMedalToNumber,
@@ -9,54 +9,29 @@ import {
   convertGamepadRatingToNumber,
   convertNumberToGamepadRating,
   convertGamepadRatingToMedalText,
-} from '~/lib/rating-conversions';
-
-interface PerformancePostData {
-  id: string;
-  postText: string;
-  postTags: {
-    postTagId: number;
-    description: string;
-  }[],
-  gamepadMetadata: {
-    gamepadId: number;
-    description: string;
-  } | null,
-  gamepadRating: GamepadRating | null;
-  createdAt: Date;
-  ratingMedal: RatingMedal;
-  frameRateAverage?: FrameRate | null;
-  frameRateStutters?: boolean | null;
-  displayName?: string | null;
-  avatarMedium?: string | null;
-  systemManufacturer?: string | null;
-  systemModel?: string | null;
-  systemOsVersion?: string | null;
-  systemCpuBrand?: string | null;
-  systemVideoDriver?: string | null;
-  systemVideoDriverVersion?: string | null;
-  systemVideoPrimaryVRAM?: string | null;
-  systemMemoryRAM?: string | null;
-}
+} from '~/lib/conversions/rating-conversions';
+import type { PerformancePostRating } from '~/interfaces';
 
 interface AppRatingOverviewProps {
-  performancePosts: PerformancePostData[];
+  performancePostRatings: PerformancePostRating[];
 }
 
 const arrAvg = (arr: number[]) => arr.reduce((a, b) => a + b, 0) / arr.length;
 
-function getAverageMedalRating(perfPosts: PerformancePostData[]) {
-  const medalRatingNumbers = perfPosts.map((perfPost) => convertRatingMedalToNumber(perfPost.ratingMedal));
+function getAverageMedalRating(perfPostRatings: PerformancePostRating[]) {
+  const medalRatingNumbers =
+    perfPostRatings
+        .map((perfPostRating) => convertRatingMedalToNumber(perfPostRating.ratingMedal));
   const avg = arrAvg(medalRatingNumbers);
   const avgInt = Math.round(avg);
   const medal = convertNumberToRatingMedal(avgInt as 0 | 1 | 2 | 3 | 4);
   return medal;
 }
 
-function getAverageFrameRate(perfPosts: PerformancePostData[]) {
+function getAverageFrameRate(perfPostRatings: PerformancePostRating[]) {
   const frameRateNumbers =
-    perfPosts
-        .map((perfPost) => perfPost.frameRateAverage)
+    perfPostRatings
+        .map((perfPostRating) => perfPostRating.frameRateAverage)
         .filter((frameRateAverage) => isTypeFrameRateAverage(frameRateAverage as string))
         .map((frameRate) => convertFrameRateToNumber(frameRate as FrameRate));
   const avg = arrAvg(frameRateNumbers);
@@ -65,28 +40,30 @@ function getAverageFrameRate(perfPosts: PerformancePostData[]) {
   return frameRate;
 }
 
-function getPercentPostsStutters(perfPosts: PerformancePostData[]) {
-  const numPostsStutters = perfPosts.filter((perfPost) => perfPost.frameRateStutters).length;
-  const percentStutters = (numPostsStutters / perfPosts.length) * 100;
+function getPercentPostsStutters(perfPostRatings: PerformancePostRating[]) {
+  const numPostsStutters =
+    perfPostRatings
+        .filter((perfPostRating) => perfPostRating.frameRateStutters).length;
+  const percentStutters = (numPostsStutters / perfPostRatings.length) * 100;
   return Math.round(percentStutters);
 }
 
-function getAverageGamepadRating(perfPosts: PerformancePostData[]) {
+function getAverageGamepadRating(perfPostRatings: PerformancePostRating[]) {
   const gamepadRatingNumbers =
-    perfPosts
-        .filter((perfPost) => isTypeGamepadRating(perfPost.gamepadRating as string))
-        .map((perfPost) => convertGamepadRatingToNumber(perfPost.gamepadRating as GamepadRating));
+    perfPostRatings
+        .filter((perfPostRating) => isTypeGamepadRating(perfPostRating.gamepadRating as string))
+        .map((perfPostRating) => convertGamepadRatingToNumber(perfPostRating.gamepadRating as GamepadRating));
   const avg = arrAvg(gamepadRatingNumbers);
   const avgInt = Math.round(avg);
   const gamepadRating = convertNumberToGamepadRating(avgInt as 0 | 1 | 2 | 3 | 4);
   return gamepadRating;
 }
 
-export default function AppRatingOverview({ performancePosts }: AppRatingOverviewProps) {
-  const avgMedalInt = getAverageMedalRating(performancePosts);
-  const avgFrameRate = getAverageFrameRate(performancePosts);
-  const percentStutters = getPercentPostsStutters(performancePosts);
-  const avgGamepadRating = getAverageGamepadRating(performancePosts);
+export default function AppRatingOverview({ performancePostRatings }: AppRatingOverviewProps) {
+  const avgMedalInt = getAverageMedalRating(performancePostRatings);
+  const avgFrameRate = getAverageFrameRate(performancePostRatings);
+  const percentStutters = getPercentPostsStutters(performancePostRatings);
+  const avgGamepadRating = getAverageGamepadRating(performancePostRatings);
   return (
     <div className="flex flex-col items-center gap-2 w-full bg-tertiary border-solid
                     border-2 border-secondary p-3 rounded-lg text-primary-highlight">
