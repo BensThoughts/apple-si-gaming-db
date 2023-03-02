b930290d0d763e37f2b04dab6821ddb96f5089e2
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-02 11:15:02 -0700

    created ProfileMenu component

---
+++ apps/remix-app/app/components/Layout/ProfileMenu/index.tsx
@@ -0,0 +1,94 @@
+import { Menu, Transition } from '@headlessui/react';
+import { NavLink } from '@remix-run/react';
+import { Fragment } from 'react';
+import { SteamIcon } from '~/components/Icons';
+import { loggedInMenuLinks } from './profileMenuLinks';
+
+export default function ProfileMenu({
+  isLoggedIn,
+}: {
+  isLoggedIn: boolean;
+}) {
+  return (
+    <div className="flex items-center text-right">
+      <Menu as="div" className="relative inline-block text-left">
+        <Menu.Button className="group flex items-center justify-center gap-2 w-full h-[38px] rounded-md bg-primary py-px px-2 text-base font-medium text-primary hover:bg-primary-highlight focus:outline-none focus-visible:show-ring">
+          Account
+          <SteamIcon
+            className="text-icon-secondary rounded-md w-full h-full group-hover:text-icon-secondary-highlight"
+            aria-hidden="true"
+          />
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-secondary border-1 border-x-1 border-secondary-highlight rounded-md bg-primary shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1">
+              {/* {isLoggedIn ? <LoggedInMenu /> : <LoggedOutMenu />} */}
+              {isLoggedIn ? (
+                <>
+                  {loggedInMenuLinks.map((link) => (
+                    <Menu.Item key={link.to}>
+                      {({ active }) => (
+                        <NavLink
+                          to={link.to}
+                          className={`${
+                          active ? 'bg-primary-highlight text-primary-highlight' : 'text-primary'
+                          } flex w-full items-center rounded-md px-2 py-2 text-base hover:bg-primary-highlight hover:text-primary-highlight`}
+                        >
+                          {/* {active ? (
+                          <EditActiveIcon
+                            className="mr-2 h-5 w-5"
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <EditInactiveIcon
+                            className="mr-2 h-5 w-5"
+                            aria-hidden="true"
+                          />
+                        )} */}
+                          {link.title}
+                        </NavLink>
+                      )}
+                    </Menu.Item>
+                  ))}
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="/api/auth/steam/logout"
+                        className={`${
+                    active ? 'bg-primary-highlight text-primary-highlight' : 'text-primary'
+                        } flex w-full items-center rounded-md px-2 py-2 text-base hover:bg-primary-highlight hover:text-primary-highlight`}
+                      >
+                        Sign Out
+                      </a>
+                    )}
+                  </Menu.Item>
+                </>
+              ) : (
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/api/auth/steam/login"
+                      className={`${
+                    active ? 'bg-primary-highlight text-primary-highlight' : 'text-primary'
+                      } flex w-full items-center rounded-md px-2 py-2 text-base hover:bg-primary-highlight hover:text-primary-highlight`}
+                    >
+                      Sign In
+                    </a>
+                  )}
+                </Menu.Item>
+              )}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+}
---
+++ apps/remix-app/app/components/Layout/ProfileMenu/profileMenuLinks.ts
@@ -0,0 +1,26 @@
+export const loggedInMenuLinks = [
+  {
+    title: 'Profile',
+    to: '/profile',
+  },
+  {
+    title: 'Library',
+    to: '/profile/library',
+  },
+  {
+    title: 'Systems',
+    to: '/profile/systems',
+  },
+  {
+    title: 'Posts',
+    to: '/profile/posts',
+  },
+  {
+    title: 'Liked Posts',
+    to: '/profile/liked-posts',
+  },
+  // {
+  //   title: 'Log Out',
+  //   to: '/api/auth/steam/logout',
+  // },
+];
