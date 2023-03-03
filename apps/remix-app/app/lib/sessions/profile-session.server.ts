2315c994e4aaa498a5b51d1f54143732bda765a7
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-03 11:04:08 -0700

    simplified profileSession

--- apps/remix-app/app/lib/sessions/profile-session.server.ts
+++ apps/remix-app/app/lib/sessions/profile-session.server.ts
@@ -10,9 +10,9 @@ if (!sessionSecret) {
 }
 
 // TODO: Change secret to use ENV var
-const profileSession = createCookieSessionStorage({
+const userProfileSession = createCookieSessionStorage({
   cookie: {
-    name: '__profile_session',
+    name: '__user_profile_session',
     secrets: [sessionSecret],
     sameSite: 'lax',
     secure: process.env.NODE_ENV === 'production' ? true : false,
@@ -21,48 +21,27 @@ const profileSession = createCookieSessionStorage({
 });
 
 async function getProfileSession(request: Request) {
-  const session = await profileSession.getSession(request.headers.get('Cookie'));
-
-  const setIsLoggedIn = () => session.set('isLoggedIn', 'true');
-  const unsetIsLoggedIn = () => session.unset('isLoggedIn');
+  const session = await userProfileSession.getSession(request.headers.get('Cookie'));
   const setUserProfileId = (userProfileId: number) => session.set('userProfileId', userProfileId.toString());
-  const unsetUserProfileId = () => session.unset('userProfileId');
-  // const setSteamUserId64 = (steamUserId64: string) => session.set('steamUserId64', steamUserId64);
-  // const unsetSteamUserId64 = () => session.unset('steamUserId64');
 
   return {
     login: (
         userProfileId: number,
-        // steamUserId64: string,
     ) => {
       setUserProfileId(userProfileId);
-      // setSteamUserId64(steamUserId64);
-      setIsLoggedIn();
     },
-    logout: () => {
-      // TODO: "When using session.unset(), you need to be sure no
-      // TODO: other loaders in the request are going to want to read that"
-      unsetUserProfileId();
-      // unsetSteamUserId64();
-      unsetIsLoggedIn();
-    },
-
-    getIsLoggedIn: () => {
-      const isLoggedIn = session.get('isLoggedIn') as string | undefined;
-      if (!isLoggedIn) {
-        return false;
-      }
-      return true;
-    },
+    logout: () => userProfileSession.destroySession(session),
     getUserProfileId: () => {
-      const userProfileId = session.get('userProfileId') as string | undefined;
-      if (!userProfileId) {
+      const userProfileId = session.get('userProfileId');
+      if (typeof userProfileId != 'string') {
+        return undefined;
+      }
+      if (!isFinite(Number(userProfileId))) {
         return undefined;
       }
       return Number(userProfileId);
     },
-    // getSteamUserId64: () => session.get('steamUserId64') as string | undefined,
-    commit: () => profileSession.commitSession(session),
+    commit: () => userProfileSession.commitSession(session),
   };
 }
 
