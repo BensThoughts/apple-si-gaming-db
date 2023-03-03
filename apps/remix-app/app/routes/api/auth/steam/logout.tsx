4f485ef38d992ec12867d7e6b48148f8581e636d
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-03 11:11:41 -0700

    added shadow route /api/auth/steam/logout to properly redirect

---
+++ apps/remix-app/app/routes/api/auth/steam/logout.tsx
@@ -0,0 +1,26 @@
+import { redirect } from '@remix-run/node';
+import type { LoaderArgs } from '@remix-run/node';
+import { useNavigate } from '@remix-run/react';
+import { useEffect } from 'react';
+
+/**
+ *  This route should never actually be routed to because the express
+ *  app in server.ts includes it as a route for passport. For some reason
+ *  however remix tries to route here after the redirect() in /actions/logout.
+ *  The loader isn't called but the route is shown.
+ *  /api/auth/steam/login and /api/auth/steam/return are also handles in server.ts
+ */
+
+export async function loader({ context }: LoaderArgs) {
+  return redirect('/');
+}
+
+export default function SteamLogoutRoute() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/');
+  }, [navigate]);
+
+  return null;
+}
