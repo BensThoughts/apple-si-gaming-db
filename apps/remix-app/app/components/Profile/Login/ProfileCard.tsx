bc571571aff6a16d50843b5ff80cb9591f9d9b39
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-03 11:06:18 -0700

    changed to useFetcher for logout

--- apps/remix-app/app/components/Profile/Login/ProfileCard.tsx
+++ apps/remix-app/app/components/Profile/Login/ProfileCard.tsx
@@ -1,4 +1,5 @@
-import AnimatedUnderline from '~/components/AnimatedUnderline';
+import { useFetcher } from '@remix-run/react';
+import RoundedButton from '~/components/RoundedButton';
 
 export default function ProfileCard({
   avatarFull,
@@ -7,12 +8,10 @@ export default function ProfileCard({
   avatarFull?: string | null;
   displayName?: string | null;
 }) {
+  const fetcher = useFetcher();
   return (
-    <div className={`flex flex-col items-center gap-4 p-6
-    border-secondary-highlight border-solid
-    border-y-2 md:border-x-2 w-full max-w-md
-    rounded-lg`}>
-      <h2><strong>{displayName}</strong></h2>
+    <div className={`flex flex-col items-center gap-4 w-full max-w-md`}>
+      <h2 className="text-2xl"><strong>{displayName}</strong></h2>
       {avatarFull && <img
         src={avatarFull}
         alt="Current User Avatar"
@@ -20,12 +19,13 @@ export default function ProfileCard({
         onError={(e) => {
           e.currentTarget.src = '/svg-images/no-image-placeholder.svg';
         }}
+        className="rounded-full border-1 border-secondary-highlight"
       />}
-      <a
-        href="/api/auth/steam/logout"
+      <RoundedButton
+        onClick={() => fetcher.submit({}, { action: '/actions/logout', method: 'post' })}
       >
-        <AnimatedUnderline>Logout</AnimatedUnderline>
-      </a>
+        Logout
+      </RoundedButton>
     </div>
   );
 }
