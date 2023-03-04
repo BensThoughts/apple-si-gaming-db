68bcada61029ac7c6397b3bf99bb1ddb04f1631d
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-04 15:40:56 -0700

    using logout function from profile-session

--- apps/remix-app/app/routes/actions/logout.ts
+++ apps/remix-app/app/routes/actions/logout.ts
@@ -1,20 +1,6 @@
-import { redirect } from '@remix-run/node';
 import type { ActionArgs } from '@remix-run/node';
-// import { getBannerSession } from '~/lib/sessions/banner-session.server';
-import { getProfileSession } from '~/lib/sessions/profile-session.server';
-import { getBannerSession } from '~/lib/sessions/banner-session.server';
+import { logout } from '~/lib/sessions/profile-session.server';
 
 export async function action({ request }: ActionArgs) {
-  const profileSession = await getProfileSession(request);
-  const bannerSession = await getBannerSession(request);
-  // profileSession.logout();
-  const headers = new Headers();
-  headers.append('Set-Cookie', await profileSession.destroy());
-  headers.append('Set-Cookie', await bannerSession.destroy());
-
-  // const bannerSession = await getBannerSession(request);
-  // bannerSession.setShowSignInBanner();
-  // headers.append('Set-Cookie', await bannerSession.commit());
-
-  return redirect('/api/auth/steam/logout', { headers, status: 302 });
+  return logout(request);
 }
