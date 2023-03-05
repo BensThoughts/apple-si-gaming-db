19ba934125b97df18c9addea5cfe0c540fdf8d8c
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-05 14:54:22 -0700

    created wide options for button

--- apps/remix-app/app/components/RoundedButton.tsx
+++ apps/remix-app/app/components/RoundedButton.tsx
@@ -1,10 +1,19 @@
-export default function RoundedButton({ className, children, ...rest }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+type RoundedButtonProps = {
+  width?: 'regular' | 'wide';
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export default function RoundedButton({
+  width = 'regular',
+  className,
+  children,
+  ...rest
+}: RoundedButtonProps) {
   return (
     <button
       className={`text-sm text-primary-highlight inline-flex justify-center items-center
                   border border-transparent font-medium px-4 py-2 rounded
                   bg-secondary hover:bg-secondary-highlight focus-visible:bg-secondary-highlight
-                  focus-visible:show-ring w-[89.66px] h-[38px] ${className}
+                  focus-visible:show-ring h-[38px] ${width === 'regular' ? 'w-[89.66px]' : 'w-36'}  ${className}
                 `}
       // previously had min-w-[89.66px]
       {...rest}
