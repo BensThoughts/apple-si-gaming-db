5a58aec0c356ad5053f8e1b434ebc427290c86d6
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-05 15:09:44 -0700

    added await to return logout

--- apps/remix-app/app/routes/actions/logout.ts
+++ apps/remix-app/app/routes/actions/logout.ts
@@ -2,5 +2,5 @@ import type { ActionArgs } from '@remix-run/node';
 import { logout } from '~/lib/sessions/profile-session.server';
 
 export async function action({ request }: ActionArgs) {
-  return logout(request);
+  return await logout(request);
 }
