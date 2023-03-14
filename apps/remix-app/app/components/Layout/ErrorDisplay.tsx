1a6c744caae9f14ae2f02ab67e52929a9703fa49
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-13 23:28:20 -0700

    changed ErrorDisplay to true ErrorDisplay

--- apps/remix-app/app/components/Layout/ErrorDisplay.tsx
+++ apps/remix-app/app/components/Layout/ErrorDisplay.tsx
@@ -1,24 +1,23 @@
-import type { ThrownResponse } from '@remix-run/react';
 import PageWrapper from './PageWrapper';
 
 export default function ErrorDisplay({
   currentRoute,
-  thrownResponse,
+  error,
 }: {
   currentRoute: string;
-  thrownResponse: ThrownResponse<number, any>;
+  error: Error;
 }) {
   const {
-    status,
-    statusText,
-    data,
-  } = thrownResponse;
+    name,
+    message,
+    // stack,
+  } = error;
   return (
     <PageWrapper currentRoute={currentRoute} topSpacer>
       <div className="flex flex-col gap-4 w-full h-[calc(100vh_-_14rem)] items-center justify-center">
-        <h1 className="text-4xl md:text-6xl text-primary-highlight">Oops! - {status} Error</h1>
-        <p>{statusText} - {data}</p>
-        {/* <p>Error in {currentRoute} route</p> */}
+        <h1 className="text-4xl md:text-6xl text-primary-highlight">Oops! - {name}</h1>
+        <p>{message}</p>
+        {/* <pre className="text-xs">{stack}</pre> */}
       </div>
     </PageWrapper>
   );
