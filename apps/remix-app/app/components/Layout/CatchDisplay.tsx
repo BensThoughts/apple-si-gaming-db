6582c123c3d6a1df5bf3aff1887af35b62e6356a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-13 23:28:04 -0700

    created CatchDisplay

---
+++ apps/remix-app/app/components/Layout/CatchDisplay.tsx
@@ -0,0 +1,25 @@
+import type { ThrownResponse } from '@remix-run/react';
+import PageWrapper from './PageWrapper';
+
+export default function CatchDisplay({
+  currentRoute,
+  thrownResponse,
+}: {
+  currentRoute: string;
+  thrownResponse: ThrownResponse<number, any>;
+}) {
+  const {
+    status,
+    statusText,
+    data,
+  } = thrownResponse;
+  return (
+    <PageWrapper currentRoute={currentRoute} topSpacer>
+      <div className="flex flex-col gap-4 w-full h-[calc(100vh_-_14rem)] items-center justify-center">
+        <h1 className="text-4xl md:text-6xl text-primary-highlight">Oops! - {status} Error</h1>
+        <p>{statusText} - {data}</p>
+        {/* <p>Error in {currentRoute} route</p> */}
+      </div>
+    </PageWrapper>
+  );
+}
