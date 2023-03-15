044ae46218c88073b74d3b2f31b31efbca5b01a7
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-15 12:48:47 -0700

    updated logger messages

--- packages/steam-api/src/config.ts
+++ packages/steam-api/src/config.ts
@@ -4,24 +4,23 @@ import { logger } from './logger';
 const getConfig = () => {
   const STEAM_API_KEY = process.env.STEAM_API_KEY;
   invariant(typeof STEAM_API_KEY === 'string', 'STEAM_API_KEY env var for packages/steam-api not set');
-  logger.info(`env var STEAM_API_KEY is set for packages/steam-api`);
 
   const STEAM_API_APP_DETAILS_ENDPOINT =
     process.env.STEAM_API_APP_DETAILS_ENDPOINT ||
     'https://store.steampowered.com/api/appdetails/';
-  logger.info(`env var STEAM_API_APP_DETAILS_ENDPOINT for packages/steam-api is set to ${STEAM_API_APP_DETAILS_ENDPOINT}`);
+  logger.info(`STEAM_API_APP_DETAILS_ENDPOINT for packages/steam-api is set to ${STEAM_API_APP_DETAILS_ENDPOINT}`);
 
   // ! 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/' both
   // ! endpoints seem to work, not sure which is more correct
   const STEAM_API_GET_OWNED_GAMES_ENDPOINT =
     process.env.STEAM_API_GET_OWNED_GAMES_ENDPOINT ||
     'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/';
-  logger.info(`env var STEAM_API_GET_OWNED_GAMES_ENDPOINT for packages/steam-api is set to ${STEAM_API_GET_OWNED_GAMES_ENDPOINT}`);
+  logger.info(`STEAM_API_GET_OWNED_GAMES_ENDPOINT for packages/steam-api is set to ${STEAM_API_GET_OWNED_GAMES_ENDPOINT}`);
 
   const STEAM_API_APP_LIST_ENDPOINT =
     process.env.STEAM_API_APP_LIST_ENDPOINT ||
     'https://api.steampowered.com/ISteamApps/GetAppList/v2/';
-  logger.info(`env var STEAM_API_APP_LIST_ENDPOINT for packages/steam-api is set to ${STEAM_API_APP_LIST_ENDPOINT}`);
+  logger.info(`STEAM_API_APP_LIST_ENDPOINT for packages/steam-api is set to ${STEAM_API_APP_LIST_ENDPOINT}`);
 
   return {
     STEAM_API_KEY,
