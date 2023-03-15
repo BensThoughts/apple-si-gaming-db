6be324dd673df1327a5d45a3fc580d23a2af9e22
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-14 17:42:24 -0700

    added option to include PageWrapper or not

--- apps/remix-app/app/components/Layout/CatchDisplay.tsx
+++ apps/remix-app/app/components/Layout/CatchDisplay.tsx
@@ -1,25 +1,40 @@
 import type { ThrownResponse } from '@remix-run/react';
 import PageWrapper from './PageWrapper';
 
-export default function CatchDisplay({
-  currentRoute,
+const MainCatchDisplay = ({
   thrownResponse,
 }: {
-  currentRoute: string;
-  thrownResponse: ThrownResponse<number, any>;
-}) {
+  thrownResponse: ThrownResponse<number, any>
+}) => {
   const {
     status,
     statusText,
     data,
   } = thrownResponse;
   return (
-    <PageWrapper currentRoute={currentRoute} topSpacer>
-      <div className="flex flex-col gap-4 w-full h-[calc(100vh_-_14rem)] items-center justify-center">
-        <h1 className="text-4xl md:text-6xl text-primary-highlight">Oops! - {status} Error</h1>
-        <p>{statusText} - {data}</p>
-        {/* <p>Error in {currentRoute} route</p> */}
-      </div>
-    </PageWrapper>
+    <div className="flex flex-col gap-4 w-full h-[calc(100vh_-_14rem)] items-center justify-center">
+      <h1 className="text-4xl md:text-6xl text-primary-highlight">Oops! - {status} Error</h1>
+      <p>{statusText} - {data}</p>
+      {/* <p>Error in {currentRoute} route</p> */}
+    </div>
   );
+};
+
+export default function CatchDisplay({
+  includePageWrap,
+  currentRoute,
+  thrownResponse,
+}: {
+  includePageWrap: boolean;
+  currentRoute: string;
+  thrownResponse: ThrownResponse<number, any>;
+}) {
+  if (includePageWrap) {
+    return (
+      <PageWrapper currentRoute={currentRoute} topSpacer>
+        <MainCatchDisplay thrownResponse={thrownResponse} />
+      </PageWrapper>
+    );
+  }
+  return <MainCatchDisplay thrownResponse={thrownResponse} />;
 }
--- apps/remix-app/app/components/Layout/ErrorDisplay.tsx
+++ apps/remix-app/app/components/Layout/ErrorDisplay.tsx
@@ -1,24 +1,35 @@
 import PageWrapper from './PageWrapper';
 
+const MainErrorDisplay = ({
+  error,
+} : {
+  error: Error
+}) => {
+  const { name, message } = error;
+  return (
+    <div className="flex flex-col gap-4 w-full h-[calc(100vh_-_14rem)] items-center justify-center">
+      <h1 className="text-4xl md:text-6xl text-primary-highlight">Oops! - {name}</h1>
+      <p>{message}</p>
+      {/* <pre className="text-xs">{stack}</pre> */}
+    </div>
+  );
+};
+
 export default function ErrorDisplay({
+  includePageWrapper,
   currentRoute,
   error,
 }: {
+  includePageWrapper: boolean;
   currentRoute: string;
   error: Error;
 }) {
-  const {
-    name,
-    message,
-    // stack,
-  } = error;
-  return (
-    <PageWrapper currentRoute={currentRoute} topSpacer>
-      <div className="flex flex-col gap-4 w-full h-[calc(100vh_-_14rem)] items-center justify-center">
-        <h1 className="text-4xl md:text-6xl text-primary-highlight">Oops! - {name}</h1>
-        <p>{message}</p>
-        {/* <pre className="text-xs">{stack}</pre> */}
-      </div>
-    </PageWrapper>
-  );
+  if (includePageWrapper) {
+    return (
+      <PageWrapper currentRoute={currentRoute} topSpacer>
+        <MainErrorDisplay error={error} />
+      </PageWrapper>
+    );
+  }
+  return <MainErrorDisplay error={error} />;
 }
