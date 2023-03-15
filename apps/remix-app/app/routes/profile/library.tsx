bb301f3335a49661f403f3318315ccb4f495d14a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-14 17:52:37 -0700

    added logic to return success/error state on action

--- apps/remix-app/app/routes/profile/library.tsx
+++ apps/remix-app/app/routes/profile/library.tsx
@@ -1,50 +1,79 @@
 import { json } from '@remix-run/node';
 import type { ActionArgs, LoaderArgs } from '@remix-run/node';
-import { useLoaderData, useTransition } from '@remix-run/react';
+import { useCatch, useLoaderData } from '@remix-run/react';
 import LibraryLayout from '~/components/Profile/Library/LibraryLayout';
 import {
   findSteamUserProfileOwnedSteamApps,
   updateSteamUserProfileOwnedSteamApps,
 } from '~/models/Steam/steamUserProfile.server';
 import { requireUserIds } from '~/lib/sessions/profile-session.server';
+import type { SteamProfileOwnedSteamApp } from '~/interfaces';
+import ErrorDisplay from '~/components/Layout/ErrorDisplay';
+import CatchDisplay from '~/components/Layout/CatchDisplay';
+
+type LibraryLoaderData = {
+  ownedSteamApps: SteamProfileOwnedSteamApp[];
+}
 
 export async function loader({ request }: LoaderArgs) {
   const { steamUserId64 } = await requireUserIds(request, '/profile');
   const ownedSteamApps = await findSteamUserProfileOwnedSteamApps(steamUserId64);
-  return json({ ownedSteamApps });
+  return json<LibraryLoaderData>({ ownedSteamApps });
+}
+
+export type LibraryActionData = {
+  updateOwnedGames: {
+    success: boolean;
+  }
 }
 
 export async function action({ request, context }: ActionArgs) {
   const { steamUserId64 } = await requireUserIds(request, '/profile');
   const formData = await request.formData();
   const action = formData.get('_profileAction');
+  if (typeof action != 'string') {
+    throw new Error('invalid form submission, typeof action does not equal string');
+  }
   switch (action) {
     case 'updateOwnedGames': {
       // const userProfileExists = await doesUserProfileExist(userProfileId);
       // if (!userProfileExists) {
       //   await upsertUserProfileBySteamUserId64(steamUserId64, steamUser);
       // }
-      await updateSteamUserProfileOwnedSteamApps(steamUserId64);
-      return json({});
+      const { success } = await updateSteamUserProfileOwnedSteamApps(steamUserId64);
+      // if (!success) {
+      //   throw new Response('failed to update library', { status: 500 });
+      // }
+      return json<LibraryActionData>({ updateOwnedGames: { success } });
     }
     default: {
-      throw new Error('Unexpected action in /profile/library');
+      throw new Error(`unexpected action ${action} in /profile/library`);
     }
   }
 }
 
 export default function ProfileAppsRoute() {
   const { ownedSteamApps } = useLoaderData<typeof loader>();
-  const transition = useTransition();
+  // const transition = useTransition();
 
-  const isSubmittingUpdateGames =
-    transition.state === 'submitting' &&
-    transition.submission.formData.get('_profileAction') === 'updateOwnedGames';
+  // const isSubmittingUpdateGames =
+  //   transition.state === 'submitting' &&
+  //   transition.submission.formData.get('_profileAction') === 'updateOwnedGames';
 
   return (
     <LibraryLayout
       ownedApps={ownedSteamApps}
-      isSubmittingUpdateGames={isSubmittingUpdateGames}
     />
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return <ErrorDisplay includePageWrapper={false} error={error} currentRoute="/profile/library" />;
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <CatchDisplay includePageWrap={false} thrownResponse={caught} currentRoute="/profile/library" />
   );
 }
