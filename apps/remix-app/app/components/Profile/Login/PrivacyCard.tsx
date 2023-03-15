5a056cc3a59f01759202b2d62d719de1737648b9
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-15 14:36:13 -0700

    added words to tell users about possible delay

--- apps/remix-app/app/components/Profile/Login/PrivacyCard.tsx
+++ apps/remix-app/app/components/Profile/Login/PrivacyCard.tsx
@@ -5,16 +5,18 @@ export default function PrivacyCard() {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <AsideCard title="Note" iconBackground="bg-tertiary" className="max-w-md">
-    Within your&nbsp;
+        Within your&nbsp;
         <ExternalLink
           href="https://steamcommunity.com/my/edit/settings"
           className="underline-offset-2"
         >
-      Steam Privacy Settings
+          Steam Privacy Settings
         </ExternalLink>
-    , &apos;My profile&apos; and &apos;Game Details&apos;
-    must be set to public to synchronize your Steam library
-    with this site.
+        , &apos;My profile&apos; and &apos;Game Details&apos;
+        must be set to public to synchronize your Steam library
+        with this site. Please note that you may need to wait up
+        to an hour if you have just recently set your privacy settings
+        to public.
       </AsideCard>
     </div>
   );
