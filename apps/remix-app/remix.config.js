7f59a19424b01381b73cb61e95b280c34aecb15e
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-14 17:38:01 -0700

    added some commented out lines for serverDepsToBundle

--- apps/remix-app/remix.config.js
+++ apps/remix-app/remix.config.js
@@ -5,6 +5,12 @@ module.exports = {
   future: {
     unstable_tailwind: true,
   },
+  // serverDependenciesToBundle: ['axios']
+  // serverDependenciesToBundle: [
+  //   '@apple-si-gaming-db/steam-api',
+  //   '@apple-si-gaming-db/database',
+  //   '@apple-si-gaming-db/logger',
+  // ]
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
