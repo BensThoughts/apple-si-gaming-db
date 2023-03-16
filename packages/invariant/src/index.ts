e6f7181ff5df2b9d9663a216c44b97ac665460aa
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-15 22:35:46 -0700

    created version of tiny-invariant that keeps message in production

---
+++ packages/invariant/src/index.ts
@@ -0,0 +1,24 @@
+const prefix: string = 'Invariant failed';
+
+// Throw an error if the condition fails
+// ! DO NOT Strip out error messages for production
+// > Not providing an inline default argument for message as the result is smaller
+export default function invariant(
+    condition: any,
+    // Can provide a string, or a function that returns a string for cases where
+    // the message takes a fair amount of effort to compute
+    message?: string | (() => string),
+): asserts condition {
+  if (condition) {
+    return;
+  }
+  // Condition not passed
+
+  const provided: string | undefined = typeof message === 'function' ? message() : message;
+
+  // Options:
+  // 1. message provided: `${prefix}: ${provided}`
+  // 2. message not provided: prefix
+  const value: string = provided ? `${prefix}: ${provided}` : prefix;
+  throw new Error(value);
+}
