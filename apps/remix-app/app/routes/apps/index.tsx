9d79d88b58287f04ce8f3f9fa649759792c06388
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-15 23:02:15 -0700

    removed new posts and trending apps

--- apps/remix-app/app/routes/apps/index.tsx
+++ apps/remix-app/app/routes/apps/index.tsx
@@ -6,7 +6,6 @@ import { findSteamAppsWherePostsExist, findTrendingSteamApps } from '~/models/St
 import { findNewestPerformancePosts } from '~/models/SteamedApples/performancePost.server';
 import type {
   TrendingSteamApp,
-  PerformancePost,
 } from '~/interfaces';
 
 import { Fragment } from 'react';
@@ -20,21 +19,13 @@ import CatchDisplay from '~/components/Layout/CatchDisplay';
 
 
 interface LoaderData {
-  trendingSteamApps: TrendingSteamApp[];
-  newPerformancePosts: Omit<PerformancePost, 'postTags' | 'systemSpec' | 'numLikes'>[];
   steamAppsWherePostsExist: SteamAppForSmallDisplayCard[];
 }
 
 export async function loader() {
-  const NUM_TRENDING_APPS = 10;
-  const NUM_RECENT_POSTS = 15;
-  const trendingSteamApps = await findTrendingSteamApps(NUM_TRENDING_APPS);
-  const newPerformancePosts = await findNewestPerformancePosts(NUM_RECENT_POSTS);
   const steamAppsWherePostsExist = await findSteamAppsWherePostsExist();
   return json<LoaderData>({
-    trendingSteamApps,
     steamAppsWherePostsExist,
-    newPerformancePosts,
   });
 }
 
@@ -44,62 +35,12 @@ export const meta: MetaFunction = () => {
 
 export default function SteamAppIdIndexRoute() {
   const {
-    trendingSteamApps,
-    newPerformancePosts,
     steamAppsWherePostsExist,
   } = useLoaderData<typeof loader>();
   return (
     <PageWrapper currentRoute="/apps" title="Games">
       <div className="flex flex-col items-center gap-12 w-full mt-6">
         <SmallAppsCard steamApps={steamAppsWherePostsExist} />
-        {(trendingSteamApps.length > 0) && (
-          <div className="flex flex-col items-center gap-6 px-6 py-8 bg-tertiary rounded-lg shadow-md w-full max-w-2xl">
-            <h2 className="text-secondary text-2xl">Top 10 Trending Apps</h2>
-            <div className="flex flex-col items-center gap-4 w-full">
-              {trendingSteamApps.map(({
-                steamAppId,
-                name,
-                headerImage,
-                numPerformancePosts,
-              }) => (
-                <Fragment key={steamAppId}>
-                  <TrendingSteamAppCard
-                    steamAppId={steamAppId}
-                    name={name}
-                    headerImage={headerImage}
-                    numPerformancePosts={numPerformancePosts}
-                  />
-                </Fragment>
-              ))}
-            </div>
-          </div>
-        )}
-        {(newPerformancePosts.length > 0) && (
-          <div className="flex flex-col items-center gap-6 px-6 py-8 bg-tertiary rounded-lg shadow-md w-full max-w-2xl">
-            <h2 className="text-secondary text-2xl">Top 15 New Posts</h2>
-            <div className="w-full flex flex-col items-center gap-4">
-              {newPerformancePosts.map(({
-                performancePostId,
-                createdAt,
-                steamApp,
-                postText,
-                userWhoCreated,
-                rating,
-              }) => (
-                <Fragment key={performancePostId}>
-                  <NewPerformancePostCard
-                    performancePostId={performancePostId}
-                    createdAt={new Date(createdAt)}
-                    steamApp={steamApp}
-                    postText={postText}
-                    userWhoCreated={userWhoCreated}
-                    rating={rating}
-                  />
-                </Fragment>
-              ))}
-            </div>
-          </div>
-        )}
       </div>
     </PageWrapper>
   );
