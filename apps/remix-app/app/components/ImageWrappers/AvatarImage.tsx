2a8c7e511db6a07e06f88ece924aeee5907ca329
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-17 10:51:02 -0700

    added options for avatarFull and added placeholder

--- apps/remix-app/app/components/ImageWrappers/AvatarImage.tsx
+++ apps/remix-app/app/components/ImageWrappers/AvatarImage.tsx
@@ -1,18 +1,21 @@
 export default function AvatarImage({
   avatarMedium,
+  avatarFull,
 }: {
-  avatarMedium: string, // the src for avatar
+  avatarMedium?: string, // the src for avatar
+  avatarFull?: string;
 }) {
   return (
     <img
-      src={avatarMedium}
+      src={avatarFull ? avatarFull : (avatarMedium ? avatarMedium : '/svg-images/no-image-placeholder.svg')}
       alt="avatar"
-      width={64}
-      height={64}
-      loading="lazy"
+      width={avatarFull ? 184 : (avatarMedium ? 64 : undefined)}
+      height={avatarFull ? 184 : (avatarMedium ? 64 : undefined)}
+      loading="eager"
       onError={(e) => {
         e.currentTarget.src = '/svg-images/no-image-placeholder.svg';
       }}
+      className="rounded-lg"
     />
   );
 }
