7edd645835a1184036bba02d61b06e3280c31eb2
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-17 10:50:47 -0700

    added placeholder, made src optional, added className

--- apps/remix-app/app/components/ImageWrappers/AppHeaderImage.tsx
+++ apps/remix-app/app/components/ImageWrappers/AppHeaderImage.tsx
@@ -1,17 +1,19 @@
 export default function AppHeaderImage({
   headerImageSrc,
   name,
+  className,
 }: {
-  headerImageSrc: string,
-  name: string,
+  headerImageSrc?: string;
+  name: string;
+  className?: string;
 }) {
   return (
     <img
-      src={headerImageSrc}
+      src={headerImageSrc ? headerImageSrc : '/svg-images/no-image-placeholder.svg'}
       alt={`cover art for ${name}`}
       width={460}
       height={215}
-      className="rounded-md"
+      className={`rounded-md ${className}`}
       loading="lazy"
       onError={(e) => {
         e.currentTarget.src = '/svg-images/no-image-placeholder.svg';
