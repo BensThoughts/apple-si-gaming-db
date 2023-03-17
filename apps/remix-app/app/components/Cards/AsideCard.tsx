aafb946cf5c543cc45e2e9aacea8717f0da0a210
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-17 10:43:27 -0700

    added padding to AsideCard

--- apps/remix-app/app/components/Cards/AsideCard.tsx
+++ apps/remix-app/app/components/Cards/AsideCard.tsx
@@ -4,25 +4,25 @@ import { InformationCircleIcon } from '~/components/Icons';
 export default function AsideCard({
   title,
   iconBackground,
-  className,
   children,
-  ...rest
 }: {
   title: string;
-  iconBackground: 'bg-primary' | 'bg-secondary' | 'bg-tertiary' | 'bg-app-bg' | 'bg-app-bg-secondary',
   children: React.ReactNode;
-} & React.HTMLAttributes<HTMLDivElement>) {
+  iconBackground: 'bg-primary' | 'bg-secondary' | 'bg-tertiary' | 'bg-app-bg' | 'bg-app-bg-secondary',
+}) {
   return (
-    <aside className={`relative px-6 py-8 border-l-[4px] border-solid border-secondary
-                       rounded-lg mt-5 bg-primary text-primary ${className}`} {...rest}>
-      <div className={`rounded-[50%] absolute inset-0 w-[40px] h-[40px]
+    <div className="p-4">
+      <aside className="relative px-6 py-8 border-l-[4px] border-solid border-secondary
+                       rounded-lg mt-5 bg-primary text-primary w-full max-w-md">
+        <div className={`rounded-[50%] absolute inset-0 w-[40px] h-[40px]
                        -translate-x-[50%] -translate-y-[50%] ${iconBackground}`}>
-        <InformationCircleIcon className="absolute inset-0 translate-x-[3px] translate-y-[3px] text-icon-secondary w-8 h-8" />
-      </div>
-      <div className="flex flex-col gap-3">
-        <strong className="font-bold">{title}</strong>
-        <div>{children}</div>
-      </div>
-    </aside>
+          <InformationCircleIcon className="absolute inset-0 translate-x-[3px] translate-y-[3px] text-icon-secondary w-8 h-8" />
+        </div>
+        <div className="flex flex-col gap-3">
+          <strong className="font-bold">{title}</strong>
+          <div>{children}</div>
+        </div>
+      </aside>
+    </div>
   );
 }
