ecb5c7c5a5f712ba360b7888302a01b8a5437c61
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-19 13:47:15 -0700

    changed to SmallAppsGridLayout

--- apps/remix-app/app/components/Profile/Library/LibraryLayout.tsx
+++ apps/remix-app/app/components/Profile/Library/LibraryLayout.tsx
@@ -1,39 +1,32 @@
-import Heading from '~/components/Heading';
-import OwnedAppsLayout from './OwnedAppsLayout';
+import SmallAppsGridLayout from '~/components/Apps/SmallAppsGridLayout';
+import type { SteamAppForSmallAppsGridLayout } from '~/interfaces';
 import SyncLibraryForm from './SyncLibraryForm';
 
-interface LibraryLayoutProps {
-  ownedApps: {
-    steamAppId: number;
-    name: string;
-    headerImage?: string | null;
-    genres: {
-      id: string;
-      description: string;
-    }[];
-    platformMac?: boolean | null;
-  }[];
+type LibraryLayoutProps = {
+  ownedApps: SteamAppForSmallAppsGridLayout[];
 }
 
 export default function LibraryLayout({
   ownedApps,
 }: LibraryLayoutProps) {
   return (
-    <div className="flex flex-col gap-8 items-center w-full p-4 bg-tertiary rounded-lg border-1 border-secondary-highlight">
-      <Heading>Library</Heading>
-
+    <div className="flex flex-col gap-8 items-center w-full p-4 rounded-lg">
       {/* <div className="flex flex-col items-center md:flex-row gap-2 md:items-start justify-between w-full">
         <Heading>Library</Heading>
       </div> */}
       {(ownedApps && ownedApps.length > 0) ? (
-        <div>
-          <OwnedAppsLayout ownedApps={ownedApps} />
+        <div className="flex flex-col items-center justify-center self-center gap-8 w-full">
+          <div className="">
+            <SyncLibraryForm />
+          </div>
+          <div>
+            <SmallAppsGridLayout steamApps={ownedApps} />
+          </div>
         </div>
       ) : (
         <div className="max-w-md">
           <div
-            className="flex flex-col gap-3 px-6 py-8 border-1 border-secondary-highlight
-                       rounded-md bg-primary text-primary"
+            className="flex flex-col gap-3 px-6 py-8 rounded-md bg-primary text-primary"
           >
             <strong className="font-bold">No Apps Found</strong>
             <div>
