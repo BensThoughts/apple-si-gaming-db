5df8a4c3def0880d005931b119ff87e17e11d431
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-19 13:53:37 -0700

    updated system spec layout style

--- apps/remix-app/app/components/Profile/Systems/SystemSpecLayout.tsx
+++ apps/remix-app/app/components/Profile/Systems/SystemSpecLayout.tsx
@@ -3,7 +3,6 @@ import type {
   DeleteSystemSpecActionData,
   EditSystemSpecActionData,
 } from '~/lib/form-actions/profile/interfaces';
-import Heading from '~/components/Heading';
 import CreateSystemForm from './CreateSystemForm';
 import SystemSpecDisplay from './SystemSpecDisplay';
 import CreateSystemInstructions from './CreateSystemInstructions';
@@ -21,7 +20,6 @@ interface SystemSpecLayoutProps {
     videoPrimaryVRAM: string | null;
     memoryRAM: string | null;
   }[];
-  isSubmittingCreateSystemForm: boolean;
   createSystemSpecActionData?: CreateSystemSpecActionData;
   editSystemSpecActionData?: EditSystemSpecActionData;
   deleteSystemSpecActionData?: DeleteSystemSpecActionData;
@@ -29,40 +27,42 @@ interface SystemSpecLayoutProps {
 
 export default function SystemSpecLayout({
   systemSpecs,
-  isSubmittingCreateSystemForm,
   createSystemSpecActionData,
   editSystemSpecActionData,
   deleteSystemSpecActionData,
 }: SystemSpecLayoutProps) {
   return (
-    <div className="flex flex-col gap-8 items-center w-full p-4 bg-tertiary rounded-lg border-1 border-secondary-highlight">
-      <Heading>Systems</Heading>
-      <div className="w-full max-w-md border-1 border-secondary-highlight rounded-md p-4">
-      Creating predefined systems makes it easier to attach system information
-      to your performance posts. You can have more than one system defined.
-        <strong className="font-semibold text-primary-highlight">
-        &nbsp;Try it out!
-        </strong>
-      </div>
-      <div className="w-full">
-        <SystemSpecDisplay
-          systemSpecs={systemSpecs}
-          editSystemSpecActionData={editSystemSpecActionData}
-          deleteSystemSpecActionData={deleteSystemSpecActionData}
-        />
-      </div>
+    <div className="flex flex-col gap-8 items-center w-full">
+
+      {systemSpecs.length > 0 && (
+        <div className="w-full">
+          <SystemSpecDisplay
+            systemSpecs={systemSpecs}
+            editSystemSpecActionData={editSystemSpecActionData}
+            deleteSystemSpecActionData={deleteSystemSpecActionData}
+          />
+        </div>
+      )}
       <div className="w-full flex flex-col items-center gap-3">
-        <h3 className="text-secondary text-lg">Create a New System</h3>
-        <div className="flex flex-col items-center xl:items-start xl:flex-row xl:justify-around w-full">
-          <div>
-            <CreateSystemInstructions />
-          </div>
-          <div className="w-full max-w-md mt-5">
+        <div className="flex flex-col gap-8 items-center xl:items-start xl:flex-row xl:justify-around w-full">
+          <div className="flex flex-col gap-6 justify-center items-center w-full bg-tertiary rounded-lg p-6">
+            <h3 className="text-secondary text-lg">Create a New System</h3>
             <CreateSystemForm
-              isSubmittingCreateSystemForm={isSubmittingCreateSystemForm}
               createSystemSpecActionData={createSystemSpecActionData}
               currentSystemNames={systemSpecs.map((systemSpec) => systemSpec.systemName)}
             />
+          </div>
+          <div className="w-full max-w-md flex flex-col gap-3">
+            <div className="w-full max-w-md bg-tertiary rounded-md p-4">
+              Creating predefined systems makes it easier to attach system information
+              to your performance posts. You can have more than one system defined.
+              <strong className="font-semibold text-primary-highlight">
+                &nbsp;Try it out!
+              </strong>
+            </div>
+            <div className="p-3 bg-tertiary rounded-lg">
+              <CreateSystemInstructions />
+            </div>
           </div>
         </div>
       </div>
