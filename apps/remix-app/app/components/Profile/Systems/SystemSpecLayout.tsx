9f10fecbedad3aec9c22ba905a9aa49ee7452e17
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-22 10:07:22 -0700

    adjusted layout with flex order

--- apps/remix-app/app/components/Profile/Systems/SystemSpecLayout.tsx
+++ apps/remix-app/app/components/Profile/Systems/SystemSpecLayout.tsx
@@ -32,40 +32,32 @@ export default function SystemSpecLayout({
   deleteSystemSpecActionData,
 }: SystemSpecLayoutProps) {
   return (
-    <div className="flex flex-col gap-8 items-center w-full">
 
-      {systemSpecs.length > 0 && (
-        <div className="w-full">
-          <SystemSpecDisplay
-            systemSpecs={systemSpecs}
-            editSystemSpecActionData={editSystemSpecActionData}
-            deleteSystemSpecActionData={deleteSystemSpecActionData}
-          />
-        </div>
-      )}
-      <div className="w-full flex flex-col items-center gap-3">
-        <div className="flex flex-col gap-8 items-center xl:items-start xl:flex-row xl:justify-around w-full">
-          <div className="flex flex-col gap-6 justify-center items-center w-full bg-tertiary rounded-lg p-6">
-            <h3 className="text-secondary text-lg">Create a New System</h3>
-            <CreateSystemForm
-              createSystemSpecActionData={createSystemSpecActionData}
-              currentSystemNames={systemSpecs.map((systemSpec) => systemSpec.systemName)}
-            />
-          </div>
-          <div className="w-full max-w-md flex flex-col gap-3">
-            <div className="w-full max-w-md bg-tertiary rounded-md p-4">
+    <div className="flex flex-col gap-8 items-center xl:items-start xl:flex-row
+                    xl:justify-around w-full">
+
+      <div className="w-full flex flex-col items-center gap-6 xl:order-2">
+        <div className="w-full max-w-lg bg-tertiary rounded-lg p-4">
               Creating predefined systems makes it easier to attach system information
-              to your performance posts. You can have more than one system defined.
-              <strong className="font-semibold text-primary-highlight">
+              to your posts. You can have more than one system defined.
+          <strong className="font-semibold text-primary-highlight">
                 &nbsp;Try it out!
-              </strong>
-            </div>
-            <div className="p-3 bg-tertiary rounded-lg">
-              <CreateSystemInstructions />
-            </div>
-          </div>
+          </strong>
         </div>
+        <SystemSpecDisplay
+          systemSpecs={systemSpecs}
+          editSystemSpecActionData={editSystemSpecActionData}
+          deleteSystemSpecActionData={deleteSystemSpecActionData}
+        />
       </div>
+
+      <div className="w-full flex justify-center xl:order-1">
+        <CreateSystemForm
+          createSystemSpecActionData={createSystemSpecActionData}
+          currentSystemNames={systemSpecs.map((systemSpec) => systemSpec.systemName)}
+        />
+      </div>
+
     </div>
   );
 }
