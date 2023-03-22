866943b94d36fa110383665d6fd1bd42f687e296
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-22 10:04:48 -0700

    removed AsideCard changed to text-sm

--- apps/remix-app/app/components/Profile/Systems/CreateSystemInstructions.tsx
+++ apps/remix-app/app/components/Profile/Systems/CreateSystemInstructions.tsx
@@ -1,28 +1,27 @@
-import AsideCard from '~/components/Cards/AsideCard';
 
 export default function CreateSystemInstructions() {
   return (
-    <AsideCard title="How To Create a System" iconBackground="bg-tertiary">
-      <div className="flex flex-col gap-3">
-        <div>
+    // <AsideCard title="How To Create a System" iconBackground="bg-tertiary">
+    <div className="flex flex-col gap-3 text-sm">
+      <div>
           Copy and paste your system information directly from Steam
           to create a system
-        </div>
-        <ul className="list-disc ml-6">
-          <li>
-            In Steam go to the help menu
-          </li>
-          <li>
-            Select "System Information"
-          </li>
-          <li>
-            Right click and select "copy all text to clipboard"
-          </li>
-          <li>
-            Paste that into the system info field
-          </li>
-        </ul>
       </div>
-    </AsideCard>
+      <ul className="list-disc ml-6">
+        <li>
+            In Steam go to the help menu
+        </li>
+        <li>
+            Select "System Information"
+        </li>
+        <li>
+            Right click and select "copy all text to clipboard"
+        </li>
+        <li>
+            Paste that into the System Information field
+        </li>
+      </ul>
+    </div>
+    // </AsideCard>
   );
 }
