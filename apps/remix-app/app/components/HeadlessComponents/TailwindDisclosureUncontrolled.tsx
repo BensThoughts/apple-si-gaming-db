3ad08d3a1be775dc2fabef99e9d07646af40710c
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-22 14:37:56 -0700

    fixed border flash with border-b-transparent

--- apps/remix-app/app/components/HeadlessComponents/TailwindDisclosureUncontrolled.tsx
+++ apps/remix-app/app/components/HeadlessComponents/TailwindDisclosureUncontrolled.tsx
@@ -1,4 +1,8 @@
-import { Disclosure } from '@headlessui/react';
+import {
+  Disclosure,
+  // Transition,
+} from '@headlessui/react';
+// import { Fragment } from 'react';
 import { ChevronUpIcon } from '~/components/Icons';
 
 export default function TailwindDisclosureUncontrolled({
@@ -18,19 +22,29 @@ export default function TailwindDisclosureUncontrolled({
             <Disclosure.Button
               className={`peer flex w-full justify-between px-4 py-2 text-left text-sm
                         font-medium focus-visible:show-ring transition-colors
-                        bg-primary hover:bg-primary-highlight
-                        ${open ? 'rounded-t-lg border-b-1 border-b-primary-highlight' : 'rounded-lg'}`}
+                        bg-primary hover:bg-primary-highlight border-b-1
+                        ${open ? 'rounded-t-lg border-b-primary-highlight' : 'rounded-lg border-b-transparent'}`}
             >
               <span>{title}</span>
               <ChevronUpIcon
                 className={`${open ? 'rotate-180 transform' : 'rotate-0 transform'} h-5 w-5 text-primary`}
               />
             </Disclosure.Button>
+            {/* <Transition
+              as={Fragment}
+              enter="transition duration-300 ease-in-out"
+              enterFrom="transform max-h-0 opacity-0"
+              enterTo="transform max-h-[200px] opacity-100"
+              leave="transition duration-300 ease-in-out"
+              leaveFrom="transform max-h-[200px] opacity-100"
+              leaveTo="transform max-h-0 opacity-0"
+            > */}
             <Disclosure.Panel
-              className={`peer-focus-visible:show-ring p-3 bg-primary rounded-b-lg`}
+              className="peer-focus-visible:show-ring p-3 bg-primary rounded-b-lg"
             >
               {children}
             </Disclosure.Panel>
+            {/* </Transition> */}
           </>
         )}
       </Disclosure>
