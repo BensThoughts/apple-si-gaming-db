601ef65772fe523a695ad14cfd735622abec4b1d
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-30 18:56:00 -0700

    moved edit button based on container query

--- apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostMetaBar/index.tsx
+++ apps/remix-app/app/components/AppInfo/PerformancePosts/PerformancePostMetaBar/index.tsx
@@ -5,7 +5,8 @@ import type {
 } from '~/interfaces';
 import { convertFrameRateToDescription, convertRatingMedalToDescription } from '~/lib/conversions/rating-conversions';
 import { useUserSession } from '~/lib/hooks/useMatchesData';
-import EditAndDeleteButtons from './EditAndDeleteButtons';
+// import EditAndDeleteButtons from './EditAndDeleteButtons';
+import EditButton from './EditButton';
 
 type PerformancePostMetaBarProps = {
   performancePostMetadata: PerformancePostMetaBarData;
@@ -44,23 +45,41 @@ export default function PerformancePostMetaBar({
 
   return (
     <div className="@container">
-      <div className="flex flex-col gap-1 items-start justify-between px-3 py-1
+      <div className="flex flex-col gap-1 items-start justify-between px-3 py-2
                       rounded-sm bg-primary w-full text-sm text-primary-faded
                       @[706px]:gap-0 @[706px]:flex-row @[706px]:items-center
                       supports-[not(container-type:inline-size)]:postMetaBarQuery:gap-0
                       supports-[not(container-type:inline-size)]:postMetaBarQuery:flex-row
                       supports-[not(container-type:inline-size)]:postMetaBarQuery:items-center"
       >
-        <div className="flex flex-col gap-2 items-start justify-center
-                        @[650px]:items-center @[650px]:flex-row @[650px]:gap-3
+        <div className="flex flex-col gap-2 items-start justify-start w-full
+                        @[650px]:items-center @[650px]:flex-row @[650px]:gap-3 @[650px]:w-fit
+                        supports-[not(container-type:inline-size)]:postMetaBarQuery:w-fit
                         supports-[not(container-type:inline-size)]:postMetaBarQuery:gap-3
                         supports-[not(container-type:inline-size)]:postMetaBarQuery:flex-row
                         supports-[not(container-type:inline-size)]:postMetaBarQuery:items-center"
         >
-          <LikeButton
-            performancePostId={performancePostId}
-            numLikes={numLikes}
-          />
+          <div
+            className="flex gap-2 justify-between items-center w-full @[650px]:w-fit
+                       supports-[not(container-type:inline-size)]:postMetaBarQuery:w-fit"
+          >
+            <LikeButton
+              performancePostId={performancePostId}
+              numLikes={numLikes}
+            />
+            {(didSteamUserCreatePost && !currentlyOnEditPage) && (
+              <div
+                className="block @[650px]:hidden
+                    supports-[not(container-type:inline-size)]:postMetaBarQuery:hidden"
+              >
+                <EditButton
+                  steamAppId={steamAppId}
+                  performancePostId={performancePostId}
+                  redirectToAfterEdit={redirectToAfterEdit}
+                />
+              </div>
+            )}
+          </div>
           <div className="flex flex-col gap-1 md:flex-row md:gap-2">
             <div>
               <span className="text-primary-highlight text-base leading-4">
@@ -84,11 +103,16 @@ export default function PerformancePostMetaBar({
             </div>
           </div>
           {(didSteamUserCreatePost && !currentlyOnEditPage) && (
-            <EditAndDeleteButtons
-              steamAppId={steamAppId}
-              performancePostId={performancePostId}
-              redirectToAfterEdit={redirectToAfterEdit}
-            />
+            <span
+              className="hidden @[650px]:inline
+                         supports-[not(container-type:inline-size)]:postMetaBarQuery:hidden"
+            >
+              <EditButton
+                steamAppId={steamAppId}
+                performancePostId={performancePostId}
+                redirectToAfterEdit={redirectToAfterEdit}
+              />
+            </span>
           )}
         </div>
 
