9439236de1b12013220501ac50fa704c88d60e63
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-03-31 09:15:55 -0700

    working nav hider with extra props

--- apps/remix-app/app/components/Layout/Navbar/NavHider.tsx
+++ apps/remix-app/app/components/Layout/Navbar/NavHider.tsx
@@ -9,11 +9,55 @@ type NavHiderProps = {
 }
 
 export default function NavHider({ className, children }: NavHiderProps) {
-  const { scrollDirection, y } = useScrollDirection();
+  const {
+    y,
+    scrollDirection,
+    // initialScrollDirection,
+    // initialScrollYCoordinate,
+    // prevScrollDirection,
+    yCoordinateOnDirectionChange,
+  } = useScrollDirection();
+
+  // console.log('START');
+  // console.log('y: ' + y);
+  // console.log('scrollDirection: ' + scrollDirection);
+  // console.log('initialScrollDirection: ' + initialScrollDirection);
+  // console.log('initialScrollYCoordinate: ' + initialScrollYCoordinate);
+
+  // console.log('yCoordinateOnDirectionChange: ' + yCoordinateOnDirectionChange);
+  // console.log('prevScrollDirection: ' + prevScrollDirection);
+
+  const alwaysShowUntil = 350;
+  const showOnUpAfter = 350;
+  // const hideOnDownAfter = 0;
+
+
+  const shouldShowNav = () => {
+    if (scrollDirection === undefined) {
+      return true;
+    }
+    if (y >= 0 && y <= alwaysShowUntil) {
+      return true;
+    }
+    if (scrollDirection === 'down' && y > alwaysShowUntil) {
+      return false;
+    }
+    if (
+      yCoordinateOnDirectionChange != undefined &&
+      scrollDirection === 'up' &&
+      y > yCoordinateOnDirectionChange - showOnUpAfter) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const showNav = shouldShowNav();
+
   return (
     <Transition
       as={Fragment}
-      show={scrollDirection === 'down' && y > 350 ? false : true}
+      show={showNav}
       enter="transform transition duration-200 ease-out"
       enterFrom="-translate-y-full"
       enterTo="translate-y-0"
--- apps/remix-app/app/lib/hooks/useScrollDirection.ts
+++ apps/remix-app/app/lib/hooks/useScrollDirection.ts
@@ -1,15 +1,49 @@
 import { useState, useEffect } from 'react';
 import { useWindowScroll } from 'react-use';
 
-type ScrollDirection = 'up' | 'down';
+export type ScrollDirection = 'up' | 'down';
+
+// type ScrollDirectionChange = {
+//   prevScrollDirection: ScrollDirection;
+//   yCoordinateOnDirectionChange: number;
+// }
 
 export default function useScrollDirection() {
   const [lastScrollYCoordinate, setLastScrollYCoordinate] = useState(0);
+  const [initialScrollDirection, setInitialScrollDirection] = useState<ScrollDirection | undefined>(undefined);
+  const [initialScrollYCoordinate, setInitialScrollYCoordinate] = useState<number | undefined>(undefined);
   const { y } = useWindowScroll();
-  const [scrollDirection, setScrollDirection] = useState<ScrollDirection>('up');
+  const [scrollDirection, setScrollDirection] = useState<ScrollDirection | undefined>(undefined);
+
+  const [yCoordinateOnDirectionChange, setYCoordinateOnDirectionChange] = useState<number | undefined>(undefined);
+  const [prevScrollDirection, setPrevScrollDirection] = useState<ScrollDirection | undefined>(undefined);
+
+  const [mounted, setMounted] = useState(false);
+  // const [scrollDirectionChange, setScrollDirectionChange] = useState<ScrollDirectionChange | undefined>(undefined);
+
+  useEffect(() => {
+    if (!mounted) {
+      setLastScrollYCoordinate(y);
+      setMounted(true);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mounted]);
 
   const listener = (e: any) => {
-    setScrollDirection(y > lastScrollYCoordinate ? 'down' : 'up');
+    const scrollDirection = y > lastScrollYCoordinate ? 'down' : 'up';
+    if (initialScrollDirection === undefined) {
+      setInitialScrollDirection(scrollDirection);
+      setInitialScrollYCoordinate(y);
+      return;
+    }
+    if (
+      scrollDirection === prevScrollDirection ||
+      scrollDirection != initialScrollDirection
+    ) {
+      setYCoordinateOnDirectionChange(y);
+      setPrevScrollDirection(scrollDirection === 'down' ? 'up' : 'down');
+    }
+    setScrollDirection(scrollDirection);
     setLastScrollYCoordinate(y);
   };
 
@@ -20,5 +54,12 @@ export default function useScrollDirection() {
     };
   });
 
-  return { scrollDirection, y };
+  return {
+    y,
+    initialScrollDirection,
+    initialScrollYCoordinate,
+    scrollDirection,
+    prevScrollDirection,
+    yCoordinateOnDirectionChange,
+  } as const;
 }
