83ed56ab17dfc6c4f63c07399c82ba3800e9bb78
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-04-07 17:46:26 -0700

    moved file, changed padding

--- apps/remix-app/app/components/Apps/PerformancePosts/PerformancePostLayoutCard.tsx
+++ apps/remix-app/app/components/Apps/PerformancePosts/PerformancePostLayoutCard.tsx
@@ -1,6 +1,6 @@
 export default function PostLayoutCard({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full bg-tertiary p-3 rounded-lg">
+    <div className="w-full bg-tertiary px-4 py-5 rounded-lg">
       {children}
     </div>
   );
