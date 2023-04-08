c5a179bdd6e30935cd825318f6b9f6b35d2f9dd3
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-04-08 09:36:56 -0700

    simplified PostDisplay for user profile

--- apps/remix-app/app/components/Profile/UserProfilePostDisplay.tsx
+++ apps/remix-app/app/components/Profile/UserProfilePostDisplay.tsx
@@ -1,161 +1,54 @@
-import { Link } from '@remix-run/react';
-import type { PerformancePostForUserProfileDisplay } from '~/interfaces';
-import { convertTierRankToName } from '~/lib/conversions/rating-conversions';
-import PerformancePostMetaBar from '../AppInfo/PerformancePosts/PerformancePostMetaBar';
-import AppHeaderImage from '../ImageWrappers/AppHeaderImage';
+import type { PerformancePost } from '~/interfaces';
+import PerformancePostTierRankContainer from '../Apps/PerformancePosts/Display/TierRankContainer';
+import EditButton from '../Buttons/EditButton';
+import SystemSpecsPopover from '../HeadlessComponents/SystemSpecPopover';
+import LikeButton from '../Buttons/LikeButton';
+import PerformancePostAppBadge from '../Apps/PerformancePosts/Display/AppBadge';
+import ViewButton from '../Buttons/ViewButton';
 
 export default function UserProfilePostDisplay({
   performancePost,
 }: {
-  performancePost: PerformancePostForUserProfileDisplay;
+  performancePost: PerformancePost;
 }) {
   const {
     performancePostId,
     createdAt,
-    userWhoCreated: {
-      steamUserId64,
-    },
+    userWhoCreated: { steamUserId64 },
     numLikes,
-    rating: {
-      ratingTierRank,
-      frameRateTierRank,
-      frameRateStutters,
-    },
     postText,
     steamApp: {
       steamAppId,
       name,
       headerImage,
     },
+    rating,
+    postTags,
+    systemSpec,
   } = performancePost;
   return (
     <div
       id={performancePostId.toString()}
-      className="rounded-xl px-0 py-0 pb-4 bg-tertiary focus:show-ring
-                 w-full max-w-md lg:max-w-4xl flex items-center justify-start"
+      className="px-3 py-4 md:px-4 rounded-lg bg-tertiary w-full flex flex-col gap-4 items-start justify-center"
     >
-      {/* Large Screens */}
-      <div className="hidden lg:flex lg:flex-col lg:gap-0 lg:w-full">
-        <Link
-          to={`/apps/${steamAppId}/posts#${performancePostId}`}
-          className="group rounded-t-xl p-3 bg-tertiary hover:bg-tertiary-highlight"
-        >
-
-          <div className="flex gap-4 w-full h-full">
-            {headerImage && (
-              <div className="w-full max-w-[12rem] self-start">
-                <AppHeaderImage headerImageSrc={headerImage} name={name} />
-              </div>
-            )}
-            <div className="flex flex-col gap-2 text-sm w-full">
-              <div className="flex justify-between">
-                <div>
-                  <span className="font-semibold">
-                    {name}
-                  </span>
-                  {` - `}<i className="italic">{convertTierRankToName(ratingTierRank)}</i>
-                </div>
-                <span className="text-secondary font-medium relative px-0 py-[0.1em]
-                  after:block after:absolute after:bottom-[-3px] after:left-0
-                  after:w-0 after:h-[0.2em] after:bg-secondary after:transition-all
-                  group-hover:after:w-full after:rounded-full
-                  after:duration-200">
-                  <span className="hidden lg:inline">
-                    View Post
-                  </span>
-                  <span className="inline lg:hidden">
-                    View
-                  </span>
-                </span>
-              </div>
-              <div className="line-clamp-3">
-                {postText}
-              </div>
-            </div>
-          </div>
-        </Link>
-
-
-        <div className="w-full">
-          <PerformancePostMetaBar
-            performancePostMetadata={{
-              performancePostId,
-              createdAt,
-              steamApp: { steamAppId },
-              userWhoCreated: { steamUserId64 },
-              rating: {
-                ratingTierRank,
-                frameRateTierRank,
-                frameRateStutters,
-              },
-              numLikes,
-            }}
+      <div className="w-full flex flex-col gap-4 justify-center lg:flex-row lg:gap-0 lg:items-center lg:justify-between">
+        <PerformancePostAppBadge headerImage={headerImage} name={name} createdAt={createdAt} />
+        <div className="flex items-center gap-2 flex-wrap justify-start">
+          <LikeButton performancePostId={performancePostId} numLikes={numLikes} />
+          <ViewButton steamAppId={steamAppId} performancePostId={performancePostId} />
+          <EditButton
+            steamAppId={steamAppId}
+            performancePostId={performancePostId}
+            userWhoCreated={{ steamUserId64 }}
           />
+          <SystemSpecsPopover systemSpec={systemSpec} giveButtonStyles />
         </div>
       </div>
-
-
-      {/* Small Screens */}
-      <div
-        className="lg:hidden flex flex-col items-center w-full"
-      >
-        <Link
-          to={`/apps/${steamAppId}/posts#${performancePostId}`}
-          className="flex flex-col items-start gap-2 group p-0 bg-tertiary
-                     hover:bg-tertiary-highlight rounded-t-xl"
-        >
-          <div className="h-auto max-w-lg">
-            <AppHeaderImage
-              headerImageSrc={headerImage}
-              name={name}
-              className="h-full w-full flex justify-center object-cover
-                         bg-tertiary rounded-t-xl"
-            />
-          </div>
-          <div className="flex flex-col gap-2 px-2 pb-2 w-full">
-            <div className="flex justify-between items-center w-full">
-              <div>
-                <span className="font-semibold">
-                  {name}
-                </span>
-                {` - `}<i className="italic">{convertTierRankToName(ratingTierRank)}</i>
-              </div>
-              <div className="self-start">
-                <span className="text-secondary font-medium relative px-0 py-[0.1em]
-                  after:block after:absolute after:bottom-[-3px] after:left-0
-                  after:w-0 after:h-[0.2em] after:bg-secondary after:transition-all
-                  group-hover:after:w-full after:rounded-full
-                  after:duration-200">
-                  <span>
-                    View
-                  </span>
-                </span>
-              </div>
-            </div>
-
-            <div className="line-clamp-4">
-              {postText}
-            </div>
-          </div>
-
-        </Link>
-
-        <div className="w-full">
-          <PerformancePostMetaBar
-            performancePostMetadata={{
-              performancePostId,
-              createdAt,
-              steamApp: { steamAppId },
-              userWhoCreated: { steamUserId64 },
-              rating: {
-                ratingTierRank,
-                frameRateTierRank,
-                frameRateStutters,
-              },
-              numLikes,
-            }}
-          />
-        </div>
+      <p className="text-primary-highlight">
+        {postText}
+      </p>
+      <div>
+        <PerformancePostTierRankContainer rating={rating} postTags={postTags} />
       </div>
     </div>
   );
