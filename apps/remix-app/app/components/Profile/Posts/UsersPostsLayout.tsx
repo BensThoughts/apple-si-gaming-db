982399b95f322077607c02c956d636942a9a6466
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-04-08 09:40:20 -0700

    added search bar for user posts layout

--- apps/remix-app/app/components/Profile/Posts/UsersPostsLayout.tsx
+++ apps/remix-app/app/components/Profile/Posts/UsersPostsLayout.tsx
@@ -1,18 +1,30 @@
 import RemixUnderlineLink from '~/components/RemixUnderlineLink';
 import type {
-  PerformancePostForUserProfileDisplay,
+  PerformancePost,
 } from '~/interfaces';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 // import UserProfilePostDisplayV2 from '../UserProfilePostDisplayV2';
 import UserProfilePostDisplay from '../UserProfilePostDisplay';
+import MaterialInputOutlined from '~/components/FormComponents/MaterialInputOutlined';
 
 interface UsersPostsLayoutProps {
-  steamUsersPosts: PerformancePostForUserProfileDisplay[];
+  steamUsersPosts: PerformancePost[];
 }
 
 export default function UsersPostsLayout({
   steamUsersPosts,
 }: UsersPostsLayoutProps) {
+  const [nameQuery, setNameQuery] = useState('');
+
+  function searchNames(performancePosts: PerformancePost[]) {
+    return performancePosts.filter(
+        (post) =>
+          (post.steamApp.name.toString().toLowerCase().includes(nameQuery.toLowerCase())))
+        .sort((a, b) => ((new Date(a.createdAt)) > new Date(b.createdAt)) ? 1 : -1);
+  }
+
+  const filteredPosts = searchNames(steamUsersPosts);
+
   if (steamUsersPosts.length < 1) {
     return (
       <div className="max-w-max p-4 bg-tertiary rounded-lg border-1 border-secondary-highlight">
@@ -29,14 +41,26 @@ export default function UsersPostsLayout({
     );
   }
   return (
-    <div className="flex flex-col gap-6 items-center w-full">
-      {steamUsersPosts.map((performancePost) => (
-        <Fragment key={performancePost.performancePostId}>
-          <UserProfilePostDisplay
-            performancePost={performancePost}
-          />
-        </Fragment>
-      ))}
+    <div className="flex flex-col items-center gap-8 w-full">
+      <div className="bg-tertiary rounded-lg p-4 w-full">
+        <MaterialInputOutlined
+          id="profileSearch"
+          name="profileSearch"
+          label="Game name..."
+          componentSize="large"
+          aria-label="Search games by name"
+          onChange={(e) => setNameQuery(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col gap-6 items-center w-full">
+        {filteredPosts.map((performancePost) => (
+          <Fragment key={performancePost.performancePostId}>
+            <UserProfilePostDisplay
+              performancePost={performancePost}
+            />
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 }
