bac5fa0ff54305c188a199b32d4709c2714b498b
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-04-07 17:47:11 -0700

    moved file, added button style option

--- apps/remix-app/app/components/HeadlessComponents/SystemSpecPopover.tsx
+++ apps/remix-app/app/components/HeadlessComponents/SystemSpecPopover.tsx
@@ -1,16 +1,33 @@
 import { Popover } from '@headlessui/react';
 import { useState } from 'react';
-import type { PerformancePostSystemSpecPopover } from '~/interfaces';
+import type { SystemSpec } from '~/interfaces';
+import { classNames } from '~/lib/css/classNames';
+import { HardDriveIcon } from '../Icons';
 
-type SystemSpecsPopoverProps = PerformancePostSystemSpecPopover & {
-  children: React.ReactNode;
+type SystemSpecsPopoverProps = {
+  systemSpec: {
+    manufacturer: SystemSpec['manufacturer'];
+    model: SystemSpec['model'];
+    osVersion: SystemSpec['osVersion'];
+    cpuBrand: SystemSpec['cpuBrand'];
+    videoDriver: SystemSpec['videoDriver'];
+    videoDriverVersion: SystemSpec['videoDriverVersion'];
+    videoPrimaryVRAM: SystemSpec['videoPrimaryVRAM'];
+    memoryRAM: SystemSpec['memoryRAM'];
+  };
+  children?: React.ReactNode;
+  giveButtonStyles?: boolean;
 }
 
 export default function SystemSpecsPopover({
   systemSpec,
   children,
+  giveButtonStyles = false,
 }: SystemSpecsPopoverProps) {
   const [isShowing, setIsShowing] = useState(false);
+  if (!Object.values(systemSpec).some(Boolean)) {
+    return null;
+  }
   const {
     manufacturer,
     model,
@@ -28,15 +45,39 @@ export default function SystemSpecsPopover({
           <Popover.Button
             onMouseEnter={() => setIsShowing(true)}
             onMouseLeave={() => setIsShowing(false)}
-            className="text-primary select-none outline-none focus-visible:show-ring-tertiary rounded-sm"
+            className={classNames(
+                'text-primary select-none outline-none',
+                'focus-visible:show-ring-tertiary rounded-sm',
+                giveButtonStyles
+                  ? classNames(
+                      'bg-primary hover:bg-primary-highlight rounded-full',
+                      'flex gap-1.5 items-center border-secondary-highlight',
+                      'border-1 py-1 px-3 h-[38px] group',
+                  )
+                  : '',
+            )}
             aria-label="show system hardware"
           >
-            {children}
+            {giveButtonStyles ? (
+              <>
+                <HardDriveIcon
+                  className="stroke-text-primary group-hover:stroke-text-primary-highlight
+                             stroke-1"
+                />
+                <span className="text-primary group-hover:text-primary-highlight text-sm font-light">
+              System
+                </span>
+              </>
+            ) : (
+              <>
+                {children}
+              </>
+            )}
           </Popover.Button>
 
           {(isShowing || open) && (
             <Popover.Panel
-              className="absolute z-10 bg-primary border-secondary-highlight
+              className="absolute z-[100] bg-primary border-secondary-highlight
                  border-1 rounded-md p-2 w-screen max-w-[18rem] md:max-w-xs"
               onMouseEnter={() => setIsShowing(true)}
               onMouseLeave={() => setIsShowing(false)}
