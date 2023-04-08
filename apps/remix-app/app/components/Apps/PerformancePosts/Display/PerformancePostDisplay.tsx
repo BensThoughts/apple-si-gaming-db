01d7596aec06ec94547776ed82ed377cc5419ab7
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-04-07 20:43:57 -0700

    created button container for PerformancePostDisplay

--- apps/remix-app/app/components/Apps/PerformancePosts/Display/PerformancePostDisplay.tsx
+++ apps/remix-app/app/components/Apps/PerformancePosts/Display/PerformancePostDisplay.tsx
@@ -1,12 +1,9 @@
 import TierRankContainer from './TierRankContainer';
-import SystemSpecsPopover from '~/components/HeadlessComponents/SystemSpecPopover';
 import type {
   PerformancePost,
 } from '~/interfaces';
-import { useUserSession } from '~/lib/hooks/useMatchesData';
-import EditButton from '~/components/Buttons/EditButton';
-import PerformancePostBadge from './UserBadge';
-import LikeButton from '~/components/Buttons/LikeButton';
+import PerformancePostUserBadge from './UserBadge';
+import ButtonContainer from './ButtonContainer';
 
 type PerformancePostDisplayProps = {
   performancePost: PerformancePost;
@@ -19,7 +16,11 @@ export default function PerformancePostDisplay({
     performancePostId,
     createdAt,
     postText,
-    userWhoCreated,
+    userWhoCreated: {
+      steamUserId64,
+      avatarMedium,
+      displayName,
+    },
     rating: {
       ratingTierRank,
       frameRateTierRank,
@@ -30,50 +31,33 @@ export default function PerformancePostDisplay({
     numLikes,
     systemSpec,
     postTags,
-    steamApp: {
-      steamAppId,
-    },
+    steamApp: { steamAppId },
   } = performancePost;
 
-  const { userSession } = useUserSession();
-
-  const didSteamUserCreatePost = userSession
-    ? userWhoCreated.steamUserId64 === userSession.steamUserProfile.steamUserId64
-    : false;
-
-  const {
-    avatarMedium,
-    displayName,
-  } = userWhoCreated;
-
   return (
-    <div className="flex flex-col gap-4 w-full h-full bg-tertiary px-3 lg:px-4 py-4 rounded-lg">
+    <div
+      id={performancePostId.toString()}
+      className="flex flex-col gap-4 w-full h-full bg-tertiary px-3 lg:px-4 py-4 rounded-lg"
+    >
       <div className="flex flex-col gap-6 lg:flex-row lg:gap-0 lg:items-center lg:justify-between">
-        <div className="flex items-center gap-2">
-          <LikeButton performancePostId={performancePostId} numLikes={numLikes} />
-          <SystemSpecsPopover systemSpec={systemSpec} giveButtonStyles />
-          {didSteamUserCreatePost && (
-            <div>
-              <EditButton
-                steamAppId={steamAppId}
-                performancePostId={performancePostId}
-              />
-            </div>
-          )}
-        </div>
+        <ButtonContainer
+          performancePostId={performancePostId}
+          numLikes={numLikes}
+          systemSpec={systemSpec}
+          steamApp={{ steamAppId }}
+          userWhoCreated={{ steamUserId64 }}
+        />
         <div className="flex pr-2 min-w-[165px]">
-          <PerformancePostBadge
+          <PerformancePostUserBadge
             avatarMedium={avatarMedium}
             displayName={displayName}
             createdAt={createdAt}
           />
         </div>
       </div>
-
       <div className="text-primary-highlight">
         {postText}
       </div>
-
       <TierRankContainer
         rating={{
           ratingTierRank,
