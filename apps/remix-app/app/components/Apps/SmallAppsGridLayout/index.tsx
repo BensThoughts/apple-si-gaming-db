5d70f59e77c2e6bb2c83fd11631643ff8bdddce2
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-04-08 09:35:17 -0700

    changed gap amount

--- apps/remix-app/app/components/Apps/SmallAppsGridLayout/index.tsx
+++ apps/remix-app/app/components/Apps/SmallAppsGridLayout/index.tsx
@@ -90,7 +90,7 @@ export default function SmallAppsGridLayout({
 
       <div className="flex flex-col items-center justify-center self-center gap-8 w-full">
 
-        <div className="flex flex-wrap justify-center items-center gap-3 md:gap-4">
+        <div className="flex flex-wrap justify-center items-center gap-3">
           {paginatedSteamApps.map(({
             steamAppId,
             name,
