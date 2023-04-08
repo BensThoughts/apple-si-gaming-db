6d2098dfab2ac7808861c902f4d8b8a25891d763
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-04-07 18:52:29 -0700

    added max-w for small screens

--- apps/remix-app/app/components/Apps/SmallAppsGridLayout/SmallAppDisplayCard.tsx
+++ apps/remix-app/app/components/Apps/SmallAppsGridLayout/SmallAppDisplayCard.tsx
@@ -17,8 +17,8 @@ export default function SmallAppDisplayCard({
     <Link
       to={`/apps/${steamAppId}/posts`}
       className={classNames(
-          'flex flex-col justify-center items-center gap-1 bg-tertiary p-2',
-          'hover:bg-tertiary-highlight rounded-xl w-full md:max-w-[210px]',
+          'flex flex-col justify-center items-center gap-1 bg-tertiary p-2 md:p-1',
+          'hover:bg-tertiary-highlight rounded-md w-full max-w-[460px] md:max-w-[210px]',
           'focus-visible:show-ring-app-bg group/small-app-card hover:scale-105',
           'duration-300 ease-in-out',
           className ? className : '',
@@ -26,20 +26,20 @@ export default function SmallAppDisplayCard({
     >
       <div
         className="w-[276px] md:w-[184px] whitespace-nowrap overflow-hidden
-                   text-ellipsis text-primary-highlight"
+                   text-ellipsis text-primary-highlight self-start"
         aria-label={name}
       >
         {name}
       </div>
       <div
-        className="relative rounded-xl overflow-hidden before:-skew-x-12
+        className="relative rounded-md overflow-hidden before:-skew-x-12
                    before:absolute before:inset-0 before:-translate-x-full
                    group-hover/small-app-card:before:animate-shimmer
                    before:bg-gradient-to-r before:from-transparent
                    before:via-white/10 before:to-transparent transition-all
                    ease-in-out duration-300"
       >
-        <AppHeaderImage headerImageSrc={headerImgSrc} name={name} className="rounded-xl" />
+        <AppHeaderImage headerImageSrc={headerImgSrc} name={name} className="rounded-md" />
       </div>
 
     </Link>
