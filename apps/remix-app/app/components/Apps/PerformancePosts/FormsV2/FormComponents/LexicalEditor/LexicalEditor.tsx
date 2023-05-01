84a37a18414cbcf9a7edfa0647d47a89ba3b0a28
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-01 09:09:56 -0700

    fixed placeholder on lexical editor

--- apps/remix-app/app/components/Apps/PerformancePosts/FormsV2/FormComponents/LexicalEditor/LexicalEditor.tsx
+++ apps/remix-app/app/components/Apps/PerformancePosts/FormsV2/FormComponents/LexicalEditor/LexicalEditor.tsx
@@ -40,7 +40,8 @@ export function LexicalEditor(props: LexicalEditorProps) {
             // className="relative"
           />
         }
-        placeholder={(isEditable) => isEditable ? <Placeholder /> : null}
+        placeholder={<Placeholder />}
+        // placeholder={(isEditable) => isEditable ? <Placeholder /> : null}
         ErrorBoundary={LexicalErrorBoundary}
       />
       <LocalStoragePlugin namespace={props.config.namespace} />
@@ -54,7 +55,7 @@ export function LexicalEditor(props: LexicalEditorProps) {
 
 const Placeholder = () => {
   return (
-    <div className="absolute top-[63px] left-4 opacity-50 pointer-events-none overflow-hidden overflow-ellipsis select-none inline-block">
+    <div className="absolute z-1 top-[63px] left-4 opacity-50 pointer-events-none overflow-hidden overflow-ellipsis select-none inline-block">
       Create Post...
     </div>
   );
@@ -80,8 +81,9 @@ export function Editor() {
           'prose prose-slate dark:prose-invert prose-p:my-0',
           'prose-headings:mb-4 prose-headings:mt-2',
           '[&_s]:line-through [&_u]:underline',
-          'w-full',
-          'border-slate-500 bg-primary border-2 rounded-md focus-within:outline-none focus-within:border-slate-900 dark:focus-within:border-slate-400',
+          'border-slate-500 bg-primary border-2 rounded-md focus-within:outline-none',
+          'focus-within:border-slate-900 dark:focus-within:border-slate-400',
+          'w-full relative z-0 isolate',
       )}
     >
       <LexicalEditor
