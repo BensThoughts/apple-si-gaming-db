5619ea45848ae8156243a9bff3a9d1a2edbe265c
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-01 06:38:47 -0700

    added icon exports to index

---
+++ apps/remix-app/app/components/Icons/Lucide/index.tsx
@@ -0,0 +1,7 @@
+export * from './Icons/Circle';
+export * from './Icons/CircleDot';
+export * from './Icons/Heading-1';
+export * from './Icons/Heading-2';
+export * from './Icons/List';
+export * from './Icons/ListOrdered';
+export * from './Icons/Strikethrough';
