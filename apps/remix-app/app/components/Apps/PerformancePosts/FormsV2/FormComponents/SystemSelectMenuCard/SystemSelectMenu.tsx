034ae6e6b85e1ad365f76e93a0f5bf63ba9bba6f
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-05 09:34:53 -0700

    updated to use form context

--- apps/remix-app/app/components/Apps/PerformancePosts/FormsV2/FormComponents/SystemSelectMenuCard/SystemSelectMenu.tsx
+++ apps/remix-app/app/components/Apps/PerformancePosts/FormsV2/FormComponents/SystemSelectMenuCard/SystemSelectMenu.tsx
@@ -2,34 +2,39 @@ import SelectMenu from '~/components/FormComponents/SelectMenu';
 import type { SelectOption } from '~/components/FormComponents/SelectMenu';
 import { PerformancePostFormFieldNames } from '~/lib/enums/FormFields/PerformancePost';
 import type { SystemSpecOption } from '~/types';
+import { initialSystemSpecOption } from '../../FormContext/initialFormOptions';
+import { PerformancePostFormStateActions, usePerformancePostFormState } from '../../FormContext/PerformancePostFormContext';
+
+export type SystemSpecSelectOption = SelectOption<number>;
 
 export default function SystemSelectMenu({
   systemSpecOptions,
-  defaultSystemSpecId,
 }: {
   systemSpecOptions: SystemSpecOption[];
-  defaultSystemSpecId?: number;
 }) {
-  const systemNameOptions: SelectOption<number>[] = systemSpecOptions.map((option) => (
+  const { state, dispatch } = usePerformancePostFormState();
+  const systemNameOptions: SystemSpecSelectOption[] = systemSpecOptions.map((option) => (
     {
       name: option.systemName,
       value: option.id,
     }
   ));
-  systemNameOptions.unshift({
-    name: 'None',
-    value: -1,
-  });
+  systemNameOptions.unshift(initialSystemSpecOption);
 
-  const defaultValue = systemNameOptions.find((option) => option.value === defaultSystemSpecId);
-
+  function onSelectionChange(selection: SystemSpecSelectOption) {
+    dispatch({
+      type: PerformancePostFormStateActions.SET_SYSTEM_SPEC_OPTION,
+      payload: selection,
+    });
+  }
 
   return (
     <SelectMenu
       name={PerformancePostFormFieldNames.SystemSpecId}
-      defaultValue={defaultValue ? defaultValue : systemNameOptions[0]}
+      value={state.systemSpecSelectedOption}
       options={systemNameOptions}
       labelText="System"
+      onChange={onSelectionChange}
     />
   );
 }
