5c40e064fa68f789397f6bd48fdd4816bc7cba52
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-05 09:38:48 -0700

    export combined create/edit action data type

--- apps/remix-app/app/lib/form-actions/performance-post/types/index.ts
+++ apps/remix-app/app/lib/form-actions/performance-post/types/index.ts
@@ -1,3 +1,15 @@
-export * from './CreatePerformancePostAction';
-export * from './EditPerformancePostAction';
+// import type { CreatePerformancePostActionData } from './CreatePerformancePostAction';
+// import type { EditPerformancePostActionData } from './EditPerformancePostAction';
+import type {
+  PerformancePostFormError,
+  PerformancePostFormFieldErrors,
+  PerformancePostFormFieldsTyped,
+} from './FormFields';
+
+export type CreateOrEditPerformancePostActionData = {
+  formError?: PerformancePostFormError;
+  fields?: PerformancePostFormFieldsTyped; // used for defaultValue options
+  fieldErrors?: PerformancePostFormFieldErrors;
+}
+
 export type { PerformancePostFormFieldsRaw } from './FormFields';
