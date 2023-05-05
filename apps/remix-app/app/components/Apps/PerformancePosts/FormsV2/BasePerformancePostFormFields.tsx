5a27d4c0e79ec98d4a9cd85737f2d8cc81344e4a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-05 08:56:14 -0700

    added isSubmittingForm prop, dispatch upsert initial Form state action

--- apps/remix-app/app/components/Apps/PerformancePosts/FormsV2/BasePerformancePostFormFields.tsx
+++ apps/remix-app/app/components/Apps/PerformancePosts/FormsV2/BasePerformancePostFormFields.tsx
@@ -3,7 +3,7 @@ import type {
   GamepadOption,
   PostTagOption,
 } from '~/types';
-import RatingTierRankSelectMenu from './FormComponents/RatingTierRankSelectMenu';
+import RatingTierRankSelectMenu, { ratingTierRankOptions } from './FormComponents/RatingTierRankSelectMenu';
 import PostTagMultiSelectMenu from './FormComponents/PostTagMultiSelectMenu';
 import SystemSelectMenuCard from './FormComponents/SystemSelectMenuCard';
 import FormRatingDisplay from './FormRatingDisplay';
@@ -15,6 +15,9 @@ import { useEffect } from 'react';
 import type {
   CreateOrEditPerformancePostActionData,
 } from '~/lib/form-actions/performance-post/types';
+import { initialFrameRateTierRankSelectOption, initialGamepadOption, initialGamepadTierRankOption, initialRatingTierRankSelectOption, initialSystemSpecOption } from './FormContext/initialValues';
+import { frameRateAverageOptions } from '../Forms/FormComponents/FrameRateTierRankSelectMenu';
+import { gamepadTierRankOptions } from './FormComponents/GamepadRating/GamepadTierRankRadioGroup';
 
 interface BasePerformancePostFormFieldsProps {
   formId: string;
@@ -25,6 +28,7 @@ interface BasePerformancePostFormFieldsProps {
   postTagOptions: PostTagOption[];
   gamepadOptions: GamepadOption[];
   systemSpecOptions: SystemSpecOption[];
+  isSubmittingForm: boolean;
 }
 
 export default function BasePerformancePostFormFields({
@@ -36,29 +40,49 @@ export default function BasePerformancePostFormFields({
   postTagOptions,
   gamepadOptions,
   systemSpecOptions,
+  isSubmittingForm,
 }: BasePerformancePostFormFieldsProps) {
   const { dispatch } = usePerformancePostFormState();
 
   useEffect(() => {
     if (fields) {
+      const defaultRatingTierRankOption = ratingTierRankOptions
+          .find((option) => option.value === fields.ratingTierRank);
+      const defaultFrameRateTierRankOption = frameRateAverageOptions
+          .find((option) => option.value === fields.frameRateTierRank);
+      const defaultGamepadOption = gamepadOptions
+          .find((option) => option.id === fields.gamepadId);
+      const defaultGamepadTierRankOption = gamepadTierRankOptions
+          .find((option) => option.value === fields.gamepadTierRank);
+      const defaultSystemSpecOption = systemSpecOptions
+          .find((option) => option.id === fields.systemSpecId);
       dispatch({
         type: PerformancePostFormStateActions.UPSERT_FORM_STATE,
         payload: {
-          ratingTierRankValue: fields.ratingTierRank ? fields.ratingTierRank : 'None',
-          frameRateTierRankValue: fields.frameRateTierRank ? fields.frameRateTierRank : 'None',
+          ratingTierRankSelectedOption: defaultRatingTierRankOption ? defaultRatingTierRankOption : initialRatingTierRankSelectOption,
+          frameRateTierRankSelectedOption: defaultFrameRateTierRankOption ? defaultFrameRateTierRankOption : initialFrameRateTierRankSelectOption,
           frameRateStuttersValue: fields.frameRateStutters ? fields.frameRateStutters : false,
-          gamepadName: '',
-          gamepadValue: fields.gamepadId ? fields.gamepadId : -1,
-          gamepadTierRankValue: fields.gamepadTierRank ? fields.gamepadTierRank : 'None',
+          gamepadSelectedOption: defaultGamepadOption
+            ? { name: defaultGamepadOption.description, value: defaultGamepadOption.id }
+            : initialGamepadOption,
+          gamepadTierRankSelectedOption: defaultGamepadTierRankOption ? defaultGamepadTierRankOption : initialGamepadTierRankOption,
+          systemSpecSelectedOption: defaultSystemSpecOption
+            ? { name: defaultSystemSpecOption.systemName, value: defaultSystemSpecOption.id }
+            : initialSystemSpecOption,
         },
       });
+    } else if (isSubmittingForm) {
+      dispatch({
+        type: PerformancePostFormStateActions.RESET_FORM_STATE,
+        payload: undefined,
+      });
     }
-  }, [dispatch, fields]);
+  }, [dispatch, fields, gamepadOptions, systemSpecOptions, isSubmittingForm]);
 
   return (
     <div className="flex flex-col gap-6">
       <div className="flex gap-x-2 gap-y-4 justify-between flex-wrap">
-        <RatingTierRankSelectMenu defaultRatingTierRank={fields?.ratingTierRank} />
+        <RatingTierRankSelectMenu />
         <FrameRateRating />
         <GamepadRating gamepads={gamepadOptions} />
       </div>
@@ -82,7 +106,7 @@ export default function BasePerformancePostFormFields({
         defaultSystemSpecId={fields?.systemSpecId}
       />
       {(formError || fieldErrors) && (
-        <div className="w-full flex justify-center">
+        <div className="w-full flex flex-col gap-2 items-center">
           {formError && <div className="text-error">{formError}</div>}
           {(fieldErrors && Object.values(fieldErrors).some(Boolean)) && (
             Object.entries(fieldErrors).map((error, idx) => (
