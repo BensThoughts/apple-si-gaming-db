cbbc4d3817f94c14277b6a4d58225f42efc11539
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-05 09:33:08 -0700

    added postHTML and serializedEditorState to form extraction

--- apps/remix-app/app/lib/form-actions/performance-post/extract-form-data.ts
+++ apps/remix-app/app/lib/form-actions/performance-post/extract-form-data.ts
@@ -1,3 +1,5 @@
+import DOMPurify from 'isomorphic-dompurify';
+
 import type {
   PerformancePostFormFieldsRaw,
 } from './types';
@@ -5,6 +7,8 @@ import type {
 import {
   PerformancePostFormFieldNames,
 } from '~/lib/enums/FormFields/PerformancePost';
+import type { PerformancePostFormFieldsTyped } from './types/FormFields';
+import { isTypeFrameRateTierRank, isTypeGamepadTierRank, isTypeRatingTierRank } from '~/lib/form-validators/posts';
 
 /**
   Converts performance post form data into usable data for creating or editing a
@@ -16,19 +20,22 @@ export function extractFormData(formData: FormData): {
   fieldsRaw?: PerformancePostFormFieldsRaw;
 } {
   const postText = formData.get(PerformancePostFormFieldNames.PostText);
-  // const postHTML = formData.get(PerformancePostFormFieldNames.PostHTML);
-  const frameRateTierRank = formData.get(PerformancePostFormFieldNames.FrameRateTierRank + '[value]');
+  const postHTML = formData.get(PerformancePostFormFieldNames.PostHTML);
+  const serializedLexicalEditorState = formData.get(PerformancePostFormFieldNames.SerializedLexicalEditorState);
+  const frameRateTierRank = formData.get(PerformancePostFormFieldNames.FrameRateTierRank);
   const frameRateStutters = formData.get(PerformancePostFormFieldNames.FrameRateStutters);
   const ratingTierRank = formData.get(PerformancePostFormFieldNames.RatingTierRank + '[value]');
   const systemSpecIdData = formData.get(PerformancePostFormFieldNames.SystemSpecId + '[value]');
   const postTagIdsData = formData.getAll(PerformancePostFormFieldNames.PostTagIds);
-  const gamepadIdData = formData.get(PerformancePostFormFieldNames.GamepadId + '[value]');
-  const gamepadTierRank = formData.get(PerformancePostFormFieldNames.GamepadTierRank + '[value]');
+  const gamepadIdData = formData.get(PerformancePostFormFieldNames.GamepadId);
+  const gamepadTierRank = formData.get(PerformancePostFormFieldNames.GamepadTierRank);
 
   if (
     typeof postText !== 'string' ||
-    // typeof postHTML !== 'string' ||
+    typeof postHTML !== 'string' ||
+    typeof serializedLexicalEditorState !== 'string' ||
     typeof frameRateTierRank !== 'string' ||
+    typeof frameRateStutters !== 'string' ||
     typeof ratingTierRank !== 'string' ||
     typeof systemSpecIdData !== 'string' ||
     typeof gamepadIdData !== 'string' ||
@@ -50,14 +57,31 @@ export function extractFormData(formData: FormData): {
   return {
     fieldsRaw: {
       postText,
-      // postHTML,
+      postHTML: DOMPurify.sanitize(postHTML), // TODO: is this computationally intensive?
+      serializedLexicalEditorState,
       frameRateTierRank,
-      frameRateStutters: frameRateStutters ? true : false,
+      frameRateStutters: frameRateStutters === 'true' ? true : false,
       ratingTierRank,
       systemSpecId,
       gamepadId,
       gamepadTierRank,
       postTagIds,
     },
+  };
+}
+
+export function convertRawToTypedPerformancePostFormFields(
+    formFieldsRaw: PerformancePostFormFieldsRaw,
+): PerformancePostFormFieldsTyped {
+  const {
+    ratingTierRank,
+    frameRateTierRank,
+    gamepadTierRank,
+  } = formFieldsRaw;
+  return {
+    ...formFieldsRaw,
+    ratingTierRank: isTypeRatingTierRank(ratingTierRank) ? ratingTierRank : undefined,
+    frameRateTierRank: isTypeFrameRateTierRank(frameRateTierRank) ? frameRateTierRank : undefined,
+    gamepadTierRank: isTypeGamepadTierRank(gamepadTierRank) ? gamepadTierRank : undefined,
   };
 }
