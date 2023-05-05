1ad4795dfdf2b0669f650c17b8747a32ab9c6401
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-05 09:38:32 -0700

    added form field error types and postHTML/serializedEditorState

--- apps/remix-app/app/lib/form-actions/performance-post/types/FormFields.ts
+++ apps/remix-app/app/lib/form-actions/performance-post/types/FormFields.ts
@@ -4,26 +4,44 @@ import type {
   GamepadTierRank,
 } from '~/types';
 
+export type PerformancePostFormError = string;
+
+export type PerformancePostFormFieldErrors ={
+  postText?: string;
+  postHTML?: string;
+  serializedLexicalEditorState?: string;
+  ratingTierRank?: string;
+  frameRateTierRank?: string;
+  frameRateStutters?: string;
+  gamepadId?: string;
+  gamepadTierRank?: string;
+  postTagIds?: string;
+  systemSpecId?: string;
+}
+
 export type PerformancePostFormFieldsTyped = {
   postText?: string;
-  // postHTML?: string;
+  postHTML?: string;
+  serializedLexicalEditorState?: string;
   ratingTierRank?: RatingTierRank;
   frameRateTierRank?: FrameRateTierRank;
   frameRateStutters?: boolean;
-  postTagIds?: number[];
   gamepadId?: number;
   gamepadTierRank?: GamepadTierRank;
+  postTagIds?: number[];
   systemSpecId?: number;
 }
 
+// This is from FormData (with some casting to number in a few cases)
 export type PerformancePostFormFieldsRaw = {
   postText: string;
-  // postHTML: string;
+  postHTML: string;
+  serializedLexicalEditorState: string;
+  ratingTierRank: string;
   frameRateTierRank: string;
   frameRateStutters: boolean;
-  ratingTierRank: string;
-  systemSpecId: number;
   gamepadId: number;
   gamepadTierRank: string;
   postTagIds: number[];
+  systemSpecId: number;
 }
