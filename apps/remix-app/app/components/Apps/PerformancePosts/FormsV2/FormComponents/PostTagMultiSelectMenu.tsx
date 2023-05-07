2601f0fb405fe094d0dd026221f905a6623a107c
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-06 19:37:19 -0700

    changed to use form state context

--- apps/remix-app/app/components/Apps/PerformancePosts/FormsV2/FormComponents/PostTagMultiSelectMenu.tsx
+++ apps/remix-app/app/components/Apps/PerformancePosts/FormsV2/FormComponents/PostTagMultiSelectMenu.tsx
@@ -2,34 +2,65 @@ import MultiSelectMenu from '~/components/FormComponents/MultiSelectMenu';
 import type { MultiSelectOption } from '~/components/FormComponents/MultiSelectMenu';
 import { PerformancePostFormFieldNames } from '~/lib/enums/FormFields/PerformancePost';
 import type { PostTagOption } from '~/types';
+import { PerformancePostFormStateActions, usePerformancePostFormState } from '../FormContext/PerformancePostFormContext';
+import type { ActionMeta } from 'react-select';
+
+export type PostTagMultiSelectOption = MultiSelectOption<number>;
+
+function isTypePostTagMultiSelectOptionArray(option: unknown): option is PostTagMultiSelectOption[] {
+  if (!Array.isArray(option)) {
+    return false;
+  }
+  const everyIsPostTagMultiSelectOption = option.every((option) => {
+    if (
+      typeof option.label === 'string' &&
+      typeof option.value === 'number'
+    ) {
+      return true;
+    }
+    return false;
+  });
+  if (everyIsPostTagMultiSelectOption) {
+    return true;
+  }
+  return false;
+}
 
 export default function PostTagMultiSelectMenu({
   formId,
   postTags,
-  defaultPostTagIds,
-  fieldError,
+  // defaultPostTagIds,
 }: {
   formId: string;
   postTags: PostTagOption[];
-  defaultPostTagIds?: number[]; // defaultValue
-  fieldError?: string;
 }) {
-  const postTagOptions: MultiSelectOption<number>[] = postTags.map((tag) => (
+  const { state, dispatch } = usePerformancePostFormState();
+
+  const onSelectionChange = (newValue: unknown, actionMeta: ActionMeta<unknown>) => {
+    if (isTypePostTagMultiSelectOptionArray(newValue)) {
+      dispatch({
+        type: PerformancePostFormStateActions.SET_POST_TAG_MULTI_SELECT,
+        payload: newValue,
+      });
+    }
+  };
+
+  const postTagOptions: PostTagMultiSelectOption[] = postTags.map((tag) => (
     {
       label: tag.description,
       value: tag.id,
     }
   ));
-  const defaultValue = postTagOptions.filter((option) => defaultPostTagIds?.includes(option.value));
+  // const defaultValue = postTagOptions.filter((option) => defaultPostTagIds?.includes(option.value));
   return (
     <MultiSelectMenu
       name={PerformancePostFormFieldNames.PostTagIds}
       id={`${PerformancePostFormFieldNames.PostTagIds}-${formId}`}
       // labelText="Tags"
       options={postTagOptions}
-      defaultValue={defaultValue}
-      fieldError={fieldError}
-      isMulti={true}
+      // defaultValue={defaultValue}
+      value={state.postTagMultiSelectOption}
+      onChange={onSelectionChange}
       closeMenuOnSelect={false}
       placeholderText="Tags..."
     />
