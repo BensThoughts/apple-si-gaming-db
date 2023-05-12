d9b7301e1f52c8026c644fa05e0db0d32006300e
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-12 06:14:51 -0700

    fixed blank editor state

--- apps/remix-app/app/components/Apps/PerformancePosts/Forms/FormComponents/LexicalEditor/LexicalEditor.tsx
+++ apps/remix-app/app/components/Apps/PerformancePosts/Forms/FormComponents/LexicalEditor/LexicalEditor.tsx
@@ -91,7 +91,7 @@ const Placeholder = ({
 };
 
 const EDITOR_NAMESPACE = 'lexical-editor';
-const BLANK_SERIALIZED_EDITOR_STATE = `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`;
+const BLANK_SERIALIZED_EDITOR_STATE = `{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}`;
 
 export function Editor({
   defaultState,
