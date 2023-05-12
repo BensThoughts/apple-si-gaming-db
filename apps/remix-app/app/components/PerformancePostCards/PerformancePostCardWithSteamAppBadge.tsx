fafdf6187c755cbfcbe08bc8f43d0fb3066d0aee
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-12 07:19:04 -0700

    created ButtonContainer changed gaps

--- apps/remix-app/app/components/PerformancePostCards/PerformancePostCardWithSteamAppBadge.tsx
+++ apps/remix-app/app/components/PerformancePostCards/PerformancePostCardWithSteamAppBadge.tsx
@@ -7,6 +7,33 @@ import PerformancePostAppBadge from '~/components/PerformancePostCards/CardCompo
 import ViewButton from '~/components/Buttons/ViewButton';
 import HTMLContainer from './CardComponents/HTMLContainer';
 
+function ButtonContainer({
+  steamAppId,
+  performancePostId,
+  userWhoCreated,
+  numLikes,
+  systemSpec,
+}: {
+  steamAppId: number;
+  performancePostId: number;
+  userWhoCreated: { steamUserId64: string };
+  numLikes: number;
+  systemSpec: PerformancePost['systemSpec'];
+}) {
+  return (
+    <div className="flex items-center gap-2 justify-start flex-wrap">
+      <LikeButton performancePostId={performancePostId} numLikes={numLikes} />
+      <ViewButton steamAppId={steamAppId} performancePostId={performancePostId} />
+      <EditButton
+        steamAppId={steamAppId}
+        performancePostId={performancePostId}
+        userWhoCreated={userWhoCreated}
+      />
+      <SystemSpecsPopover systemSpec={systemSpec} paddingY={8} giveButtonStyles />
+    </div>
+  );
+}
+
 export default function PerformancePostCardWithSteamAppBadge({
   performancePost,
 }: {
@@ -15,7 +42,7 @@ export default function PerformancePostCardWithSteamAppBadge({
   const {
     performancePostId,
     createdAt,
-    userWhoCreated: { steamUserId64 },
+    userWhoCreated,
     numLikes,
     postText,
     postHTML,
@@ -31,25 +58,25 @@ export default function PerformancePostCardWithSteamAppBadge({
   return (
     <div
       id={performancePostId.toString()}
-      className="px-3 py-4 md:px-4 rounded-lg bg-tertiary w-full flex flex-col gap-4 items-start justify-center"
+      className="px-3 py-4 md:px-4 rounded-lg bg-tertiary w-full flex
+                 flex-col gap-y-4 items-start justify-center"
     >
-      <div className="w-full flex flex-col gap-4 justify-center lg:flex-row lg:gap-0 lg:items-center lg:justify-between">
-        <PerformancePostAppBadge headerImage={headerImage} name={name} createdAt={createdAt} />
-        <div className="flex items-center gap-2 flex-wrap justify-start">
-          <LikeButton performancePostId={performancePostId} numLikes={numLikes} />
-          <ViewButton steamAppId={steamAppId} performancePostId={performancePostId} />
-          <EditButton
-            steamAppId={steamAppId}
-            performancePostId={performancePostId}
-            userWhoCreated={{ steamUserId64 }}
-          />
-          <SystemSpecsPopover systemSpec={systemSpec} paddingY={8} giveButtonStyles />
-        </div>
+      <div className="w-full flex justify-between flex-wrap gap-y-4 gap-x-2">
+        <PerformancePostAppBadge
+          headerImage={headerImage}
+          name={name}
+          createdAt={createdAt}
+        />
+        <ButtonContainer
+          steamAppId={steamAppId}
+          performancePostId={performancePostId}
+          userWhoCreated={userWhoCreated}
+          numLikes={numLikes}
+          systemSpec={systemSpec}
+        />
       </div>
       <HTMLContainer htmlString={postHTML ? postHTML : postText} />
-      <div>
-        <TierRankContainer rating={rating} postTags={postTags} />
-      </div>
+      <TierRankContainer rating={rating} postTags={postTags} />
     </div>
   );
 }
