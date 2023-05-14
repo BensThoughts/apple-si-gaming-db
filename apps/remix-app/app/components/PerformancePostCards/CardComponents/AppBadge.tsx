36bc62a6c68613fc19b45d77d3121cf367eb680f
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2023-05-13 20:23:53 -0700

    fixed text wrap and overflow for safari with flex grow

--- apps/remix-app/app/components/PerformancePostCards/CardComponents/AppBadge.tsx
+++ apps/remix-app/app/components/PerformancePostCards/CardComponents/AppBadge.tsx
@@ -10,15 +10,15 @@ export default function PerformancePostAppBadge({
   createdAt: string;
 }) {
   return (
-    <div className="flex gap-2 items-center justify-center rounded-md border-0
-                    border-secondary-highlight">
+    <div className="flex gap-2 items-center rounded-md border-0
+                    border-secondary-highlight grow">
       <AppHeaderImage
         headerImageSrc={headerImage}
         name={name}
         className="object-cover w-auto h-[44px]"
       />
-      <div className="flex flex-col gap-0 w-full">
-        <span className="font-semibold break-normal text-ellipsis overflow-hidden">{name}</span>
+      <div className="flex flex-col gap-0 overflow-hidden">
+        <span className="font-semibold break-words text-ellipsis overflow-hidden">{name}</span>
         <i className="italic text-sm">{createdAt}</i>
       </div>
     </div>
--- apps/remix-app/app/components/PerformancePostCards/PerformancePostCard.tsx
+++ apps/remix-app/app/components/PerformancePostCards/PerformancePostCard.tsx
@@ -42,7 +42,7 @@ export default function PerformancePostCard({
       className="flex flex-col gap-4 w-full h-full bg-tertiary px-3 lg:px-4 py-4 rounded-lg"
     >
       <div className="flex flex-col gap-6 lg:flex-row lg:gap-0 lg:items-center lg:justify-between">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 order-2 lg:order-1">
           <LikeButton performancePostId={performancePostId} numLikes={numLikes} />
           <SystemSpecsPopover systemSpec={systemSpec} paddingY={8} giveButtonStyles />
           <EditButton
@@ -51,7 +51,7 @@ export default function PerformancePostCard({
             userWhoCreated={{ steamUserId64 }}
           />
         </div>
-        <div className="flex pr-2 min-w-[165px]">
+        <div className="flex pr-2 min-w-[165px] order-1 lg:order-2">
           <PerformancePostUserBadge
             avatarMedium={avatarMedium}
             displayName={displayName}
